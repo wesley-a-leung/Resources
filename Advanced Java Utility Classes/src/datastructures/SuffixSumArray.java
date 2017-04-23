3b544dd460b5e6f508da67cb0e50c34f24236289
wesley-a-leung <wesley.a.leung@gmail.com>
2017-04-23 13:16:53 -0400

    Prefix and Suffix Sums Array

---
+++ Advanced Java Utility Classes/src/datastructures/SuffixSumArray.java
@@ -0,0 +1,42 @@
+package datastructures;
+
+/**
+ *  @author Wesley Leung
+ */
+public class SuffixSumArray {
+	private int[] suffixSum;
+	
+	/**
+     * Initializes a suffix sum array for the given {@code array} array.
+     * @param array the original array of integers
+     */	
+	public SuffixSumArray(int[] array) {
+		suffixSum = new int[array.length];
+		suffixSum[array.length - 1] = array[array.length - 1];
+		for (int i = suffixSum.length - 2; i >= 0; i--) {
+			suffixSum[i] = suffixSum[i + 1] + array[i];
+		}
+	}
+	
+	/**
+     * Returns the cumulative sum from index 0 to <em>end</em>
+     * @param end the 0-based start index (inclusive)
+     * @return the cumulative sum
+     */
+	public int query(int start) {
+		if (start < 0 || start >= suffixSum.length) throw new ArrayIndexOutOfBoundsException();
+		return suffixSum[start];
+	}
+	
+	/**
+     * Returns the cumulative sum from index <em>start</em> to <em>end</em>
+     * @param start the 0-based start index (inclusive)
+     * @param end the 0-based end index (inclusive)
+     * @return the cumulative sum
+     */
+	public int query(int start, int end) {
+		if (start < 0 || end < 0 || start >= suffixSum.length || end >= suffixSum.length) throw new ArrayIndexOutOfBoundsException();
+		if (end < start) throw new IllegalArgumentException();
+		return query(start) - query(end + 1);
+	}
+}
