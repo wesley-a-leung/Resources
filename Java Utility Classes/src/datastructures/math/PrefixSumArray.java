3a22d00743e5a9af576e71014191736264dab99e
wesley-a-leung <wesley.a.leung@gmail.com>
2017-05-14 15:20:12 -0400

    Changed PrefixSumArray to 1-based indexing and added 2D variant

--- Java Utility Classes/src/datastructures/math/PrefixSumArray.java
+++ Java Utility Classes/src/datastructures/math/PrefixSumArray.java
@@ -11,31 +11,28 @@ public class PrefixSumArray {
      * @param array the original array of integers
      */	
 	public PrefixSumArray(int[] array) {
-		prefixSum = new int[array.length];
-		prefixSum[0] = array[0];
+		prefixSum = new int[array.length + 1];
 		for (int i = 1; i < prefixSum.length; i++) {
-			prefixSum[i] = prefixSum[i - 1] + array[i];
+			prefixSum[i] = prefixSum[i - 1] + array[i - 1];
 		}
 	}
 	
 	/**
-     * Returns the cumulative sum from index 0 to <em>end</em>
-     * @param end the 0-based end index (inclusive)
+     * Returns the cumulative sum from index 1 to <em>end</em>
+     * @param end the 1-based end index (inclusive)
      * @return the cumulative sum
      */
 	public int query(int end) {
-		if (end < 0 || end >= prefixSum.length) throw new ArrayIndexOutOfBoundsException();
 		return prefixSum[end];
 	}
 	
 	/**
      * Returns the cumulative sum from index <em>start</em> to <em>end</em>
-     * @param start the 0-based start index (inclusive)
-     * @param end the 0-based end index (inclusive)
+     * @param start the 1-based start index (inclusive)
+     * @param end the 1-based end index (inclusive)
      * @return the cumulative sum
      */
 	public int query(int start, int end) {
-		if (start < 0 || end < 0 || start >= prefixSum.length || end >= prefixSum.length) throw new ArrayIndexOutOfBoundsException();
 		if (end < start) throw new IllegalArgumentException();
 		return query(end) - query(start - 1);
 	}
