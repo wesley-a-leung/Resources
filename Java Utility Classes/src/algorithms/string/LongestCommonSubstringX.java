88ed66adc6181de38ba5f07b1af275014b2b352c
wesley-a-leung <wesley.a.leung@gmail.com>
2017-05-22 17:45:42 -0400

    String algorithms optimizations

--- Java Utility Classes/src/algorithms/string/LongestCommonSubstringX.java
+++ Java Utility Classes/src/algorithms/string/LongestCommonSubstringX.java
@@ -1,23 +1,25 @@
 package algorithms.string;
 
-public class LongestCommonSubstring {
+public class LongestCommonSubstringX {
 	private String substring;
 	private int length;
 	
-	public LongestCommonSubstring(String s1, String s2) {
+	public LongestCommonSubstringX(String s1, String s2) {
 		process(s1, s2);
 	}
 	
 	private void process(String s1, String s2) {
-		int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+		int[][] dp = new int[2][s2.length() + 1];
 		int start = 0, maxLen = 0;
+		int s = 1;
 		for (int i = 1; i <= s1.length(); i++) {
+			s = 1 - s;
 			for (int j = 1; j <= s2.length(); j++) {
 				if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-					dp[i][j] = 1 + dp[i - 1][j - 1];
-					if (dp[i][j] > maxLen) {
-						maxLen = dp[i][j];
-						start = i - dp[i][j];
+					dp[s][j] = 1 + dp[1 - s][j - 1];
+					if (dp[s][j] > maxLen) {
+						maxLen = dp[s][j];
+						start = i - dp[s][j];
 					}
 				}
 			}
