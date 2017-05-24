b6414ad6d59a79d1c9137018673d76266618580d
wesley-a-leung <wesley.a.leung@gmail.com>
2017-05-24 08:55:59 -0400

    Removed flag variable in string algorithms

--- Java Utility Classes/src/algorithms/string/LongestCommonSubstringX.java
+++ Java Utility Classes/src/algorithms/string/LongestCommonSubstringX.java
@@ -11,15 +11,13 @@ public class LongestCommonSubstringX {
 	private void process(String s1, String s2) {
 		int[][] dp = new int[2][s2.length() + 1];
 		int start = 0, maxLen = 0;
-		int s = 1;
 		for (int i = 1; i <= s1.length(); i++) {
-			s = 1 - s;
 			for (int j = 1; j <= s2.length(); j++) {
 				if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-					dp[s][j] = 1 + dp[1 - s][j - 1];
-					if (dp[s][j] > maxLen) {
-						maxLen = dp[s][j];
-						start = i - dp[s][j];
+					dp[i%2][j] = 1 + dp[1 - i%2][j - 1];
+					if (dp[i%2][j] > maxLen) {
+						maxLen = dp[i%2][j];
+						start = i - dp[i%2][j];
 					}
 				}
 			}
