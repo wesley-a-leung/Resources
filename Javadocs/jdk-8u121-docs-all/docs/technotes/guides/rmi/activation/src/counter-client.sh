c33ed4f708780b1965ee55adaddad02fdee0fa00
wesley-a-leung <wesley.a.leung@gmail.com>
2017-05-26 22:12:54 -0400

    Removed Javadocs

--- Javadocs/jdk-8u121-docs-all/docs/technotes/guides/rmi/activation/src/counter-client.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-host=zaphod
-
-java -cp /tmp/client \
-     -Djava.security.policy=/tmp/policy/client.policy \
-     -Dexamples.activation.client.codebase=file:/tmp/client/ \
-     -Dexamples.activation.name=examples.activation.Counter \
-     examples.activation.CounterClient $host
