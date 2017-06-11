6c0d58f0f9274b85881fdb7aed530f4b75411c67
wesley-a-leung <wesley.a.leung@gmail.com>
2017-06-10 23:04:32 -0400

    Changes to WeightedUF

--- Java Utility Classes/datastructures/math/WeightedUF.java
+++ Java Utility Classes/datastructures/math/WeightedUF.java
@@ -7,7 +7,8 @@ package datastructures.math;
  *  methods for determining whether two sites are in the same component
  *  and the total number of components.
  *  <p>
- *  This implementation uses weighted quick union (by size) with full path compression.
+ *  This implementation uses weighted quick union (by size)
+ *  with path compaction (via halving).
  *  Initializing a data structure with <em>n</em> sites takes linear time.
  *  Afterwards, <em>union</em>, <em>find</em>, and <em>connected</em> take
  *  logarithmic time (in the worst case) and <em>count</em> takes constant
@@ -53,28 +54,31 @@ public class WeightedUF {
         return count;
     }
   
-
     /**
      * Returns the component identifier for the component containing site {@code p}.
      *
-     * @param  p the integer representing one site
+     * @param  p the integer representing one object
      * @return the component identifier for the component containing site {@code p}
      * @throws IndexOutOfBoundsException unless {@code 0 <= p < n}
      */
     public int find(int p) {
         validate(p);
-        int root = p;
-        while (root != parent[root])
-            root = parent[root];
-        while (p != root) {
-            int newp = parent[p];
-            parent[p] = root;
-            p = newp;
+        while (p != parent[p]) {
+            parent[p] = parent[parent[p]];    // path compression by halving
+            p = parent[p];
         }
-        return root;
+        return p;
     }
 
-   /**
+    // validate that p is a valid index
+    private void validate(int p) {
+        int n = parent.length;
+        if (p < 0 || p >= n) {
+            throw new IndexOutOfBoundsException("index " + p + " is not between 0 and " + (n-1));  
+        }
+    }
+
+    /**
      * Returns true if the the two sites are in the same component.
      *
      * @param  p the integer representing one site
@@ -86,16 +90,6 @@ public class WeightedUF {
      */
     public boolean connected(int p, int q) {
         return find(p) == find(q);
-    }
-    
-    /**
-     * Returns the size of the component containing p
-     * 
-     * @param p the integer representing one site
-     * @return the size of the component containing p
-     */
-    public int size(int p) {
-    	return size[find(p)];
     }
 
     /**
@@ -125,13 +119,11 @@ public class WeightedUF {
     }
     
     /**
-     * Unmerges the component containing site {@code p} with the 
+     * Unmerges the component containing site {@code p} with the
      * the component containing site {@code q}.
      *
      * @param  p the integer representing one site
      * @param  q the integer representing the other site
-     * @throws IndexOutOfBoundsException unless
-     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
      */
     public void disjoin(int p, int q) {
         int rootP = find(p);
@@ -149,12 +141,4 @@ public class WeightedUF {
         }
         count++;
     }
-    
-    // validate that p is a valid index
-    private void validate(int p) {
-        int n = parent.length;
-        if (p < 0 || p >= n) {
-            throw new IndexOutOfBoundsException("index " + p + " is not between 0 and " + (n-1));  
-        }
-    }  
 }
