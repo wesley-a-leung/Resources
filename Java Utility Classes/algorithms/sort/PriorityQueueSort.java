cdd0d2d1dcda96d659899bec41fa2995fafdd843
wesley-a-leung <wesley.a.leung@gmail.com>
2017-06-12 12:46:14 -0400

    TreeSort and PriorityQueueSort

---
+++ Java Utility Classes/algorithms/sort/PriorityQueueSort.java
@@ -0,0 +1,30 @@
+package algorithms.sort;
+
+import datastructures.MinPQ;
+
+/**
+ * The {@code PriorityQueueSort} class provides a static methods for sorting
+ * an array using a Minimum Priority Queue.
+ * 
+ * @author Wesley Leung
+ *
+ */
+public class PriorityQueueSort {
+	
+	// This class should not be instantiated.
+    private PriorityQueueSort() { }
+
+    /**
+     * Rearranges the array in ascending order, using the natural order.
+     * @param a the array to be sorted
+     */
+    public static void sort(Comparable[] a) {
+    	MinPQ<Comparable> pq = new MinPQ<Comparable>();
+    	for (Comparable x: a) {
+    		pq.insert(x);
+    	}
+    	for (int i = 0; i < a.length; i++) {
+    		a[i] = pq.delMin();
+    	}
+    }
+}
---
+++ Java Utility Classes/algorithms/sort/TreeSort.java
@@ -0,0 +1,36 @@
+package algorithms.sort;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * The {@code TreeSort} class provides a static methods for treesorting
+ * an array using TreeMaps to store frequencies.
+ * 
+ * @author Wesley Leung
+ *
+ */
+public class TreeSort {
+	
+	// This class should not be instantiated.
+    private TreeSort() { }
+
+    /**
+     * Rearranges the array in ascending order, using the natural order.
+     * @param a the array to be sorted
+     */
+    public static void sort(Comparable[] a) {
+    	TreeMap<Comparable, Integer> tree = new TreeMap<Comparable, Integer>();
+    	for (Comparable x: a) {
+    		Integer count = tree.get(x);
+    		if (count == null) tree.put(x, 1);
+    		else tree.put(x, count + 1);
+    	}
+    	int i = 0;
+    	for (Map.Entry<Comparable, Integer> x: tree.entrySet()) {
+    		for (int j = 0; j < x.getValue(); j++) {
+    			a[i++] = x.getKey();
+    		}
+    	}
+    }
+}
