e7750d47cb656c7f3f7b6802e19f44543cd62941
wesley-a-leung <wesley.a.leung@gmail.com>
2017-06-18 09:03:59 -0400

    Added DynamicSegmentTree for C++

---
+++ C++ Utility Classes/datastructures/math/DynamicSegmentTree.h
@@ -0,0 +1,82 @@
+/*
+ * DynamicSegmentTree.h
+ *
+ *  Created on: Jun 17, 2017
+ *      Author: Wesley Leung
+ */
+
+#ifndef DATASTRUCTURES_MATH_DYNAMICSEGMENTTREE_H_
+#define DATASTRUCTURES_MATH_DYNAMICSEGMENTTREE_H_
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+public:
+    Node *left;
+    Node *right;
+    int minVal;
+    Node(int val) {
+        this->minVal = val;
+        this->left = this->right = nullptr;
+    }
+
+    Node(Node *l, Node *r) {
+        this->left = l;
+        this->right = r;
+        this->minVal = min(l->minVal, r->minVal);
+    }
+};
+
+struct SegmentTree {
+private:
+    Node *root;
+    int *arr;
+    int N;
+
+    Node *build(int cL, int cR) {
+        if (cL == cR) return new Node(arr[cL]);
+        int m = (cL + cR) >> 1;
+        return new Node(build(cL , m), build(m + 1, cR));
+    }
+
+    Node *update(Node *cur, int cL, int cR, int ind) {
+        if (cL <= ind && ind <= cR) {
+            if (cL == cR) return new Node(arr[cL]);
+            int m = (cL + cR) >> 1;
+            return new Node(update(cur->left, cL, m, ind), update(cur->right, m + 1, cR, ind));
+        }
+        return cur;
+    }
+
+    int rMinQ(Node *cur, int cL, int cR, int l, int r) {
+        if (cL > r || cR < l) return INT_MAX;
+        if (cL >= l && cR <= r) return cur->minVal;
+        int m = (cL + cR) >> 1;
+        int left = rMinQ(cur->left, cL, m, l, r);
+        int right = rMinQ(cur->right, m + 1, cR, l, r);
+        return min(left, right);
+    }
+
+public:
+    void init(int size, int *array) {
+        arr = new int[size + 1];
+        for (int i = 1; i <= size; i++) {
+            arr[i] = array[i - 1];
+        }
+        root = build(1, size);
+        N = size;
+    }
+
+    void update(int ind, int val) {
+        arr[ind] = val;
+        root = update(root, 1, N, ind);
+    }
+
+    int rMinQ(int l, int r) {
+        return rMinQ(root, 1, N, l, r);
+    }
+};
+
+#endif /* DATASTRUCTURES_MATH_DYNAMICSEGMENTTREE_H_ */
