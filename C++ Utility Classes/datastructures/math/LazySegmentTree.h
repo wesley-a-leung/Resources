80f0c184728ad5a289b665cc9dfd1920925a9658
wesley-a-leung <wesley.a.leung@gmail.com>
2017-07-04 02:44:52 -0400

    Exact size of segment tree

--- C++ Utility Classes/datastructures/math/LazySegmentTree.h
+++ C++ Utility Classes/datastructures/math/LazySegmentTree.h
@@ -68,7 +68,7 @@ private:
     }
 
 public:
-    LazySegmentTree(int size, int *arr, bool oneIndexed): tree(4 * size) {
+    LazySegmentTree(int size, int *arr, bool oneIndexed): tree((int) (2 * pow(2.0, ceil(log((double) size) / log(2.0))))) {
         array = new int[size + 1];
         for (int i = 1; i <= size; i++) {
             array[i] = arr[i - !oneIndexed];
@@ -77,7 +77,7 @@ public:
         N = size;
     }
 
-    LazySegmentTree(int size): tree(4 * size) {
+    LazySegmentTree(int size): tree((int) (2 * pow(2.0, ceil(log((double) size) / log(2.0))))) {
         array = new int[size + 1];
         for (int i = 1; i <= size; i++) {
             array[i] = 0;
