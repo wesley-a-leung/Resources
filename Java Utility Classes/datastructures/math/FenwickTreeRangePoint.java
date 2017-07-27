1c192f0fe4cf036c6d5184bbec2a705e33c3f164
wesley-a-leung <wesley.a.leung@gmail.com>
2017-07-26 22:19:53 -0400

    FenwickTree Range Updates and Queries

--- Java Utility Classes/datastructures/math/FenwickTreeRangePoint.java
+++ Java Utility Classes/datastructures/math/FenwickTreeRangePoint.java
@@ -1,21 +1,21 @@
 package datastructures.math;
 
 /**
- * FenwickTree supporting point updates and range queries
+ * FenwickTree supporting range updates and point queries
  * Memory usage:  O(n)
  *
  * @author Wesley Leung
  */
-public class FenwickTree {
+public class FenwickTreeRangePoint {
 
     private int[] array; // 1-indexed array, In this array We save cumulative information to perform efficient range queries and updates
 
-    public FenwickTree(int size) {
+    public FenwickTreeRangePoint(int size) {
         array = new int[size + 1];
     }
 
     /**
-     * Range Sum query from 1 to ind
+     * Range Sum query at index ind
      * ind is 1-indexed
      * <p>
      * Time-Complexity:    O(log(n))
@@ -32,21 +32,6 @@ public class FenwickTree {
     }
 
     /**
-     * Range Sum Query from a to b.
-     * Search for the sum from array index from a to b
-     * a and b are 1-indexed
-     * <p>
-     * Time-Complexity:    O(log(n))
-     *
-     * @param  a left index
-     * @param  b right index
-     * @return sum
-     */
-    public int rsq(int a, int b) {
-        return rsq(b) - rsq(a - 1);
-    }
-
-    /**
      * Update the array at ind and all the affected regions above ind.
      * ind is 1-indexed
      * <p>
@@ -59,6 +44,20 @@ public class FenwickTree {
         for (int x = ind; x < array.length; x += (x & -x)) {
             array[x] += value;
         }
+    }
+    
+    /**
+     * Update the array between a and b.
+     * ind is 1-indexed
+     * <p>
+     * Time-Complexity:    O(log(n))
+     *
+     * @param  ind   index
+     * @param  value value
+     */
+    public void update(int a, int b, int value) {
+        update(a, value);
+        update(b + 1, -value);
     }
 
     public int size() {
