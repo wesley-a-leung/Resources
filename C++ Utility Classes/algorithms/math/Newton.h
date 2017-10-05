0baa44d650b7131b418329290fef1985183bc654
wesley-a-leung <wesley.a.leung@gmail.com>
2017-10-04 23:56:28 -0400

    Updated Newton

--- C++ Utility Classes/algorithms/math/Newton.h
+++ C++ Utility Classes/algorithms/math/Newton.h
@@ -20,12 +20,9 @@ double newton(double (*f)(double), double (*df)(double), double x0) {
 // Finds a root of function f with derivative df based on initial guess x0 by
 // repeating newton's approximation a specified number of times
 double newton(double (*f)(double), double (*df)(double), double x0, int iterations) {
-    double cur = x0, next = x0;
-    while (iterations--) {
-        cur = next;
-        next = cur - f(cur) / df(cur);
-    }
-    return next;
+    double cur = x0;
+    while (iterations--) cur = cur - f(cur) / df(cur);
+    return cur;
 }
 
 #endif /* ALGORITHMS_MATH_NEWTON_H_ */
