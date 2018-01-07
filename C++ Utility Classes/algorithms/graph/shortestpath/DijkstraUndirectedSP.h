64734c400df8d714138663eebf7952bb2ced39fa
wesley-a-leung <wesley.a.leung@gmail.com>
2018-01-06 19:57:01 -0500

    Changed <= to <

--- C++ Utility Classes/algorithms/graph/shortestpath/DijkstraUndirectedSP.h
+++ C++ Utility Classes/algorithms/graph/shortestpath/DijkstraUndirectedSP.h
@@ -8,7 +8,7 @@ using namespace std;
 
 typedef double unit;
 const unit INF = numeric_limits<unit>::infinity();
-// rules for INF (infinity): (length of the longest path + length of greatest weight) <= INF
+// rules for INF (infinity): (length of the longest path + length of greatest weight) < INF
 
 unit *distTo;
 WeightedEdge **edgeTo;
