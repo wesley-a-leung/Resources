5a2be782817f80b0dca7d32f914d174321f760d5
wesley-a-leung <wesley.a.leung@gmail.com>
2018-03-07 18:50:18 -0500

    Ternary serach for integral values

--- C++ Utility Classes/algorithms/math/TernarySearch.h
+++ C++ Utility Classes/algorithms/math/TernarySearch.h
@@ -27,4 +27,22 @@ pair<double, double> ternary_search(double (*f)(double), double lo, double hi) {
     return make_pair(m1, f(m1));
 }
 
+/**
+ * Performs ternary search to find the minimum of a function at integral values.
+ *
+ * @param f the function
+ * @param lo the starting x value
+ * @param hi the ending x value
+ * @returns a pair containing the x value at the minimum of the function, and the minimum value of the function.
+ */
+pair<long long, long long> ternary_search(long long (*f)(long long), long long lo, long long hi) {
+    long long mid;
+    while (lo < hi) {
+        mid = lo + (hi - lo) / 2;
+        if (f(mid) < f(mid + 1)) hi = mid; // change to f(mid) < f(mid + 1) for minimum, f(mid) > f(mid + 1) for maximum
+        else lo = mid + 1;
+    }
+    return make_pair(lo, f(lo));
+}
+
 #endif /* ALGORITHMS_MATH_TERNARYSEARCH_H_ */
