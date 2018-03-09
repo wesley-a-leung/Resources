d81546d0d4d847889279e6b7b7f33fcad96d4262
wesley-a-leung <wesley.a.leung@gmail.com>
2018-03-09 02:07:54 -0500

    Improvements to SqrtArray

--- C++ Utility Classes/datastructures/SqrtArray.h
+++ C++ Utility Classes/datastructures/SqrtArray.h
@@ -10,8 +10,8 @@ using namespace std;
  *
  * Insert: O(sqrt(N))
  * Erase: O(sqrt(N))
- * At: O(sqrt(N))
- * Lower Bound, Upper Bound: O(sqrt(N))
+ * At, Accessor: O(log(N))
+ * Lower Bound, Upper Bound: O(log(N))
  * Size: O(1)
  */
 template <typename Value, typename Comparator = less<Value>>
@@ -20,6 +20,13 @@ private:
     Comparator cmp;
     int n;
     vector<vector<Value>> a;
+    vector<int> prefixSZ;
+
+    void updateSZ() {
+        for (int i = 1; i < (int) a.size(); i++) {
+            prefixSZ[i] = prefixSZ[i - 1] + (int) a[i - 1].size();
+        }
+    }
 
 public:
     SqrtArray() : n(0) {}
@@ -28,7 +35,9 @@ public:
         int sqrtn = (int) sqrt(n);
         for (int i = n; i > 0; i -= sqrtn) {
             a.push_back(vector<Value>(min(i, sqrtn)));
+            prefixSZ.push_back(0);
         }
+        updateSZ();
     }
 
     template <typename It>
@@ -37,42 +46,73 @@ public:
         int sqrtn = (int) sqrt(n);
         for (It i = en; i > st; i -= sqrtn) {
             a.push_back(vector<Value>(i - min((int) (i - st), sqrtn), i));
+            prefixSZ.push_back(0);
         }
+        updateSZ();
         reverse(a.begin(), a.end());
     }
 
     void insert(int k, const Value val) {
-        if (n++ == 0) a.push_back({});
-        int i;
-        for (i = 0; i < (int) a.size() && k >= (int) a[i].size(); k -= (int) a[i++].size());
-        if (i == (int) a.size()) a[--i].push_back(val);
+        if (n++ == 0) {
+            a.push_back({});
+            prefixSZ.push_back(0);
+        }
+        int lo = 0, hi = (int) (a.size()) - 1, mid;
+        while (lo <= hi) {
+            mid = lo + (hi - lo) / 2;
+            if (k < prefixSZ[mid]) hi = mid - 1;
+            else lo = mid + 1;
+        }
+        int i = hi;
+        k -= prefixSZ[hi];
+        if (i == -1) a[i += (int) a.size()].push_back(val);
         else a[i].insert(a[i].begin() + k, val);
         int sqrtn = (int) sqrt(n);
         if ((int) a[i].size() > 2 * sqrtn) {
-            vector<Value> y(a[i].begin() + sqrtn, a[i].end());
+            vector<int> y(a[i].begin() + sqrtn, a[i].end());
             a[i].resize(sqrtn);
             a.insert(a.begin() + i + 1, y);
+            prefixSZ.push_back(0);
         }
+        updateSZ();
     }
 
     void erase(int k) {
         --n;
-        int i;
-        for (i = 0; k >= (int) a[i].size(); k -= (int) a[i++].size());
+        int lo = 0, hi = (int) (a.size()) - 1, mid;
+        while (lo <= hi) {
+            mid = lo + (hi - lo) / 2;
+            if (k < prefixSZ[mid]) hi = mid - 1;
+            else lo = mid + 1;
+        }
+        int i = hi;
+        k -= prefixSZ[hi];
         a[i].erase(a[i].begin() + k);
-        if (a[i].empty()) a.erase(a.begin() + i);
+        if (a[i].empty()) {
+            a.erase(a.begin() + i);
+            prefixSZ.pop_back();
+        }
+        updateSZ();
     }
 
     Value &operator [](int k) {
-        int i;
-        for (i = 0; k >= (int) a[i].size(); k -= (int) a[i++].size());
-        return a[i][k];
+        int lo = 0, hi = (int) (a.size()) - 1, mid;
+        while (lo <= hi) {
+            mid = lo + (hi - lo) / 2;
+            if (k < prefixSZ[mid]) hi = mid - 1;
+            else lo = mid + 1;
+        }
+        return a[hi][k - prefixSZ[hi]];
     }
 
     Value at(int k) const {
-        int i;
-        for (i = 0; k >= (int) a[i].size(); k -= (int) a[i++].size());
-        return a[i][k];
+        int lo = 0, hi = (int) (a.size()) - 1, mid;
+        while (lo <= hi) {
+            mid = lo + (hi - lo) / 2;
+            if (k < prefixSZ[mid]) hi = mid - 1;
+            else lo = mid + 1;
+        }
+        return a[hi][k - prefixSZ[hi]];
     }
 
     Value operator [](int k) const {
@@ -80,19 +120,39 @@ public:
     }
 
     pair<int, Value> lower_bound(const Value val) const {
-        int i, j, k;
-        for (i = 0, k = 0; i < (int) a.size() && cmp(a[i].back(), val); k += a[i++].size());
-        if (i == (int) a.size()) return {n, 0};
-        for (j = 0; cmp(a[i][j], val); j++, k++);
-        return {k, a[i][j]};
+        int lo = 0, hi = (int) a.size(), mid;
+        while (lo < hi) {
+            mid = lo + (hi - lo) / 2;
+            if (cmp(a[mid].back(), val)) lo = mid + 1;
+            else hi = mid;
+        }
+        if (lo == (int) a.size()) return {n, 0};
+        int i = lo;
+        lo = 0, hi = (int) a[i].size();
+        while (lo < hi) {
+            mid = lo + (hi - lo) / 2;
+            if (cmp(a[i][mid], val)) lo = mid + 1;
+            else hi = mid;
+        }
+        return {prefixSZ[i] + lo, a[i][lo]};
     }
 
     pair<int, Value> upper_bound(const Value val) const {
-        int i, j, k;
-        for (i = 0, k = 0; i < (int) a.size() && !cmp(val, a[i].back()); k += a[i++].size());
-        if (i == (int) a.size()) return {n, 0};
-        for (j = 0; !cmp(val, a[i][j]); j++, k++);
-        return {k, a[i][j]};
+        int lo = 0, hi = (int) a.size(), mid;
+        while (lo < hi) {
+            mid = lo + (hi - lo) / 2;
+            if (cmp(val, a[mid].back())) hi = mid;
+            else lo = mid + 1;
+        }
+        if (lo == (int) a.size()) return {n, 0};
+        int i = lo;
+        lo = 0, hi = (int) a[i].size();
+        while (lo < hi) {
+            mid = lo + (hi - lo) / 2;
+            if (cmp(val, a[i][mid])) hi = mid;
+            else lo = mid + 1;
+        }
+        return {prefixSZ[i] + lo, a[i][lo]};
     }
 
     int size() const {
