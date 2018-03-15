41a6a1fd36d68f3c115899a748deb4adc81b2d79
wesley-a-leung <wesley.a.leung@gmail.com>
2018-03-14 22:30:04 -0400

    Improved PrimMST

--- C++ Utility Classes/algorithms/graph/mst/PrimMST.h
+++ C++ Utility Classes/algorithms/graph/mst/PrimMST.h
@@ -6,62 +6,17 @@
 
 using namespace std;
 
-/**
- *  The {@code PrimMST} class represents a data type for computing a
- *  <em>minimum spanning tree</em> in an edge-weighted graph.
- *  The edge weights can be positive, zero, or negative and need not
- *  be distinct. If the graph is not connected, it computes a <em>minimum
- *  spanning forest</em>, which is the union of minimum spanning trees
- *  in each connected component. The {@code weight()} method returns the
- *  weight of a minimum spanning tree and the {@code edges()} method
- *  returns its edges.
- *  <p>
- *  This implementation uses a lazy version of <em>Prim's algorithm</em>
- *  with a binary heap of edges.
- *  The constructor takes time proportional to <em>E</em> log <em>E</em>
- *  and extra space (not including the graph) proportional to <em>E</em>,
- *  where <em>V</em> is the number of vertices and <em>E</em> is the number of edges.
- *  Afterwards, the {@code weight()} method takes constant time
- *  and the {@code edges()} method takes time proportional to <em>V</em>.
- *  <p>
- *  For additional documentation,
- *  see <a href="http://algs4.cs.princeton.edu/43mst">Section 4.3</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- *  For alternate implementations, see {@link KruskalMST}, and {@link BoruvkaMST}.
- *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
- */
 typedef double unit;
+const unit INF = numeric_limits<unit>::infinity();
+
 class PrimMST {
 private:
-    unit weight;       // total weight of MST
-    vector<WeightedEdge*> mst;     // edges in the MST
-    bool *marked;    // marked[v] = true if v on tree
-    priority_queue<WeightedEdge*, vector<WeightedEdge*>, WeightedEdge_greater> PQ;      // edges with one endpoint in tree
-
-    // add all edges e incident to v onto PQ if the other endpoint has not yet been scanned
-    void scan(WeightedGraph *G, int v) {
-        marked[v] = true;
-        for (WeightedEdge *e : G->adj(v)) {
-            if (!marked[e->other(v)]) PQ.push(e);
-        }
-    }
-
-    // run Prim's algorithm
-    void prim(WeightedGraph *G, int s) {
-        scan(G, s);
-        while (!PQ.empty()) {                          // better to stop when mst has V-1 edges
-            WeightedEdge *e = PQ.top();                // smallest edge on PQ
-            PQ.pop();
-            int v = e->either(), w = e->other(v);      // two endpoints
-            if (marked[v] && marked[w]) continue;      // lazy, both v and w already scanned
-            mst.push_back(e);                          // add e to MST
-            weight += e->getWeight();
-            if (!marked[v]) scan(G, v);                // v becomes part of tree
-            if (!marked[w]) scan(G, w);                // w becomes part of tree
-        }
-    }
+    unit weight;
+    vector<WeightedEdge*> mst;
+    bool *marked;
+    unit *cost;
+    WeightedEdge **edgeTo;
+    priority_queue<pair<unit, int>, vector<pair<unit, int>>, greater<pair<unit, int>>> PQ;
 
 public:
     /**
@@ -71,11 +26,35 @@ public:
     PrimMST(WeightedGraph *G) {
         weight = (unit) 0;
         marked = new bool[G->getV()];
+        cost = new unit[G->getV()];
+        edgeTo = new WeightedEdge*[G->getV()];
         for (int v = 0; v < G->getV(); v++) {
             marked[v] = false;
+            cost[v] = INF;
+            edgeTo[v] = nullptr;
         }
-        for (int v = 0; v < G->getV(); v++) {     // run Prim from all vertices to
-            if (!marked[v]) prim(G, v);           // get a minimum spanning forest
+        for (int s = 0; s < G->getV(); s++) {
+            if (marked[s]) continue;
+            cost[s] = 0;
+            PQ.push({cost[s], s});
+            while (!PQ.empty()) {
+                int v = PQ.top().second;
+                PQ.pop();
+                marked[v] = true;
+                for (WeightedEdge *e : G->adj(v)) {
+                    int w = e->other(v);
+                    if (marked[w] || e->getWeight() >= cost[w]) continue;
+                    cost[w] = e->getWeight();
+                    edgeTo[w] = e;
+                    PQ.push({cost[w], w});
+                }
+            }
+        }
+        for (int v = 0; v < G->getV(); v++) {
+            if (edgeTo[v] == nullptr) {
+                mst.push_back(edgeTo[v]);
+                weight += edgeTo[v]->getWeight();
+            }
         }
     }
 
