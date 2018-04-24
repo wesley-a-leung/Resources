5b3f20ebc8fb46b519abda7f540ec89cd2b48c7d
wesley-a-leung <wesley.a.leung@gmail.com>
2018-04-24 03:07:13 -0400

    Removed whitespace in Reader

--- Java Utility Classes/utils/Reader.java
+++ Java Utility Classes/utils/Reader.java
@@ -13,13 +13,9 @@ public class Reader {
     private BufferedReader in;
     private StringTokenizer st;
 
-    public Reader(InputStream inputStream) {
-        in = new BufferedReader(new InputStreamReader(inputStream));
-    }
+    public Reader(InputStream inputStream) { in = new BufferedReader(new InputStreamReader(inputStream)); }
 
-    public Reader(String fileName) throws FileNotFoundException {
-        in = new BufferedReader(new FileReader(fileName));
-    }
+    public Reader(String fileName) throws FileNotFoundException { in = new BufferedReader(new FileReader(fileName)); }
 
     public String next() throws IOException {
         while (st == null || !st.hasMoreTokens()) {
@@ -34,69 +30,26 @@ public class Reader {
         }
         return st.nextToken(delim);
     }
-
-    public BigInteger nextBigInteger() throws IOException {
-        return new BigInteger(next());
-    }
-
-    public byte nextByte() throws IOException {
-        return Byte.parseByte(next());
-    }
-
-    public byte nextByte(String delim) throws IOException {
-        return Byte.parseByte(next(delim));
-    }
-
-    public char nextChar() throws IOException {
-        return next().charAt(0);
-    }
-
-    public char nextChar(String delim) throws IOException {
-        return next(delim).charAt(0);
-    }
-
-    public double nextDouble() throws IOException {
-        return Double.parseDouble(next());
-    }
-
-    public double nextDouble(String delim) throws IOException {
-        return Double.parseDouble(next(delim));
-    }
-
-    public float nextFloat() throws IOException {
-        return Float.parseFloat(next());
-    }
-
-    public float nextFloat(String delim) throws IOException {
-        return Float.parseFloat(next(delim));
-    }
-
-    public int nextInt() throws IOException {
-        return Integer.parseInt(next());
-    }
-
-    public int nextInt(String delim) throws IOException {
-        return Integer.parseInt(next(delim));
-    }
-
-    public long nextLong() throws IOException {
-        return Long.parseLong(next());
-    }
-
-    public long nextLong(String delim) throws IOException {
-        return Long.parseLong(next(delim));
-    }
-
-    public short nextShort() throws IOException {
-        return Short.parseShort(next());
-    }
-
-    public short nextShort(String delim) throws IOException {
-        return Short.parseShort(next(delim));
-    }
-
+    
     public String nextLine() throws IOException {
         st = null;
         return in.readLine();
     }
+
+    // public BigInteger nextBigInteger() throws IOException { return new BigInteger(next()); }
+
+    public byte nextByte() throws IOException { return Byte.parseByte(next()); }
+    public byte nextByte(String delim) throws IOException { return Byte.parseByte(next(delim)); }
+    public char nextChar() throws IOException { return next().charAt(0); }
+    public char nextChar(String delim) throws IOException { return next(delim).charAt(0); }
+    public double nextDouble() throws IOException { return Double.parseDouble(next()); }
+    public double nextDouble(String delim) throws IOException { return Double.parseDouble(next(delim)); }
+    public float nextFloat() throws IOException { return Float.parseFloat(next()); }
+    public float nextFloat(String delim) throws IOException { return Float.parseFloat(next(delim)); }
+    public int nextInt() throws IOException { return Integer.parseInt(next()); }
+    public int nextInt(String delim) throws IOException { return Integer.parseInt(next(delim)); }
+    public long nextLong() throws IOException { return Long.parseLong(next()); }
+    public long nextLong(String delim) throws IOException { return Long.parseLong(next(delim)); }
+    public short nextShort() throws IOException { return Short.parseShort(next()); }
+    public short nextShort(String delim) throws IOException { return Short.parseShort(next(delim)); }
 } // Reader class
