f5f56911b4223c6ceb49cd16f024bf35243fdb5b
wesley-a-leung <wesley.a.leung@gmail.com>
2018-04-23 23:06:50 -0400

    Removed slow KMP

--- Java Utility Classes/algorithms/string/KMPStringSearch.java
+++ Java Utility Classes/algorithms/string/KMPStringSearch.java
@@ -6,17 +6,15 @@ package algorithms.string;
  *  <p>
  *  This implementation uses a version of the Knuth-Morris-Pratt substring search
  *  algorithm. The version takes time and space proportional to
- *  <em>N</em> + <em>M R</em> in the worst case, where <em>N</em> is the length
- *  of the text string, <em>M</em> is the length of the pattern, and <em>R</em>
- *  is the alphabet size.
+ *  <em>N</em> + <em>M</em> in the worst case, where <em>N</em> is the length
+ *  of the text string and <em>M</em> is the length of the pattern.
  *  <p>
  *  For additional documentation,
  *  see <a href="http://algs4.cs.princeton.edu/53substring">Section 5.3</a> of
  *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
  */
 public class KMPStringSearch {
-    private final int R;       // the radix
-    private int[][] dfa;       // the KMP automoton
+    private int[] LCP;
 
     private char[] pattern;    // either the character array for the pattern
     private String pat;        // or the pattern string
@@ -27,43 +25,32 @@ public class KMPStringSearch {
      * @param pat the pattern string
      */
     public KMPStringSearch(String pat) {
-        this.R = 256;
         this.pat = pat;
-
-        // build DFA from pattern
         int m = pat.length();
-        dfa = new int[R][m]; 
-        dfa[pat.charAt(0)][0] = 1; 
-        for (int x = 0, j = 1; j < m; j++) {
-            for (int c = 0; c < R; c++) 
-                dfa[c][j] = dfa[c][x];     // Copy mismatch cases. 
-            dfa[pat.charAt(j)][j] = j+1;   // Set match case. 
-            x = dfa[pat.charAt(j)][x];     // Update restart state. 
-        } 
+        LCP = new int[m + 1];
+        LCP[0] = -1;
+        for (int i = 0, j = -1; i < m; i++, j++, LCP[i] = j) {
+            while (j >= 0 && pat.charAt(i) != pat.charAt(j)) {
+                j = LCP[j];
+            }
+        }
     } 
 
     /**
      * Preprocesses the pattern string.
      *
      * @param pattern the pattern string
-     * @param R the alphabet size
      */
-    public KMPStringSearch(char[] pattern, int R) {
-        this.R = R;
-        this.pattern = new char[pattern.length];
-        for (int j = 0; j < pattern.length; j++)
-            this.pattern[j] = pattern[j];
-
-        // build DFA from pattern
+    public KMPStringSearch(char[] pattern) {
+        this.pattern = pattern;
         int m = pattern.length;
-        dfa = new int[R][m]; 
-        dfa[pattern[0]][0] = 1; 
-        for (int x = 0, j = 1; j < m; j++) {
-            for (int c = 0; c < R; c++) 
-                dfa[c][j] = dfa[c][x];     // Copy mismatch cases. 
-            dfa[pattern[j]][j] = j+1;      // Set match case. 
-            x = dfa[pattern[j]][x];        // Update restart state. 
-        } 
+        LCP = new int[m + 1];
+        LCP[0] = -1;
+        for (int i = 0, j = -1; i < m; i++, j++, LCP[i] = j) {
+            while (j >= 0 && pattern[i] != pattern[j]) {
+                j = LCP[j];
+            }
+        }
     } 
 
     /**
@@ -75,16 +62,17 @@ public class KMPStringSearch {
      *         in the text string; -1 if no such match
      */
     public int search(String txt) {
-
-        // simulate operation of DFA on text
         int m = pat.length();
         int n = txt.length();
-        int i, j;
-        for (i = 0, j = 0; i < n && j < m; i++) {
-            j = dfa[txt.charAt(i)][j];
+        for (int i = 0, j = 0; i < n; i++, j++) {
+            while (j >= 0 && txt.charAt(i) != pat.charAt(j)) {
+                j = LCP[j];
+            }
+            if (j == m - 1) {
+                return i - j;
+            }
         }
-        if (j == m) return i - m;    // found
-        return -1;                   // not found
+        return -1;
     }
 
     /**
@@ -96,15 +84,16 @@ public class KMPStringSearch {
      *         in the text string; -1 if no such match
      */
     public int search(char[] text) {
-
-        // simulate operation of DFA on text
         int m = pattern.length;
         int n = text.length;
-        int i, j;
-        for (i = 0, j = 0; i < n && j < m; i++) {
-            j = dfa[text[i]][j];
+        for (int i = 0, j = 0; i < n; i++, j++) {
+            while (j >= 0 && text[i] != pattern[j]) {
+                j = LCP[j];
+            }
+            if (j == m - 1) {
+                return i - j;
+            }
         }
-        if (j == m) return i - m;    // found
-        return -1;                   // not found
+        return -1;
     }
 }
