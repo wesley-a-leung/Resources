718d3886bf4cb61a995f8d0d90fd98eccf39b421
wesley-a-leung <wesley.a.leung@gmail.com>
2018-06-09 22:55:09 -0400

    Changed Fast Reader

--- Java Utility Classes/utils/FastReader.java
+++ Java Utility Classes/utils/FastReader.java
@@ -26,54 +26,62 @@ public class FastReader {
 
     public byte nextByte() throws IOException {
         byte ret = 0;
-        byte c = read();
-        while (c <= ' ') c = read();
+        byte c;
+        do {
+            c = read();
+        } while (c <= ' ');
         boolean neg = (c == '-');
         if (neg) c = read();
         do {
             ret *= 10;
             ret += c - '0';
-        } while ((c = read()) >= '0' && c <= '9');
+        } while ((c = read()) >= '0');
         if (neg) return (byte) (-ret);
         return ret;
     }
     
     public short nextShort() throws IOException {
         short ret = 0;
-        byte c = read();
-        while (c <= ' ') c = read();
+        byte c;
+        do {
+            c = read();
+        } while (c <= ' ');
         boolean neg = (c == '-');
         if (neg) c = read();
         do {
             ret *= 10;
             ret += c - '0';
-        } while ((c = read()) >= '0' && c <= '9');
+        } while ((c = read()) >= '0');
         if (neg) return (short) (-ret);
         return ret;
     }
 
     public int nextInt() throws IOException {
         int ret = 0;
-        byte c = read();
-        while (c <= ' ') c = read();
+        byte c;
+        do {
+            c = read();
+        } while (c <= ' ');
         boolean neg = (c == '-');
         if (neg) c = read();
         do {
             ret = ret * 10 + c - '0';
-        } while ((c = read()) >= '0' && c <= '9');
+        } while ((c = read()) >= '0');
         if (neg) return -ret;
         return ret;
     }
 
     public long nextLong() throws IOException {
         long ret = 0;
-        byte c = read();
-        while (c <= ' ') c = read();
+        byte c;
+        do {
+            c = read();
+        } while (c <= ' ');
         boolean neg = (c == '-');
         if (neg) c = read();
         do {
             ret = ret * 10 + c - '0';
-        } while ((c = read()) >= '0' && c <= '9');
+        } while ((c = read()) >= '0');
         if (neg) return -ret;
         return ret;
     }
@@ -86,22 +94,24 @@ public class FastReader {
         if (neg) c = read();
         do {
             ret = ret * 10 + c - '0';
-        } while ((c = read()) >= '0' && c <= '9');
-        if (c == '.') while ((c = read()) >= '0' && c <= '9') ret += (c - '0') / (div *= 10);
+        } while ((c = read()) >= '0');
+        if (c == '.') while ((c = read()) >= '0') ret += (c - '0') / (div *= 10);
         if (neg) return -ret;
         return ret;
     }
 
     public double nextDouble() throws IOException {
         double ret = 0, div = 1;
-        byte c = read();
-        while (c <= ' ') c = read();
+        byte c;
+        do {
+            c = read();
+        } while (c <= ' ');
         boolean neg = (c == '-');
         if (neg) c = read();
         do {
             ret = ret * 10 + c - '0';
-        } while ((c = read()) >= '0' && c <= '9');
-        if (c == '.') while ((c = read()) >= '0' && c <= '9') ret += (c - '0') / (div *= 10);
+        } while ((c = read()) >= '0');
+        if (c == '.') while ((c = read()) >= '0') ret += (c - '0') / (div *= 10);
         if (neg) return -ret;
         return ret;
     }
@@ -110,7 +120,7 @@ public class FastReader {
         byte c;
         do {
             c = read();
-        } while (c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1);
+        } while (c <= ' ');
         return (char) c;
     }
 
@@ -119,12 +129,10 @@ public class FastReader {
         int cnt = 0, c;
         do {
             c = read();
-        } while (c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1);
-        buf[cnt++] = (byte) c;
-        while ((c = read()) != -1) {
-            if (c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1) break;
+        } while (c <= ' ');
+        do {
             buf[cnt++] = (byte) c;
-        }
+        } while ((c = read()) > ' ');
         return new String(buf, 0, cnt);
     }
 
@@ -133,12 +141,10 @@ public class FastReader {
         int cnt = 0, c;
         do {
             c = read();
-        } while (c == ' ' || c == '\n' || c == '\r' || c == -1);
-        buf[cnt++] = (byte) c;
-        while ((c = read()) != -1) {
-            if (c == '\n' || c == '\r' || c == -1) break;
+        } while (c <= ' ');
+        do {
             buf[cnt++] = (byte) c;
-        }
+        } while ((c = read()) >= ' ');
         return new String(buf, 0, cnt);
     }
     
