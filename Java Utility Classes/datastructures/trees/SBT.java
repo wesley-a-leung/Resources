598de85908491f04a9b2afd5e8f62d37c65691a7
wesley-a-leung <wesley.a.leung@gmail.com>
2018-06-30 11:52:57 -0400

    Fixed count inversions

--- Java Utility Classes/datastructures/trees/SBT.java
+++ Java Utility Classes/datastructures/trees/SBT.java
@@ -140,7 +140,7 @@ public class SBT<Key extends Comparable<Key>, Value> {
      * @return the subtree
      */
     private Node put(Node x, Key key, Value val) {
-        if (x == null) return new Node(key, val,1);
+        if (x == null) return new Node(key, val, 1);
         int cmp = key.compareTo(x.key);
         if (cmp < 0) x.left = put(x.left, key, val);
         else if (cmp > 0) x.right = put(x.right, key, val);
