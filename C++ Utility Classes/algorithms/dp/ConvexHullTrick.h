b20a4e1ea7516682dd8eed16e3b6b1093fa0df66
wesley-a-leung <wesley.a.leung@gmail.com>
2018-06-30 22:15:35 -0400

    C Resources, C++ templated convex hull trick

--- C++ Utility Classes/algorithms/dp/ConvexHullTrick.h
+++ C++ Utility Classes/algorithms/dp/ConvexHullTrick.h
@@ -4,22 +4,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// convention is same as priority_queue in STL
+template <typename T, typename Comparator = less<T>>
 class ConvexHullTrick {
 private:
-    vector<long long> M, B;
+    Comparator cmp;
+    vector<T> M, B;
     int front, back;
 
 public:
     ConvexHullTrick(int N) : M(N), B(N), front(0), back(0) {}
 
-    void addLine(long long m, long long b) {
-        while (back >= 2 && (B[back - 2] - B[back - 1]) * (m - M[back - 1]) >= (B[back - 1] - b) * (M[back - 1] - M[back - 2])) back--;
+    void addLine(T m, T b) {
+        while (back >= 2 && !cmp((B[back - 2] - B[back - 1]) * (m - M[back - 1]), (B[back - 1] - b) * (M[back - 1] - M[back - 2]))) back--;
         M[back] = m;
         B[back++] = b;
     }
 
-    long long getMax(long long x) {
-        while (front < back - 1 && M[front + 1] * x + B[front + 1] >= M[front] * x + B[front]) front++;
+    T getMax(T x) {
+        while (front < back - 1 && !cmp(M[front + 1] * x + B[front + 1], M[front] * x + B[front])) front++;
         return M[front] * x + B[front];
     }
 };
