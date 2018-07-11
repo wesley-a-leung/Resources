ca4118746f5705f51bb8e2e4c211d30951c438a8
Wesley Leung <wesley.a.leung@gmail.com>
2018-07-11 14:19:51 -0400

    Fixed Convex Hull Trick Comments

--- C++ Utility Classes/algorithms/dp/ConvexHullTrick.h
+++ C++ Utility Classes/algorithms/dp/ConvexHullTrick.h
@@ -15,14 +15,14 @@ private:
 public:
     ConvexHullTrick(int N) : M(N), B(N), front(0), back(0) {}
 
-    // in non descending order of slope, as sorted by the comparator
+    // in non decreasing order of slope, as sorted by the comparator
     void addLine(T m, T b) {
         while (back >= 2 && (B[back - 2] - B[back - 1]) * (m - M[back - 1]) >= (B[back - 1] - b) * (M[back - 1] - M[back - 2])) back--;
         M[back] = m;
         B[back++] = b;
     }
 
-    // in non descreading order of x, regardless of comparator
+    // in non decreasing order of x, regardless of comparator
     T getMax(T x) {
         while (front < back - 1 && !cmp(M[front + 1] * x + B[front + 1], M[front] * x + B[front])) front++;
         return M[front] * x + B[front];
