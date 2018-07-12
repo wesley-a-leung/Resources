98fec6b5a5b47434f02611084cebdf4c36154e4d
Wesley Leung <wesley.a.leung@gmail.com>
2018-07-12 15:39:46 -0400

    Added Kotlin Reader

---
+++ Kotlin Utility Classes/utils/Reader.kt
@@ -0,0 +1,29 @@
+import java.io.BufferedReader
+import java.io.FileReader
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+class Reader {
+    private val In: BufferedReader
+    private var st: StringTokenizer? = null
+    constructor(inputStream: InputStream) {
+        In = BufferedReader(InputStreamReader(inputStream))
+    }
+    constructor(fileName: String) {
+        In = BufferedReader(FileReader(fileName))
+    }
+    fun next(): String {
+        while (st == null || !st!!.hasMoreTokens()) st = StringTokenizer(In.readLine().trim())
+        return st!!.nextToken()
+    }
+    fun nextLine(): String {
+        st = null
+        return In.readLine()
+    }
+    fun nextChar(): Char = next()[0]
+    fun nextDouble(): Double = next().toDouble()
+    fun nextInt(): Int = next().toInt()
+    fun nextLong(): Long  = next().toLong()
+    fun close(): Unit = In.close()
+}
