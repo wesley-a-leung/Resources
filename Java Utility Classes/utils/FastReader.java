f53b800bd423fbec043b9773f86f4f08712a9a7d
wesley-a-leung <wesley.a.leung@gmail.com>
2018-07-17 23:49:17 -0400

    Updated Fast Reader

--- Java Utility Classes/utils/FastReader.java
+++ Java Utility Classes/utils/FastReader.java
@@ -108,6 +108,11 @@ public class FastReader {
         LENGTH = length;
     }
 
+    public boolean hasNext() throws IOException {
+        while (peek() != -1 && peek() <= ' ') read();
+        return peek() != -1;
+    }
+
     private void fillBuffer() throws IOException {
         bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
         if (bytesRead == -1) buffer[0] = -1;
@@ -116,6 +121,11 @@ public class FastReader {
     private byte read() throws IOException {
         if (bufferPointer == bytesRead) fillBuffer();
         return buffer[bufferPointer++];
+    }
+
+    private byte peek() throws IOException {
+        if (bufferPointer == bytesRead) fillBuffer();
+        return buffer[bufferPointer];
     }
 
     public void close() throws IOException {
