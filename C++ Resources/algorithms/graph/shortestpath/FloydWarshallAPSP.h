3cbf7eaa6502b8a32446036d4ff362b4a595d6a8
wesley-a-leung <wesley.a.leung@gmail.com>
2018-12-29 10:58:55 -0500

    Floyd Warshall with Path Reconstruction

--- C++ Resources/algorithms/graph/shortestpath/FloydWarshallAPSP.h
+++ C++ Resources/algorithms/graph/shortestpath/FloydWarshallAPSP.h
@@ -7,15 +7,15 @@ using namespace std;
 // Time Complexity: O(V^3)
 // Memory Complexity: O(V^2)
 template <const int MAXV, class unit> struct FloydWarshallAPSP {
-    unit INF, dist[MAXV][MAXV]; bool hasNegativeCycle; FloydWarshallAPSP(unit INF) : INF(INF) {}
-    void init(int V = MAXV) { for (int i = 0; i < V; i++) fill(dist[i], dist[i] + V, INF); }
-    void addEdge(int v, int w, unit weight) { dist[v][w] = min(dist[v][w], weight); }
+    unit INF, dist[MAXV][MAXV]; int nxt[MAXV][MAXV]; bool hasNegativeCycle; FloydWarshallAPSP(unit INF) : INF(INF) {}
+    void init(int V = MAXV) { for (int i = 0; i < V; i++) { fill(dist[i], dist[i] + V, INF); fill(nxt[i], nxt[i] + V, -1); } }
+    void addEdge(int v, int w, unit weight) { dist[v][w] = min(dist[v][w], weight); nxt[v][w] = w; }
     void addBiEdge(int v, int w, unit weight) { addEdge(v, w, weight); addEdge(w, v, weight); }
     void run(int V) {
         hasNegativeCycle = false;
         for (int v = 0; v < V; v++) dist[v][v] = 0;
         for (int u = 0; u < V; u++) for (int v = 0; v < V; v++) if (dist[v][u] < INF) for (int w = 0; w < V; w++)
-            if (dist[u][w] < INF && dist[v][w] > dist[v][u] + dist[u][w]) dist[v][w] = dist[v][u] + dist[u][w]; 
+            if (dist[u][w] < INF && dist[v][w] > dist[v][u] + dist[u][w]) { dist[v][w] = dist[v][u] + dist[u][w]; nxt[v][w] = nxt[v][u]; }
         for (int u = 0; u < V; u++) for (int v = 0; v < V; v++) for (int w = 0; w < V; w++)
             if (dist[w][w] < 0 && dist[u][w] < INF && dist[w][v] < INF) { dist[u][v] = -INF; hasNegativeCycle = true; break; }
     }
