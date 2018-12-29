d0c1d8b804f98f896811842415acd4080296eddc
wesley-a-leung <wesley.a.leung@gmail.com>
2018-12-29 08:36:45 -0500

    Fixed Ternary Search

--- C++ Resources/algorithms/math/TernarySearch.h
+++ C++ Resources/algorithms/math/TernarySearch.h
@@ -4,7 +4,7 @@ using namespace std;
 
 // Ternary search to find the minimum of a function
 // Time Complexity: O(log((hi - lo) / EPS)) * (cost to compute f(x))
-template <class T, class F> ternary_search(F f, T lo, T hi, T EPS) {
+template <class T, class F> T ternary_search(F f, T lo, T hi, T EPS) {
     T m1, m2;
     do {
         m1 = lo + (hi - lo) / 3; m2 = hi - (hi - lo) / 3;
@@ -16,7 +16,7 @@ template <class T, class F> ternary_search(F f, T lo, T hi, T EPS) {
 
 // Ternary search to find the minimum of a function at integral values
 // Time Complexity: O(log(hi - lo)) * (cost to compute f(x))
-template <class T, class F> ternary_search(F f, T lo, T hi) {
+template <class T, class F> T ternary_search(F f, T lo, T hi) {
     static_assert(is_integral<T>::value, "T must be an integral type"); T mid;
     while (lo < hi) {
         mid = lo + (hi - lo) / 2;
