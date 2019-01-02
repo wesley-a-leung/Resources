c8974dc8437b7db95b1bbba4a497997524481fd1
wesley-a-leung <wesley.a.leung@gmail.com>
2019-01-01 21:56:04 -0500

    Hashing improvements

--- C++ Resources/algorithms/string/Hashing.h
+++ C++ Resources/algorithms/string/Hashing.h
@@ -6,20 +6,20 @@ seed_seq seq {
     (uint64_t)chrono::duration_cast<chrono::nanoseconds>(chrono::high_resolution_clock::now().time_since_epoch()).count(),
     (uint64_t)__builtin_ia32_rdtsc(),(uint64_t)(uintptr_t)make_unique<char>().get()
 };
-mt19937 rng(seq);
+mt19937_64 rng64(seq);
 
 // Computes the hash of a string to allow for easy compuptation of substring hashes
 // Time Complexity:
 //   compute: O(S)
 //   getHash: O(1)
 // Memory Complexity: O(S)
-template <const int MAXN, const long long MOD> struct Hashing {
-    long long H[MAXN], POW[MAXN], BASE = uniform_int_distribution<int>(MOD / 2, MOD - 1)(rng);
+template <const int MAXN, class T, const T MOD> struct Hashing {
+    T H[MAXN], POW[MAXN], BASE = uniform_int_distribution<T>(MOD / 2, MOD - 1)(rng64);
     void compute(const string &s) {
         int N = int(s.length()); POW[0] = 1; H[N] = 0;
         for (int i = 1; i <= N; i++) POW[i] = POW[i - 1] * BASE % MOD;
         for (int i = N - 1; i >= 0; i--) H[i] = (H[i + 1] * BASE + s[i] - 'a' + 1) % MOD;
     }
     // hash for substring [l, r)
-    long long getHash(int l, int r) { return ((H[l] - H[r] * POW[r - l]) % MOD + MOD) % MOD; }
+    T getHash(int l, int r) { return ((H[l] - H[r] * POW[r - l]) % MOD + MOD) % MOD; }
 };
