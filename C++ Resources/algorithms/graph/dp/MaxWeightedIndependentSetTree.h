dd6220a8ab9cdf439f44606005a4c035dbba869b
wesley-a-leung <wesley.a.leung@gmail.com>
2019-01-20 09:13:31 -0500

    MWIS

--- C++ Resources/algorithms/graph/dp/MaxWeightedIndependentSetTree.h
+++ C++ Resources/algorithms/graph/dp/MaxWeightedIndependentSetTree.h
@@ -8,6 +8,7 @@ using namespace std;
 template <const int MAXV> struct MaxWeightedIndependentSet {
     vector<int> adj[MAXV]; int val[MAXV], dp[MAXV][2];
     void addEdge(int v, int w) { adj[v].push_back(w); adj[w].push_back(w); }
+    void addBiEdge(int v, int w) { addEdge(v, w); addEdge(w, v); }
     int dfs(int v, int prev, bool take) {
         if (dp[v][take] != -1) return dp[v][take];
         int ret = INT_MIN, cnt = 0;
