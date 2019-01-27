cd90e274dd0c412009e6301edb79b2792e94aaf7
wesley-a-leung <wesley.a.leung@gmail.com>
2019-01-26 20:17:51 -0500

    Removed EPS from ternary search

--- C++ Resources/algorithms/math/TernarySearch.h
+++ C++ Resources/algorithms/math/TernarySearch.h
@@ -3,10 +3,10 @@
 using namespace std;
 
 // Ternary search to find the minimum of a function
-// Time Complexity: O(log((hi - lo) / EPS)) * (cost to compute f(x))
-template <class T, class F> T ternary_search(T lo, T hi, F f, T EPS, int iters) {
+// Time Complexity: O(iters * (cost to compute f(x)))
+template <class T, class F> T ternary_search(T lo, T hi, F f, int iters) {
     T m1, m2;
-    for (int it = 0; it < iters && hi - lo > EPS; it++) {
+    for (int it = 0; it < iters; it++) {
         m1 = lo + (hi - lo) / 3; m2 = hi - (hi - lo) / 3;
         if (f(m1) < f(m2)) hi = m2; // f(m1) < f(m2) for minimum, f(m1) > f(m2) for maximum
         else lo = m1;
