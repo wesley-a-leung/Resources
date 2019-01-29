8e6a6c85c830b2c207e00852787424a0f2781c40
wesley-a-leung <wesley.a.leung@gmail.com>
2019-01-29 02:01:13 -0500

    Hashing Update

--- C++ Resources/datastructures/math/Fraction.h
+++ C++ Resources/datastructures/math/Fraction.h
@@ -3,46 +3,48 @@
 #include "../../algorithms/math/Combinatorics.h"
 using namespace std;
 
+using T = long long;
+
 // Data structure representing a Fraction
-template <class T> struct Fraction {
+struct Fraction {
     T num, den;
     Fraction(T num = 0, T den = 0) : num(num), den(den) {}
-    Fraction<T> operator + (const Fraction<T> &f) const {
-        return Fraction<T>((num * f.den + f.num * den), den * f.den);
+    Fraction operator + (const Fraction &f) const {
+        return Fraction((num * f.den + f.num * den), den * f.den);
     }
-    Fraction<T> &operator += (const Fraction<T> &f) {
+    Fraction &operator += (const Fraction &f) {
         num = (num * f.den + f.num * den); den = den * f.den;
         return *this;
     }
-    Fraction<T> operator - (const Fraction<T> &f) const {
-        return Fraction<T>((num * f.den - f.num * den), den * f.den);
+    Fraction operator - (const Fraction &f) const {
+        return Fraction((num * f.den - f.num * den), den * f.den);
     }
-    Fraction<T> &operator -= (const Fraction<T> &f) {
+    Fraction &operator -= (const Fraction &f) {
         num = (num * f.den - f.num * den); den = den * f.den;
         return *this;
     }
-    Fraction<T> operator * (const Fraction<T> &f) const {
-        return Fraction<T>(num * f.num, den * f.den);
+    Fraction operator * (const Fraction &f) const {
+        return Fraction(num * f.num, den * f.den);
     }
-    Fraction<T> &operator *= (const Fraction<T> &f) {
+    Fraction &operator *= (const Fraction &f) {
         num = num * f.num; den = den * f.den;
         return *this;
     }
-    Fraction<T> operator / (const Fraction<T> &f) const {
-        return Fraction<T>(num * f.den, den * f.num);
+    Fraction operator / (const Fraction &f) const {
+        return Fraction(num * f.den, den * f.num);
     }
-    Fraction<T> &operator /= (const Fraction<T> &f) {
+    Fraction &operator /= (const Fraction &f) {
         T t_num = num * f.den, t_den = den * f.num; num = t_num; den = t_den;
         return *this;
     }
-    Fraction<T> reduce() {
+    Fraction reduce() {
         T g = __gcd(num, den);
-        return Fraction<T>(num / g, den / g);
+        return Fraction(num / g, den / g);
     }
-    bool operator < (const Fraction<T> &f) { return num * f.den < f.num * den; }
-    bool operator <= (const Fraction<T> &f) { return !(f < *this); }
-    bool operator > (const Fraction<T> &f) { return f < *this; }
-    bool operator >= (const Fraction<T> &f) { return !(*this < f); }
-    bool operator == (const Fraction<T> &f) { return !(*this < f) && !(f < *this); }
-    bool operator != (const Fraction<T> &f) { return !(*this == f); }
+    bool operator < (const Fraction &f) const { return num * f.den < f.num * den; }
+    bool operator <= (const Fraction &f) const { return !(f < *this); }
+    bool operator > (const Fraction &f) const { return f < *this; }
+    bool operator >= (const Fraction &f) const { return !(*this < f); }
+    bool operator == (const Fraction &f) const { return !(*this < f) && !(f < *this); }
+    bool operator != (const Fraction &f) const { return !(*this == f); }
 };
