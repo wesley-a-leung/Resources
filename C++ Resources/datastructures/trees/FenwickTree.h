a04c6bcc21909fd52b03e771937320b4a63209c2
wesley-a-leung <wesley.a.leung@gmail.com>
2019-01-30 22:22:17 -0500

    New BIT

--- C++ Resources/datastructures/trees/FenwickTree.h
+++ C++ Resources/datastructures/trees/FenwickTree.h
@@ -2,15 +2,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Fenwick Tree or Binary Indexed Tree supporting point updates and range queries
+// Fenwick Tree or Binary Indexed Tree supporting point updates and range queries in any number of dimensions
 // Time Complexity:
-//   init: O(N)
-//   update, rsq: O(log N)
-// Memory Complexity: O(N)
-template <const int MAXN, class T, const bool ONE_INDEXED> struct FenwickTree {
-    T BIT[MAXN];
-    void init() { fill(BIT, BIT + MAXN, 0); }
-    void update(int i, T v) { for (i += !ONE_INDEXED; i < MAXN; i += i & -i) BIT[i] += v; }
-    T rsq(int i) { T ret = 0; for (i += !ONE_INDEXED; i > 0; i -= i & -i) ret += BIT[i]; return ret; }
-    T rsq(int a, int b) { return rsq(b) - rsq(a - 1); }
+//   init: O(PI(N_i))
+//   update, rsq: O(PI(log(N_i)))
+// Memory Complexity: O(PI(N_i))
+// where PI is the product function, and N_i is the size in the ith dimension
+template <class T, const bool ONE_INDEXED, const int... Args> struct FenwickTree {
+    T val;
+    void init() { val = 0; }
+    void update(int v) { val += v; }
+    T rsq() { return val; }
+};
+
+template <class T, const bool ONE_INDEXED, const int MAXN, const int... Ns> struct FenwickTree <T, ONE_INDEXED, MAXN, Ns...> {
+    FenwickTree<T, ONE_INDEXED, Ns...> BIT[MAXN];
+    void init() { for (int i = 0; i < MAXN; i++) BIT[i].init(); }
+    template <typename... Args> void update(int i, Args... args) { for (i += !ONE_INDEXED; i < MAXN; i += i & -i) BIT[i].update(args...); }
+    template <typename... Args> T rsq(int l, int r, Args... args) {
+        T ret = 0;
+        for (r += !ONE_INDEXED; r > 0; r -= r & -r) ret += BIT[r].rsq(args...);
+        for (l -= ONE_INDEXED; l > 0; l -= l & -l) ret -= BIT[l].rsq(args...);
+        return ret;
+    }
 };
