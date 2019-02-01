1e77558f9942e9e2e86eb7906e44e9a601b370c2
wesley-a-leung <wesley.a.leung@gmail.com>
2019-01-31 23:07:12 -0500

    ND Fenwick Tree

--- C++ Resources/datastructures/trees/FenwickTreeND.h
+++ C++ Resources/datastructures/trees/FenwickTreeND.h
@@ -13,31 +13,28 @@ template <typename T> struct FenwickTreeND {
     T rsq(const vector<int> &ind, int curDim, int pos) {
         T sum = 0;
         if (curDim == n) sum += BIT[pos];
-        else for (int i = ind[curDim]; i > 0; i -= i & -i)
-            sum += rsq(ind, curDim + 1, pos + (i - 1) * sufProd[curDim]);
+        else for (int i = ind[curDim]; i > 0; i -= i & -i) sum += rsq(ind, curDim + 1, pos + (i - 1) * sufProd[curDim]);
         return sum;
     }
     T rsq(const vector<int> &start, const vector<int> &end, int curDim, int pos) {
         T sum = 0;
         if (curDim == n) sum += BIT[pos];
         else {
-            for (int i = end[curDim]; i > 0; i -= i & -i)
-                sum += rsq(start, end, curDim + 1, pos + (i - 1) * sufProd[curDim]);
-            for (int i = start[curDim] - 1; i > 0; i -= i & -i)
-                sum -= rsq(start, end, curDim + 1, pos + (i - 1) * sufProd[curDim]);
+            for (int i = end[curDim]; i > 0; i -= i & -i) sum += rsq(start, end, curDim + 1, pos + (i - 1) * sufProd[curDim]);
+            for (int i = start[curDim] - 1; i > 0; i -= i & -i) sum -= rsq(start, end, curDim + 1, pos + (i - 1) * sufProd[curDim]);
         }
         return sum;
     }
     void update(const vector<int> &ind, int curDim, int pos, T value) {
         if (curDim == n) BIT[pos] += value;
-        else for (int i = ind[curDim]; i <= dim[curDim]; i += i & -i)
-            update(ind, curDim + 1, pos + (i - 1) * sufProd[curDim], value);
+        else for (int i = ind[curDim]; i <= dim[curDim]; i += i & -i) update(ind, curDim + 1, pos + (i - 1) * sufProd[curDim], value);
     }
     FenwickTreeND(const vector<int> &dim) : prod(1), n(dim.size()), dim(dim) {
         for (int i = n - 1; i >= 0; i--) { sufProd.push_back(prod); prod *= dim[i]; }
         reverse(sufProd.begin(), sufProd.end());
         for (int i = 0; i < prod; i++) BIT.push_back(0);
     }
+    // 1-indexed
     T rsq(const vector<int> &ind) { return rsq(ind, 0, 0); }
     T rsq(const vector<int> &start, const vector<int> &end) { return rsq(start, end, 0, 0); }
     void update(const vector<int> &ind, T value) { update(ind, 0, 0, value); }
