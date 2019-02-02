c3d5267819509a7e708b60d63bb7ad3747e3beec
wesley-a-leung <wesley.a.leung@gmail.com>
2019-02-02 01:35:54 -0500

    Moved MaxContiguousSubarrayData

--- C++ Resources/datastructures/trees/BottomUpSegmentTrees.h
+++ C++ Resources/datastructures/trees/BottomUpSegmentTrees.h
@@ -107,20 +107,3 @@ template <const int MAXN, const bool ONE_INDEXED> struct SegmentTree_RAM_RQ {
         return merge(ql, qr);
     }
 };
-
-template <class T> struct MaxContiguousSubarrayData { T pre, suf, sum, maxSum; bool isNull; };
-template <class T> MaxContiguousSubarrayData<T> nullNode() {
-    MaxContiguousSubarrayData<T> ret; ret.pre = ret.suf = ret.sum = ret.maxSum = 0; ret.isNull = true; return ret;
-}
-template <class T> MaxContiguousSubarrayData<T> merge(const MaxContiguousSubarrayData<T> &l, const MaxContiguousSubarrayData<T> &r) {
-    if (l.isNull) return r;
-    if (r.isNull) return l;
-    MaxContiguousSubarrayData<T> ret; ret.isNull = false; ret.pre = max(l.pre, l.sum + r.pre); ret.suf = max(l.suf + r.sum, r.suf);
-    ret.sum = l.sum + r.sum; ret.maxSum = max(max(l.maxSum, r.maxSum), l.suf + r.pre); return ret;
-}
-// range assignments
-template <class T> MaxContiguousSubarrayData<T> applyLazy(const MaxContiguousSubarrayData<T> &x, const T &v) {
-    MaxContiguousSubarrayData<T> ret; ret.pre = ret.suf = ret.sum = ret.maxSum = v; ret.isNull = false; return ret;
-}
-template <class T> T getSegmentVal(const T &l, int len) { return l * len; }
-template <class T> T mergeLazy(const T &l, const T &r) { return r; }
--- C++ Resources/datastructures/trees/TopDownSegmentTrees.h
+++ C++ Resources/datastructures/trees/TopDownSegmentTrees.h
@@ -142,20 +142,3 @@ template <const int MAXNODES, const int MAXROOTS, const bool ONE_INDEXED> struct
     void revert(int x) { roots[curRoot++] = roots[x]; }
     void clear() { curNode = curRoot = 0; }
 };
-
-template <class T> struct MaxContiguousSubarrayData { T pre, suf, sum, maxSum; bool isNull; };
-template <class T> MaxContiguousSubarrayData<T> nullNode() {
-    MaxContiguousSubarrayData<T> ret; ret.pre = ret.suf = ret.sum = ret.maxSum = 0; ret.isNull = true; return ret;
-}
-template <class T> MaxContiguousSubarrayData<T> merge(const MaxContiguousSubarrayData<T> &l, const MaxContiguousSubarrayData<T> &r) {
-    if (l.isNull) return r;
-    if (r.isNull) return l;
-    MaxContiguousSubarrayData<T> ret; ret.isNull = false; ret.pre = max(l.pre, l.sum + r.pre); ret.suf = max(l.suf + r.sum, r.suf);
-    ret.sum = l.sum + r.sum; ret.maxSum = max(max(l.maxSum, r.maxSum), l.suf + r.pre); return ret;
-}
-// range assignments
-template <class T> MaxContiguousSubarrayData<T> applyLazy(const MaxContiguousSubarrayData<T> &x, const T &v) {
-    MaxContiguousSubarrayData<T> ret; ret.pre = ret.suf = ret.sum = ret.maxSum = v; ret.isNull = false; return ret;
-}
-template <class T> T getSegmentVal(const T &l, int len) { return l * len; }
-template <class T> T mergeLazy(const T &l, const T &r) { return r; }
