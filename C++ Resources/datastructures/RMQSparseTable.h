f20b7ec776a4ec09640e4a006fcc061e0a435101
wesley-a-leung <wesley.a.leung@gmail.com>
2019-02-03 00:34:54 -0500

    Sparse Table Changes

--- C++ Resources/datastructures/RMQSparseTable.h
+++ C++ Resources/datastructures/RMQSparseTable.h
@@ -12,11 +12,8 @@ template <const int MAXN, const int MAXLGN, class T> struct RMQSparseTable {
     int minInd(int l, int r) { return A[l] <= A[r] ? l : r; }
     void init(int N) {
         int lg = 32 - __builtin_clz(N); assert(lg < MAXLGN); iota(ST[0], ST[0] + N, 0);
-        for (int i = 0; i < lg - 1; i++) for (int j = 0; j + (1 << i) < N; j++)
-            ST[i + 1][j] = minInd(ST[i][j], ST[i][j + (1 << i)]);
+        for (int i = 0; i < lg - 1; i++) for (int j = 0; j + (1 << i) < N; j++) ST[i + 1][j] = minInd(ST[i][j], ST[i][j + (1 << i)]);
     }
-    int query(int l, int r) { // 0-indexed, inclusive
-        int i = 31 - __builtin_clz(r - l + 1);
-        return minInd(ST[i][l], ST[i][r - (1 << i) + 1]);
-    }
+    // 0-indexed, inclusive
+    int query(int l, int r) { int i = 31 - __builtin_clz(r - l + 1); return minInd(ST[i][l], ST[i][r - (1 << i) + 1]); }
 };
