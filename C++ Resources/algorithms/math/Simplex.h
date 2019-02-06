a43b42875db0c349ba5cd65c21002a493baa0fe6
wesley-a-leung <wesley.a.leung@gmail.com>
2019-02-05 21:07:36 -0500

    Updated Simplex

--- C++ Resources/algorithms/math/Simplex.h
+++ C++ Resources/algorithms/math/Simplex.h
@@ -7,39 +7,54 @@ using namespace std;
 // subject to Ax <= b and x >= 0
 // where A is an M x N Matrix; b is a Vector with dimension M; c, x are Vectors with dimension N
 // Time Complexity: exponential worst case, polynomial in the average case ~O(N * M * (N + M))
-// Memory Complexity: O(M * (N + M))
+// Memory Complexity: O(M * N)
 template <const int MAXM, const int MAXN, class F> struct Simplex {
-    F EPS, A[MAXM][MAXN], b[MAXM], c[MAXN], x[MAXN], T[MAXM][MAXN + MAXM]; Simplex(F EPS) : EPS(EPS) {}
-    F solve(int M, int N) {
-        for (int i = 0; i <= M; i++) fill(T[i], T[i] + N + M + 1, 0);
-        for (int i = 0; i < M; i++) {
-            for (int j = 0; j < N; j++) T[i][j] = A[i][j];
-            T[i][N + i] = 1;
-            T[i][N + M] = b[i];
+    int M, N, IN[MAXM], OUT[MAXN]; F INF, EPS, A[MAXM][MAXN], b[MAXM], c[MAXN], x[MAXN], T[MAXM][MAXN];
+    Simplex(const F INF, const F EPS) : INF(INF), EPS(EPS) {}
+    void pivot(int r, int s) {
+        auto &a1 = T[r]; F inv = 1 / a1[s];
+        for (int i = 0; i <= M + 1; i++) if (i != r) {
+            auto &a2 = T[i]; F inv2 = a2[s] * inv;
+            for (int j = 0; j <= N + 1; j++) a2[j] -= a1[j] * inv2;
+            a2[s] = a1[s] * inv2;
         }
-        for (int j = 0; j < N; j++) T[M][j] = -c[j];
+        for (int j = 0; j <= N + 1; j++) if (j != s) a1[j] *= inv;
+        for (int i = 0; i <= M + 1; i++) if (i != r) T[i][s] *= -inv;
+        a1[s] = inv; swap(IN[r], OUT[s]);
+    }
+    bool simplex(int phase) {
+        int x = M + phase - 1;
         while (true) {
-            int p = 0, q = 0;
-            for (int i = 0; i < M; i++) if (T[i][N + M] <= T[p][N + M]) p = i;
-            for (int j = 0; j < N + M; j++) if (T[M][j] <= T[M][q]) q = j;
-            F t = min(T[p][N + M], T[M][q]);
-            if (t >= -EPS) break; // optimal
-            if (t < T[p][N + M]) {
-                for (int i = 0; i < M; i++) if (T[i][q] >= EPS && T[i][q] * (T[p][N + M] - t) >= T[p][q] * (T[i][N + M] - t)) p = i;
-                if (T[p][q] <= EPS) return numeric_limits<F>::infinity(); // unbounded
-            } else {
-                for (int j = 0; j <= N + M; j++) T[p][j] *= -1;
-                for (int j = 0; j < N + M; j++) if (T[p][j] >= EPS && T[p][j] * (T[M][q] - t) >= T[p][q] * (T[M][j] - t)) q = j;
-                if (T[p][q] <= EPS) return -numeric_limits<F>::infinity(); // infeasible
-            }
-            for (int j = 0; j <= N + M; j++) if (j != q) T[p][j] /= T[p][q];
-            T[p][q] = 1;
-            for (int i = 0; i <= M; i++) if (i != p) {
-                F alpha = T[i][q];
-                if (abs(alpha) > EPS) for (int j = 0; j <= N + M; j++) T[i][j] -= T[p][j] * alpha;
+            int s = -1;
+            for (int j = 0; j <= N; j++) if (OUT[j] != -phase && (s == -1 || make_pair(T[x][j], OUT[j]) < make_pair(T[x][s], OUT[s]))) s = j;
+            if (T[x][s] >= -EPS) return true;
+            int r = -1;
+            for (int i = 0; i < M; i++)
+                if (T[i][s] > EPS && (r == -1 || make_pair(T[i][N + 1] / T[i][s], IN[i]) < make_pair(T[r][N + 1] / T[r][s], IN[r]))) r = i;
+            if (r == -1) return false;
+            pivot(r, s);
+        }
+    }
+    F solve(int m, int n) {
+        M = m; N = n;
+        for (int i = 0; i <= M + 1; i++) fill(T[i], T[i] + N + 1, 0);
+        for (int i = 0; i < M; i++) for (int j = 0; j < N; j++) T[i][j] = A[i][j];
+        for (int i = 0; i < M; i++) { IN[i] = N + i; T[i][N] = -1; T[i][N + 1] = b[i]; }
+        for (int j = 0; j < N; j++) { OUT[j] = j; T[M][j] = -c[j]; }
+        OUT[N] = -1; T[M + 1][N] = 1;
+        int r = 0;
+        for (int i = 1; i < M; i++) if (T[i][N + 1] < T[r][N + 1]) r = i;
+        if (T[r][N + 1] < -EPS) {
+            pivot(r, N);
+            if (!simplex(2) || T[M + 1][N + 1] < -EPS) return -INF; // infeasible
+            for (int i = 0; i < M; i++) if (IN[i] == -1) {
+                int s = 0;
+                for (int j = 1; j <= N; j++) if (s == -1 || make_pair(T[i][j], OUT[j]) < make_pair(T[i][s], OUT[s])) s = j;
+                pivot(i, s);
             }
         }
-        for (int i = 0; i < M; i++) for (int j = 0; j < N; j++) if (T[i][j] >= 0.5) x[j] = T[i][N + M];
-        return T[M][N + M];
+        bool unbounded = !simplex(1); fill(x, x + N, 0);
+        for (int i = 0; i < M; i++) if (IN[i] < N) x[IN[i]] = T[i][N + 1];
+        return unbounded ? INF : T[M][N + 1];
     }
 };
