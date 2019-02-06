ae9dc32cfe1b45dc90256ef62163a3dbfd6bb36f
wesley-a-leung <wesley.a.leung@gmail.com>
2019-02-05 21:20:02 -0500

    Simplex Changes

--- C++ Resources/algorithms/math/Simplex.h
+++ C++ Resources/algorithms/math/Simplex.h
@@ -11,6 +11,7 @@ using namespace std;
 template <const int MAXM, const int MAXN, class F> struct Simplex {
     int M, N, IN[MAXM], OUT[MAXN]; F INF, EPS, A[MAXM][MAXN], b[MAXM], c[MAXN], x[MAXN], T[MAXM][MAXN];
     Simplex(const F INF, const F EPS) : INF(INF), EPS(EPS) {}
+    bool cmp(F a, int b, F c, int d) { return abs(a - c) > EPS ? a < c : b < d; }
     void pivot(int r, int s) {
         auto &a1 = T[r]; F inv = 1 / a1[s];
         for (int i = 0; i <= M + 1; i++) if (i != r) {
@@ -26,11 +27,11 @@ template <const int MAXM, const int MAXN, class F> struct Simplex {
         int x = M + phase - 1;
         while (true) {
             int s = -1;
-            for (int j = 0; j <= N; j++) if (OUT[j] != -phase && (s == -1 || make_pair(T[x][j], OUT[j]) < make_pair(T[x][s], OUT[s]))) s = j;
+            for (int j = 0; j <= N; j++) if (OUT[j] != -phase && (s == -1 || cmp(T[x][j], OUT[j], T[x][s], OUT[s]))) s = j;
             if (T[x][s] >= -EPS) return true;
             int r = -1;
             for (int i = 0; i < M; i++)
-                if (T[i][s] > EPS && (r == -1 || make_pair(T[i][N + 1] / T[i][s], IN[i]) < make_pair(T[r][N + 1] / T[r][s], IN[r]))) r = i;
+                if (T[i][s] > EPS && (r == -1 || cmp(T[i][N + 1] * T[r][s], IN[i], T[r][N + 1] * T[i][s], IN[r]))) r = i;
             if (r == -1) return false;
             pivot(r, s);
         }
@@ -41,15 +42,14 @@ template <const int MAXM, const int MAXN, class F> struct Simplex {
         for (int i = 0; i < M; i++) for (int j = 0; j < N; j++) T[i][j] = A[i][j];
         for (int i = 0; i < M; i++) { IN[i] = N + i; T[i][N] = -1; T[i][N + 1] = b[i]; }
         for (int j = 0; j < N; j++) { OUT[j] = j; T[M][j] = -c[j]; }
-        OUT[N] = -1; T[M + 1][N] = 1;
-        int r = 0;
+        OUT[N] = -1; T[M + 1][N] = 1; int r = 0;
         for (int i = 1; i < M; i++) if (T[i][N + 1] < T[r][N + 1]) r = i;
         if (T[r][N + 1] < -EPS) {
             pivot(r, N);
             if (!simplex(2) || T[M + 1][N + 1] < -EPS) return -INF; // infeasible
             for (int i = 0; i < M; i++) if (IN[i] == -1) {
                 int s = 0;
-                for (int j = 1; j <= N; j++) if (s == -1 || make_pair(T[i][j], OUT[j]) < make_pair(T[i][s], OUT[s])) s = j;
+                for (int j = 1; j <= N; j++) if (s == -1 || cmp(T[i][j], OUT[j], T[i][s], OUT[s])) s = j;
                 pivot(i, s);
             }
         }
