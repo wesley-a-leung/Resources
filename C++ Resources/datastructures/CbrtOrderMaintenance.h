6c3d53dbbe5dda669ff39d3cde0ee9279a4470db
wesley-a-leung <wesley.a.leung@gmail.com>
2019-02-12 23:03:39 -0500

    Root Order Maintenance

---
+++ C++ Resources/datastructures/CbrtOrderMaintenance.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+// Maintains the rank of an element in an array that supports only insertions
+// Time Complexity:
+//   constructor: O(N)
+//   insert: O(1) amortized
+//   empty, size: O(1)
+//   floor, ceiling, above, below, contains: O(log(N) + cbrt(N)) amortized
+// Memory Complexity: O(N)
+template <class Value, class Comparator = less<Value>> struct CbrtOrderMaintenance {
+    Comparator cmp; double SCALE_FACTOR; vector<Value> small, medium, large;
+    CbrtOrderMaintenance(const double SCALE_FACTOR = 1) : SCALE_FACTOR(SCALE_FACTOR) {}
+    template <class It> CbrtOrderMaintenance(const It st, const It en, const double SCALE_FACTOR = 1) : large(st, en), SCALE_FACTOR(SCALE_FACTOR) {}
+    void resize() {
+        double c = cbrt(small.size() + medium.size() + large.size());
+        if (int(small.size()) > SCALE_FACTOR * c) {
+            int mediumSz = int(medium.size()); sort(small.begin(), small.end(), cmp);
+            for (auto &&x : small) medium.push_back(x);
+            small.clear(); inplace_merge(medium.begin(), medium.begin() + mediumSz, medium.end(), cmp);
+            if (int(medium.size()) > SCALE_FACTOR * c * c) {
+                int largeSz = int(large.size()); sort(medium.begin(), medium.end(), cmp);
+                for (auto &&x : medium) large.push_back(x);
+                medium.clear(); inplace_merge(large.begin(), large.begin() + largeSz, large.end(), cmp);
+            }
+        }
+    }
+    void insert(const Value &val) { small.push_back(val); }
+    int aboveInd(const Value &val) {
+        resize(); int ret = upper_bound(large.begin(), large.end(), val, cmp) - large.begin();
+        ret += upper_bound(medium.begin(), medium.end(), val, cmp) - medium.begin();
+        for (auto &&x : small) ret += !cmp(val, x);
+        return ret;
+    }
+    int ceilingInd(const Value &val) {
+        resize(); int ret = lower_bound(large.begin(), large.end(), val, cmp) - large.begin();
+        ret += lower_bound(medium.begin(), medium.end(), val, cmp) - medium.begin();
+        for (auto &&x : small) ret += cmp(x, val);
+        return ret;
+    }
+    int floorInd(const Value &val) { return aboveInd(val) - 1; }
+    int belowInd(const Value &val) { return ceilingInd(val) - 1; }
+    bool contains(const Value &val) {
+        if (binary_search(large.begin(), large.end(), val, cmp)) return true;
+        if (binary_search(medium.begin(), medium.end(), val, cmp)) return true;
+        resize();
+        if (binary_search(large.begin(), large.end(), val, cmp)) return true;
+        if (binary_search(medium.begin(), medium.end(), val, cmp)) return true;
+        for (auto &&x : small) if (!cmp(val, x) && !cmp(x, val)) return true;
+        return false;
+    }
+    bool empty() const { return small.empty() && medium.empty() && large.empty(); } 
+    int size() const { return int(small.size() + medium.empty() + large.size()); } 
+    void clear() const { small.clear(); medium.clear(); large.clear(); }
+};
---
+++ C++ Resources/datastructures/RootOrderMaintenance.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+// Maintains the rank of an element in an array that supports only insertions
+// Time Complexity:
+//   constructor: O(N)
+//   insert: O(1) amortized
+//   empty, size: O(1)
+//   floor, ceiling, above, below, contains: O(log(N) + N ^ (1 / R)) amortized
+// Memory Complexity: O(N)
+template <const int R, class Value, class Comparator = less<Value>> struct RootOrderMaintenance {
+    Comparator cmp; int n; double SCALE_FACTOR; vector<Value> A[R];
+    RootOrderMaintenance(const double SCALE_FACTOR = 1) : n(0), SCALE_FACTOR(SCALE_FACTOR) {}
+    template <class It> RootOrderMaintenance(const It st, const It en, const double SCALE_FACTOR = 1) : n(en - st), SCALE_FACTOR(SCALE_FACTOR) {
+        A.back() = vector<Value>(st, en);
+    }
+    void resize() {
+        double b = pow(n, 1.0 / R), c = b;
+        for (int i = 0; i < R - 1; i++, c *= b) {
+            if (int(A[i].size()) > SCALE_FACTOR * c) {
+                int nxtSz = int(A[i + 1].size()); sort(A[i].begin(), A[i].end(), cmp);
+                for (auto &&x : A[i]) A[i + 1].push_back(x);
+                A[i].clear(); inplace_merge(A[i + 1].begin(), A[i + 1].begin() + nxtSz, A[i + 1].end(), cmp);   
+            }
+        }
+    }
+    void insert(const Value &val) { A[0].push_back(val); n++; }
+    int aboveInd(const Value &val) {
+        resize(); int ret = 0;
+        for (int i = R - 1; i >= 1; i--) ret += upper_bound(A[i].begin(), A[i].end(), val, cmp) - A[i].begin();
+        for (auto &&x : A[0]) ret += !cmp(val, x);
+        return ret;
+    }
+    int ceilingInd(const Value &val) {
+        resize(); int ret = 0;
+        for (int i = R - 1; i >= 1; i--) ret += lower_bound(A[i].begin(), A[i].end(), val, cmp) - A[i].begin();
+        for (auto &&x : A[0]) ret += !cmp(val, x);
+        return ret;
+    }
+    int floorInd(const Value &val) { return aboveInd(val) - 1; }
+    int belowInd(const Value &val) { return ceilingInd(val) - 1; }
+    bool contains(const Value &val) {
+        for (int i = R - 1; i >= 1; i--) if (binary_search(A[i].begin(), A[i].end(), val, cmp)) return true;
+        resize();
+        for (int i = R - 1; i >= 1; i--) if (binary_search(A[i].begin(), A[i].end(), val, cmp)) return true;
+        for (auto &&x : A[0]) if (!cmp(val, x) && !cmp(x, val)) return true;
+        return false;
+    }
+    bool empty() const { return n == 0; } 
+    int size() const { return n; } 
+    void clear() const { for (int i = 0; i < R; i++) A[i].clear(); }
+};
