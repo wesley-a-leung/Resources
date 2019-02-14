e6da34f14e1ef46ab374d649b396326f709b29d0
wesley-a-leung <wesley.a.leung@gmail.com>
2019-02-14 01:38:17 -0500

    Arpas Trick Update

--- C++ Resources/algorithms/queries/ArpasTrick.h
+++ C++ Resources/algorithms/queries/ArpasTrick.h
@@ -6,16 +6,14 @@ using namespace std;
 // Time Complexity: O((N + Q) * (Inverse Ackermann))
 // Memory Complexity: O(N + Q)
 template <const int MAXN, const int MAXQ, class T, const bool ONE_INDEXED> struct ArpasTrick {
-    T A[MAXN], ans[MAXQ]; int Q = 0, UF[MAXN]; vector<pair<int, int>> L[MAXN];
+    T A[MAXN], ans[MAXQ]; int Q = 0, UF[MAXN], stk[MAXN]; vector<pair<int, int>> L[MAXN];
     int find(int i) { while (i != UF[i]) i = UF[i] = UF[UF[i]]; return i; }
     // 0-indexed, inclusive
     void query(int l, int r) { L[r].emplace_back(l, Q++); }
     void solve(int N) {
-        stack<int> stk;
-        for (int r = ONE_INDEXED; r < N + ONE_INDEXED; r++) {
-            for (UF[r] = r; !stk.empty() && A[stk.top()] > A[r]; stk.pop()) UF[stk.top()] = r;
-            stk.push(r);
-            for (auto &&q : L[r]) ans[q.second] = A[find(q.first)];
+        for (int r = ONE_INDEXED, top = 0; r < N + ONE_INDEXED; r++) {
+            for (UF[r] = r; top > 0 && A[stk[top - 1]] > A[r];) UF[stk[--top]] = r;
+            for (auto &&q : L[stk[top++] = r]) ans[q.second] = A[find(q.first)];
         }
     }
     void clear(int N = MAXN) { Q = 0; for (int i = 0; i < N; i++) L[i].clear(); }
