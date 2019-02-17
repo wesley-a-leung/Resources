5e84948ad55fdf476f98abb17d42bf69e7b46f3e
wesley-a-leung <wesley.a.leung@gmail.com>
2019-02-17 00:17:45 -0500

    Top down seg tree update

--- C++ Resources/datastructures/trees/MaxContiguousSubarraySumData.h
+++ C++ Resources/datastructures/trees/MaxContiguousSubarraySumData.h
@@ -2,10 +2,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Data struct used for maximum contiguous subarray for Segment Trees, Link Cut Trees, etc
+// Data struct used for maximum non empty contiguous subarray for Segment Trees, Link Cut Trees, etc
 template <class T> struct MaxContiguousSubarrayData { T pre, suf, sum, maxSum; bool isNull; };
-template <class T> MaxContiguousSubarrayData<T> nullNode() {
-    MaxContiguousSubarrayData<T> ret; ret.pre = ret.suf = ret.sum = ret.maxSum = 0; ret.isNull = true; return ret;
+template <class T> MaxContiguousSubarrayData<T> makeNode(bool isNull = true, const T &v = 0) {
+    MaxContiguousSubarrayData<T> ret; ret.pre = ret.suf = ret.sum = ret.maxSum = v; ret.isNull = isNull; return ret;
 }
 template <class T> MaxContiguousSubarrayData<T> merge(const MaxContiguousSubarrayData<T> &l, const MaxContiguousSubarrayData<T> &r) {
     if (l.isNull) return r;
@@ -14,8 +14,9 @@ template <class T> MaxContiguousSubarrayData<T> merge(const MaxContiguousSubarra
     ret.sum = l.sum + r.sum; ret.maxSum = max(max(l.maxSum, r.maxSum), l.suf + r.pre); return ret;
 }
 // range assignments
-template <class T> MaxContiguousSubarrayData<T> applyLazy(const MaxContiguousSubarrayData<T> &x, const T &v) {
-    MaxContiguousSubarrayData<T> ret; ret.pre = ret.suf = ret.sum = ret.maxSum = v; ret.isNull = false; return ret;
+template <class T> MaxContiguousSubarrayData<T> applyLazy(const MaxContiguousSubarrayData<T> &l, const pair<T, T> &r) {
+    MaxContiguousSubarrayData<T> ret; ret.pre = ret.suf = ret.maxSum = max(l.first, l.second); ret.sum = l.first; ret.isNull = false; return ret;
 }
-template <class T> T getSegmentVal(const T &l, int len) { return l * len; }
-template <class T> T mergeLazy(const T &l, const T &r) { return r; }
+// getSegmentVal returns a pair containing the range value, and the actual value
+template <class T> pair<T, T> getSegmentVal(const pair<T, T> &l, int len) { return make_pair(l.first * len, l.second); }
+template <class T> pair<T, T> mergeLazy(const pair<T, T> &l, const pair<T, T> &r) { return r; }
--- C++ Resources/datastructures/trees/SegmentTreeLinear.h
+++ C++ Resources/datastructures/trees/SegmentTreeLinear.h
@@ -14,38 +14,39 @@ Data operator + (const Data &a, const Data &b) { return make_pair(a.first + b.fi
 Data operator * (const Data &a, const Data &b) { return make_pair(a.first * b.first, a.second * b.second); }
 
 template <const int MAXN, const bool ONE_INDEXED> struct SegmentTreeLinear {
-    Data T[MAXN * 4], L[MAXN * 4]; type A[MAXN]; int N; const Data ZERO = make_pair(0, 0);
+    Data T[MAXN * 2], L[MAXN * 2]; type A[MAXN]; int N; const Data ZERO = make_pair(0, 0);
     type sumTo(type n) { return n * (n + 1) / 2; }
     Data sumBet(type l, type r) { return make_pair(r - l + 1, sumTo(r) - sumTo(l - 1)); }
     void propagate(int cur, int cL, int cR) {
-        int m = cL + (cR - cL) / 2;
+        int m = cL + (cR - cL) / 2, rc = cur + (m - cL + 1) * 2;
         if (L[cur] != ZERO) {
-            T[cur * 2] = T[cur * 2] + L[cur] * sumBet(cL, m); L[cur * 2] = L[cur * 2] + L[cur];
-            T[cur * 2 + 1] = T[cur * 2 + 1] + L[cur] * sumBet(m + 1, cR); L[cur * 2 + 1] = L[cur * 2 + 1] + L[cur];
+            T[cur + 1] = T[cur + 1] + L[cur] * sumBet(cL, m); L[cur + 1] = L[cur + 1] + L[cur];
+            T[rc] = T[rc] + L[cur] * sumBet(m + 1, cR); L[rc] = L[rc] + L[cur];
             L[cur] = ZERO;
         }
     }
     void build(int cur, int cL, int cR) {
         if (cL == cR) { T[cur] = make_pair(A[cL], 0); L[cur] = ZERO; return; }
-        int m = cL + (cR - cL) / 2; build(cur * 2, cL, m); build(cur * 2 + 1, m + 1, cR); T[cur] = T[cur * 2] + T[cur * 2 + 1];
+        int m = cL + (cR - cL) / 2, rc = cur + (m - cL + 1) * 2; build(cur + 1, cL, m); build(rc, m + 1, cR); T[cur] = T[cur + 1] + T[rc];
     }
     void update(int cur, int cL, int cR, int l, int r, const Data &val) {
         if (cL > r || cR < l) return;
         if (cL >= l && cR <= r) { T[cur] = T[cur] + val * sumBet(cL, cR); L[cur] = L[cur] + val; return; }
-        int m = cL + (cR - cL) / 2; propagate(cur, cL, cR);
-        update(cur * 2, cL, m, l, r, val); update(cur * 2 + 1, m + 1, cR, l, r, val); T[cur] = T[cur * 2] + T[cur * 2 + 1];
+        int m = cL + (cR - cL) / 2, rc = cur + (m - cL + 1) * 2; propagate(cur, cL, cR);
+        update(cur + 1, cL, m, l, r, val); update(rc, m + 1, cR, l, r, val); T[cur] = T[cur + 1] + T[rc];
     }
     Data query(int cur, int cL, int cR, int l, int r) {
         if (cL > r || cR < l) return ZERO;
         if (cL >= l && cR <= r) return T[cur];
-        int m = cL + (cR - cL) / 2; propagate(cur, cL, cR); return query(cur * 2, cL, m, l, r) + query(cur * 2 + 1, m + 1, cR, l, r);
+        int m = cL + (cR - cL) / 2, rc = cur + (m - cL + 1) * 2; propagate(cur, cL, cR);
+        return query(cur + 1, cL, m, l, r) + query(rc, m + 1, cR, l, r);
     }
     template <class It> void init(It st, It en) {
         N = en - st;
         for (int i = 1; i <= N; i++) A[i] = *(st + i - 1);
-        build(1, 1, N);
+        build(0, 1, N);
     }
-    void init(int size) { N = size; fill(A + ONE_INDEXED, A + N + ONE_INDEXED, 0); build(1, ONE_INDEXED, N - !ONE_INDEXED); }
-    void update(int l, int r, type val) { update(1, ONE_INDEXED, N - !ONE_INDEXED, l, r, make_pair((1 - l) * val, val)); }
-    type query(int l, int r) { Data q = query(1, ONE_INDEXED, N - !ONE_INDEXED, l, r); return q.first + q.second; }
+    void init(int size) { N = size; fill(A + ONE_INDEXED, A + N + ONE_INDEXED, 0); build(0, ONE_INDEXED, N - !ONE_INDEXED); }
+    void update(int l, int r, type val) { update(0, ONE_INDEXED, N - !ONE_INDEXED, l, r, make_pair((1 - l) * val, val)); }
+    type query(int l, int r) { Data q = query(0, ONE_INDEXED, N - !ONE_INDEXED, l, r); return q.first + q.second; }
 };
