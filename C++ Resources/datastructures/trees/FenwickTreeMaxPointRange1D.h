5dd5ce9b5ccbc50d0887a88a46fbcddbeee838ef
wesley-a-leung <wesley.a.leung@gmail.com>
2019-02-18 22:14:00 -0500

    Testing Fenwick Tree Max

--- C++ Resources/datastructures/trees/FenwickTreeMaxPointRange1D.h
+++ C++ Resources/datastructures/trees/FenwickTreeMaxPointRange1D.h
@@ -8,9 +8,9 @@ using namespace std;
 //   update, rmq(i): O(log N)
 //   rmq(l, r): O((log N)^2)
 // Memory Complexity: O(N)
-template <const int MAXN, class T, const bool ONE_INDEXED> struct FenwickTreeMaxPointRange {
+template <const int MAXN, class T, const bool ONE_INDEXED> struct FenwickTreeMaxPointRange1D {
     T A[MAXN], BIT[MAXN], NEG_INF;
-    FenwickTreeMaxPointRange(T NEG_INF) : NEG_INF(NEG_INF) { fill(A, A + MAXN, NEG_INF); fill(BIT, BIT + MAXN, NEG_INF); }
+    FenwickTreeMaxPointRange1D(T NEG_INF) : NEG_INF(NEG_INF) { fill(A, A + MAXN, NEG_INF); fill(BIT, BIT + MAXN, NEG_INF); }
     void update(int i, T v) { for (i += !ONE_INDEXED, A[i] = max(A[i], v); i < MAXN; i += i & -i) BIT[i] = max(BIT[i], v); }
     T rmq(int i) { T ret = NEG_INF; for (i += !ONE_INDEXED; i > 0; i -= i & -i) ret = max(ret, BIT[i]); return ret; }
     T rmq(int l, int r) {
