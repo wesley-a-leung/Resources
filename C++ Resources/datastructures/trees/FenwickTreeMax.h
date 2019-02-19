3cab5f974374269b19b2411d4d0db43be0ba4ef2
wesley-a-leung <wesley.a.leung@gmail.com>
2019-02-18 22:14:00 -0500

    Testing Fenwick Tree Max

--- C++ Resources/datastructures/trees/FenwickTreeMax.h
+++ C++ Resources/datastructures/trees/FenwickTreeMax.h
@@ -2,14 +2,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Fenwick Tree or Binary Indexed Tree supporting point updates and prefix range queries for maximum value queries
+// Fenwick Tree or Binary Indexed Tree supporting point updates and prefix range queries for maximum value queries in any number of dimensions
 // Time Complexity:
-//   init: O(N)
-//   update, rmq: O(log N)
-// Memory Complexity: O(N)
-template <const int MAXN, class T, const bool ONE_INDEXED> struct FenwickTreeMax {
-    T BIT[MAXN], NEG_INF;
-    FenwickTreeMax(T NEG_INF) : NEG_INF(NEG_INF) { fill(BIT, BIT + MAXN, NEG_INF); }
-    void update(int i, T v) { for (i += !ONE_INDEXED; i < MAXN; i += i & -i) BIT[i] = max(BIT[i], v); }
-    T rmq(int i) { T ret = NEG_INF; for (i += !ONE_INDEXED; i > 0; i -= i & -i) ret = max(ret, BIT[i]); return ret; }
+//   init: O(PI(N_i))
+//   update, rmq: O(PI(log(N_i)))
+// Memory Complexity: O(PI(N_i))
+// where PI is the product function, N_i is the size in the ith dimension, and D is the number of dimensions
+using T = int; const T NEG_INF = 0;
+
+template <const bool ONE_INDEXED, const int ...Args> struct FenwickTreeMax {
+    T val;
+    void init() { val = NEG_INF; }
+    void update(T v) { val = max(val, v); }
+    T rmq() { return val; }
+};
+
+template <const bool ONE_INDEXED, const int MAXN, const int ...Ns> struct FenwickTreeMax <ONE_INDEXED, MAXN, Ns...> {
+    FenwickTreeMax<ONE_INDEXED, Ns...> BIT[MAXN];
+    void init() { for (int i = 0; i < MAXN; i++) BIT[i].init(); }
+    template <class ...Args> void update(int i, Args ...args) { for (i += !ONE_INDEXED; i < MAXN; i += i & -i) BIT[i].update(args...); }
+    template <class ...Args> T rmq(int i, Args ...args) {
+        T ret = NEG_INF; for (i += !ONE_INDEXED; i > 0; i -= i & -i) ret = max(ret, BIT[i].rmq(args...)); return ret;
+    }
 };
---
+++ C++ Resources/datastructures/trees/FenwickTreeMax1D.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+// Fenwick Tree or Binary Indexed Tree supporting point updates and prefix range queries for maximum value queries
+// Time Complexity:
+//   init: O(N)
+//   update, rmq: O(log N)
+// Memory Complexity: O(N)
+template <const int MAXN, class T, const bool ONE_INDEXED> struct FenwickTreeMax1D {
+    T BIT[MAXN], NEG_INF;
+    FenwickTreeMax1D(T NEG_INF) : NEG_INF(NEG_INF) { fill(BIT, BIT + MAXN, NEG_INF); }
+    void update(int i, T v) { for (i += !ONE_INDEXED; i < MAXN; i += i & -i) BIT[i] = max(BIT[i], v); }
+    T rmq(int i) { T ret = NEG_INF; for (i += !ONE_INDEXED; i > 0; i -= i & -i) ret = max(ret, BIT[i]); return ret; }
+};
