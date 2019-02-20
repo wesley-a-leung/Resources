b4d4181305db0c8e5333dcd1a685e186a6828f16
wesley-a-leung <wesley.a.leung@gmail.com>
2019-02-20 00:54:23 -0500

    HLD Update

--- C++ Resources/algorithms/graph/queries/HLD.h
+++ C++ Resources/algorithms/graph/queries/HLD.h
@@ -12,6 +12,7 @@ using namespace std;
 template <const int MAXV, const bool ONE_INDEXED, const bool VALUES_ON_EDGES> struct HLD {
     using Data = int; using Lazy = int; const Data qdef = 0;
     int dep[MAXV], par[MAXV], chain[MAXV], size[MAXV], head[MAXV], ind[MAXV], vert[MAXV], chainNum, curInd; vector<int> adj[MAXV];
+    void addEdge(int a, int b) { adj[a].push_back(b); adj[b].push_back(a); }
     void dfs(int v, int prev, int d) {
         dep[v] = d; par[v] = prev; size[v] = 1;
         for (int w : adj[v]) if (w != prev) { dfs(w, v, d + 1); size[v] += size[w]; }
@@ -27,26 +28,34 @@ template <const int MAXV, const bool ONE_INDEXED, const bool VALUES_ON_EDGES> st
     void update(int i, const Lazy &val); // to be implemented
     void update(int l, int r, bool up, const Lazy &val); // to be implemented
     Data query(int l, int r, bool up); // to be implemented
-    Data queryUp(int v, int w, bool up, bool includeW) {
-        Data ans = qdef;
+    Data queryPath(int v, int w) {
+        Data up = qdef, down = qdef;
         while (chain[v] != chain[w]) {
-            ans = up ? merge(ans, query(ind[head[chain[v]]], ind[v], up)) : merge(query(ind[head[chain[v]]], ind[v], up), ans);
-            v = par[head[chain[v]]];
+            if (dep[head[chain[v]]] < dep[head[chain[w]]]) { 
+                down = merge(query(ind[head[chain[w]]], ind[w], false), down);
+                w = par[head[chain[w]]];
+            } else {
+                up = merge(up, query(ind[head[chain[v]]], ind[v], true));
+                v = par[head[chain[v]]];
+            }
         }
-        if (!includeW && v == w) return ans;
-        return up ? merge(ans, query(ind[w] + !includeW, ind[v], up)) : merge(query(ind[w] + !includeW, ind[v], up), ans);
+        if (v != w) {
+            if (dep[v] < dep[w]) up = merge(up, query(ind[v] + VALUES_ON_EDGES, ind[w], false));
+            else down = merge(query(ind[w] + VALUES_ON_EDGES, ind[v], true), down);
+        } else if (!VALUES_ON_EDGES) up = merge(up, query(ind[dep[v] < dep[w] ? v : w], ind[dep[v] < dep[w] ? v : w], true));
+        return merge(up, down);
     }
-    void updateUp(int v, int w, bool up, bool includeW, const Lazy &val) {
+    void updatePath(int v, int w, const Lazy &val) {
         while (chain[v] != chain[w]) {
-            update(ind[head[chain[v]]], ind[v], up, val);
-            v = par[head[chain[v]]];
+            if (dep[head[chain[v]]] < dep[head[chain[w]]]) { update(ind[head[chain[w]]], ind[w], false, val); w = par[head[chain[w]]]; }
+            else { update(ind[head[chain[v]]], ind[v], true, val); v = par[head[chain[v]]]; }
         }
-        if (!includeW && v == w) return;
-        update(ind[w] + !includeW, ind[v], up, val);
+        if (v != w) {
+            if (dep[v] < dep[w]) update(ind[v] + VALUES_ON_EDGES, ind[w], false, val);
+            else update(ind[w] + VALUES_ON_EDGES, ind[v], true, val);
+        } else if (!VALUES_ON_EDGES) update(ind[dep[v] < dep[w] ? v : w], ind[dep[v] < dep[w] ? v : w], true, val);
     }
-    void clear(int V = MAXV) { for (int i = 0; i < V; i++) adj[i].clear(); }
-    void run(int V, int root = 0) { chainNum = 0; curInd = ONE_INDEXED; fill(head, head + V, -1); dfs(root, -1, 0); hld(root, -1); }
-    void addEdge(int a, int b) { adj[a].push_back(b); adj[b].push_back(a); }
+    void updateVertex(int v, const Lazy &val) { update(ind[v], val); }
     int lca(int v, int w) {
         while (chain[v] != chain[w]) {
             if (dep[head[chain[v]]] < dep[head[chain[w]]]) w = par[head[chain[w]]];
@@ -54,11 +63,6 @@ template <const int MAXV, const bool ONE_INDEXED, const bool VALUES_ON_EDGES> st
         }
         return dep[v] < dep[w] ? v : w;
     }
-    Data queryPath(int v, int w) {
-        int lcavertex = lca(v, w); return merge(queryUp(v, lcavertex, true, false), queryUp(w, lcavertex, false, !VALUES_ON_EDGES));
-    }
-    void updatePath(int v, int w, const Lazy &val) {
-        int lcavertex = lca(v, w); updateUp(v, lcavertex, true, false, val); updateUp(w, lcavertex, false, !VALUES_ON_EDGES, val);
-    }
-    void updateVertex(int v, const Lazy &val) { update(ind[v], val); }
+    void clear(int V = MAXV) { for (int i = 0; i < V; i++) adj[i].clear(); }
+    void run(int V, int root = 0) { chainNum = 0; curInd = ONE_INDEXED; fill(head, head + V, -1); dfs(root, -1, 0); hld(root, -1); }
 };
