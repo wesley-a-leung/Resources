9c3120b4739ee8c9c99d157e66c79ac563b1ae47
wesley-a-leung <wesley.a.leung@gmail.com>
2019-02-20 22:40:31 -0500

    Updated Sparse table

--- C++ Resources/datastructures/RMQSparseTable.h
+++ C++ Resources/datastructures/RMQSparseTable.h
@@ -2,18 +2,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Sparse Table for Range Minimum Queries
+// Sparse Table for Range Minimum / Maximum Queries
 // Time Complexity:
 //   init: O(N log N)
 //   query: O(1)
 // Memory Complexity: O(N log N)
-template <const int MAXN, const int MAXLGN, class T> struct RMQSparseTable {
+template <const int MAXN, const int MAXLGN, class T, const bool IS_MAX> struct RMQSparseTable {
     T A[MAXN]; int ST[MAXLGN][MAXN];
-    int minInd(int l, int r) { return A[l] <= A[r] ? l : r; }
+    int cmpInd(int l, int r) { return (A[l] <= A[r]) ^ IS_MAX ? l : r; }
     void init(int N) {
         int lg = 32 - __builtin_clz(N); assert(lg < MAXLGN); iota(ST[0], ST[0] + N, 0);
-        for (int i = 0; i < lg - 1; i++) for (int j = 0; j + (1 << i) < N; j++) ST[i + 1][j] = minInd(ST[i][j], ST[i][j + (1 << i)]);
+        for (int i = 0; i < lg - 1; i++) for (int j = 0; j + (1 << i) < N; j++) ST[i + 1][j] = cmpInd(ST[i][j], ST[i][j + (1 << i)]);
     }
     // 0-indexed, inclusive
-    int query(int l, int r) { int i = 31 - __builtin_clz(r - l + 1); return minInd(ST[i][l], ST[i][r - (1 << i) + 1]); }
+    int query(int l, int r) { int i = 31 - __builtin_clz(r - l + 1); return cmpInd(ST[i][l], ST[i][r - (1 << i) + 1]); }
 };
