a4aece7bcb570ba532778e826a303376b39fafe5
wesley-a-leung <wesley.a.leung@gmail.com>
2019-03-02 16:52:51 -0500

    Fixed DAGSP

--- C++ Resources/algorithms/graph/shortestpath/DAGSP.h
+++ C++ Resources/algorithms/graph/shortestpath/DAGSP.h
@@ -8,12 +8,12 @@ using namespace std;
 // Memory Complexity: O(V + E)
 // less<unit> is the shortest path, greater<unit> is the longest path
 template <const int MAXV, class unit, class Comparator = less<unit>> struct DAGSP {
-    Comparator cmp; unit NEG_INF, dist[MAXV]; pair<int, unit> to[MAXV]; vector<pair<int, unit>> adj[MAXV]; TopologicalOrder<MAXV> ord;
-    DAGSP(unit NEG_INF) : NEG_INF(NEG_INF) {}
+    Comparator cmp; unit INF, dist[MAXV]; pair<int, unit> to[MAXV]; vector<pair<int, unit>> adj[MAXV]; TopologicalOrder<MAXV> ord;
+    DAGSP(unit INF) : INF(INF) {}
     void addEdge(int v, int w, unit weight) { adj[v].emplace_back(w, weight); ord.addEdge(v, w); }
     void clear(int V = MAXV) { ord.clear(V); for (int i = 0; i < V; i++) adj[i].clear(); }
     void run(int V, const vector<int> &src = vector<int>()) { // if src is empty, then all vertices are sources
-        fill(dist, dist + V, src.empty() ? 0 : NEG_INF); fill(to, to + V, make_pair(-1, 0)); ord.run(V);
+        fill(dist, dist + V, src.empty() ? 0 : INF); fill(to, to + V, make_pair(-1, 0)); ord.run(V);
         for (int s : src) dist[s] = 0;
         for (int i = 0; i < V; i++) {
             int v = ord.ord[i];
