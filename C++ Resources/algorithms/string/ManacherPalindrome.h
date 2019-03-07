5b3334caa574b38df6eeb795dee4a07d3db11f79
wesley-a-leung <wesley.a.leung@gmail.com>
2019-03-07 16:05:18 -0500

    Updated manacher

--- C++ Resources/algorithms/string/ManacherPalindrome.h
+++ C++ Resources/algorithms/string/ManacherPalindrome.h
@@ -6,7 +6,7 @@ using namespace std;
 // Time Complexity: O(S)
 // Memory Complexity: O(S) additional memory
 template <const int MAXS> struct ManacherPalindrome {
-    int p[MAXS]; string s; char t[MAXS * 2];
+    int p[MAXS * 2]; string s; char t[MAXS * 2];
     void preProcess(const string &s) {
         this->s = s; t[0] = '$'; t[s.length() * 2 + 1] = '#'; t[s.length() * 2 + 2] = '@';
         for (int i = 0; i < int(s.length()); i++) { t[2 * i + 1] = '#'; t[2 * i + 2] = s[i]; }
@@ -18,13 +18,15 @@ template <const int MAXS> struct ManacherPalindrome {
             if (i + p[i] > right) { center = i; right = i + p[i]; }
         }
     }
-    string longestPalindromicSubstring() { // longest palindromic substring
+    // longest palindromic substring, returns a pair containing the starting index and length
+    pair<int, int> longestPalindromicSubstring() {
         int length = 0, center = 0;
         for (int i = 1; i < int(s.length()) * 2 + 2; i++) if (p[i] > length) { length = p[i]; center = i; }
-        return s.substr((center - 1 - length) / 2, (center - 1 + length) / 2 - (center - 1 - length) / 2);
+        return make_pair((center - 1 - length) / 2, (center - 1 + length) / 2 - (center - 1 - length) / 2);
     }
-    string longestPalindromicSubstring(int i) { // longest palindromic substring centered at index i/2
+    // longest palindromic substring centered at index i/2, returns a pair containing the starting index and length
+    pair<int, int> longestPalindromicSubstring(int i) {
         int length = p[i + 2], center = i + 2;
-        return s.substr((center - 1 - length) / 2, (center - 1 + length) / 2 - (center - 1 - length) / 2);
+        return make_pair((center - 1 - length) / 2, (center - 1 + length) / 2 - (center - 1 - length) / 2);
     }
 };
