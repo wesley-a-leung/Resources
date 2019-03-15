c7aacc3c65aeb37c2350b205d8ed7bf3e0d611ba
wesley-a-leung <wesley.a.leung@gmail.com>
2019-03-15 17:19:55 -0400

    Fraction Mod Update

--- C++ Resources/datastructures/math/FractionMod.h
+++ C++ Resources/datastructures/math/FractionMod.h
@@ -3,46 +3,23 @@
 #include "../../algorithms/math/Combinatorics.h"
 using namespace std;
 
-using T = long long;
-T MOD = 1e9 + 7;
-
-// Data structure representing a Fraction modulo MOD
-struct FractionMod {
-    T num, den;
-    FractionMod(T num = 0, T den = 0) {
-        num %= MOD; den %= MOD;
-        if (num < 0) num += MOD;
-        if (den < 0) den += MOD;
-        this->num = num; this->den = den;
+// Data structure representing a Fraction modulo a prime MOD
+template <class T, const T MOD> struct FractionMod {
+    static_assert(is_integral<T>::value, "T must be an integral type");
+    static_assert(0 < MOD, "MOD must be a positive integer");
+    using FM = FractionMod<T, MOD>; T num, den;
+    FractionMod(T n = 0, T d = 0) {
+        num = -MOD < n && n < MOD ? n : n % MOD; if (num < 0) num += MOD;
+        den = -MOD < d && d < MOD ? d : d % MOD; if (den < 0) den += MOD;
     }
-    FractionMod operator + (const FractionMod &f) const {
-        return FractionMod((num * f.den % MOD + f.num * den % MOD) % MOD, den * f.den % MOD);
-    }
-    FractionMod &operator += (const FractionMod &f) {
-        num = (num * f.den % MOD + f.num * den % MOD) % MOD; den = den * f.den % MOD;
-        return *this;
-    }
-    FractionMod operator - (const FractionMod &f) const {
-        return FractionMod((num * f.den % MOD - f.num * den % MOD + MOD) % MOD, den * f.den % MOD);
-    }
-    FractionMod &operator -= (const FractionMod &f) {
-        num = (num * f.den % MOD - f.num * den % MOD + MOD) % MOD; den = den * f.den % MOD;
-        return *this;
-    }
-    FractionMod operator * (const FractionMod &f) const {
-        return FractionMod(num * f.num % MOD, den * f.den % MOD);
-    }
-    FractionMod &operator *= (const FractionMod &f) {
-        num = num * f.num % MOD; den = den * f.den % MOD;
-        return *this;
-    }
-    FractionMod operator / (const FractionMod &f) const {
-        return FractionMod(num * f.den % MOD, den * f.num % MOD);
-    }
-    FractionMod &operator /= (const FractionMod &f) {
-        T t_num = num * f.den % MOD, t_den = den * f.num % MOD; num = t_num; den = t_den;
-        return *this;
-    }
-    bool operator == (const FractionMod &f) const { return divMod(num, den, MOD) == divMod(f.num, f.den, MOD); }
-    bool operator != (const FractionMod &f) const { return !(*this == f); }
+    FM operator + (const FM &f) const { return FM(addMod(mulMod(num, f.den, MOD), mulMod(f.num, den, MOD), MOD), mulMod(den, f.den, MOD)); }
+    FM &operator += (const FM &f) { num = addMod(mulMod(num, f.den, MOD), mulMod(f.num, den, MOD), MOD); den = mulMod(den, f.den, MOD); return *this; }
+    FM operator - (const FM &f) const { return FM(subMod(mulMod(num, f.den, MOD), mulMod(f.num, den, MOD), MOD), mulMod(den, f.den, MOD)); }
+    FM &operator -= (const FM &f) { num = subMod(mulMod(num, f.den, MOD), mulMod(f.num, den, MOD), MOD); den = mulMod(den, f.den, MOD); return *this; }
+    FM operator * (const FM &f) const { return FM(mulMod(num, f.num, MOD), mulMod(den, f.den, MOD)); }
+    FM &operator *= (const FM &f) { num = mulMod(num, f.num, MOD); den = mulMod(den, f.den, MOD); return *this; }
+    FM operator / (const FM &f) const { return FM(mulMod(num, f.den, MOD), mulMod(den, f.num, MOD)); }
+    FM &operator /= (const FM &f) { T t_num = mulMod(num, f.den, MOD), t_den = mulMod(den, f.num, MOD); num = t_num; den = t_den; return *this; }
+    bool operator == (const FM &f) const { return mulMod(num, f.den, MOD) == mulMod(f.num, den, MOD); }
+    bool operator != (const FM &f) const { return mulMod(num, f.den, MOD) != mulMod(f.num, den, MOD); }
 };
