4cb1c5d42acb4508543588fd6f2664a16bbdbfa8
wesley-a-leung <wesley.a.leung@gmail.com>
2019-03-18 17:45:35 -0400

    Tidal flow update

--- C++ Resources/algorithms/graph/networkflow/DinicMaxFlow.h
+++ C++ Resources/algorithms/graph/networkflow/DinicMaxFlow.h
@@ -11,21 +11,21 @@ using namespace std;
 template <const int MAXV, const int MAXE, class unit, const bool SCALING> struct DinicMaxFlow {
     unit INF, EPS; DinicMaxFlow(unit INF, unit EPS) : INF(INF), EPS(EPS) {}
     struct Edge {
-        int from, to; unit origCap, cap; int rev; char isRev; Edge() {}
-        Edge(int from, int to, unit origCap, unit cap, char isRev) :
-            from(from), to(to), origCap(origCap), cap(cap), isRev(isRev) {}
+        int from, to; unit origCap, cap, maxCap; int rev; char isRev; Edge() {}
+        Edge(int from, int to, unit cap, unit maxCap, char isRev) :
+            from(from), to(to), origCap(cap), cap(cap), maxCap(maxCap), isRev(isRev) {}
     };
     int E, cur[MAXV], level[MAXV], q[MAXV], st[MAXV], deg[MAXV], ord[MAXE * 2], ind[MAXE * 2];
     bool cut[MAXV]; Edge e[MAXE * 2]; unit maxFlow, minCut, maxCap;
     void addEdge(int v, int w, unit vw, unit wv = 0) {
-        e[E++] = Edge(v, w, max(vw, wv), vw, 0); e[E++] = Edge(w, v, max(vw, wv), wv, 1);
+        e[E++] = Edge(v, w, vw, max(vw, wv), 0); e[E++] = Edge(w, v, wv, max(vw, wv), 1);
         e[E - 2].rev = E - 1; e[E - 1].rev = E - 2; deg[v]++; deg[w]++; maxCap = max(maxCap, max(vw, wv));
     }
     bool bfs(int V, int s, int t, unit lim, char r) {
         fill(level, level + V, -1); level[s] = 0; int front = 0, back = 0; q[back++] = s;
         while (front < back && level[t] == -1) {
             int v = q[front++];
-            for (int i = st[v]; i < st[v] + deg[v] && e[i].origCap > lim && e[i].isRev <= r; i++) {
+            for (int i = st[v]; i < st[v] + deg[v] && e[i].maxCap > lim && e[i].isRev <= r; i++) {
                 if (level[e[i].to] == -1 && e[i].cap > EPS) {
                     level[e[i].to] = level[v] + 1; q[back++] = e[i].to;
                     if (level[t] != -1) return true;
@@ -37,7 +37,7 @@ template <const int MAXV, const int MAXE, class unit, const bool SCALING> struct
     unit dfs(int v, int t, unit flow, unit lim, char r) {
         if (v == t || flow <= EPS) return flow;
         unit ret = 0;
-        for (int &i = cur[v]; i < st[v] + deg[v] && e[i].origCap > lim && e[i].isRev <= r; i++) {
+        for (int &i = cur[v]; i < st[v] + deg[v] && e[i].maxCap > lim && e[i].isRev <= r; i++) {
             if (e[i].cap > EPS && level[e[i].to] == level[v] + 1) {
                 unit res = dfs(e[i].to, t, min(flow, e[i].cap), lim, r);
                 if (res > EPS) {
@@ -54,14 +54,14 @@ template <const int MAXV, const int MAXE, class unit, const bool SCALING> struct
         stable_sort(ord, ord + E, [&] (const int &a, const int &b) {
             if (e[a].from != e[b].from) return e[a].from < e[b].from;
             if (e[a].isRev != e[b].isRev) return e[a].isRev < e[b].isRev;
-            return e[a].origCap > e[b].origCap;
+            return e[a].maxCap > e[b].maxCap;
         });
         for (int i = 0; i < E; i++) ind[ord[i]] = i;
         for (int i = 0; i < E; i++) e[i].rev = ind[e[i].rev];
         stable_sort(e, e + E, [&] (const Edge &a, const Edge &b) {
             if (a.from != b.from) return a.from < b.from;
             if (a.isRev != b.isRev) return a.isRev < b.isRev;
-            return a.origCap > b.origCap;
+            return a.maxCap > b.maxCap;
         });
         for (int v = 0, curSum = 0; v < V; v++) { st[v] = curSum; curSum += deg[v]; }
         for (char r = 1 - int(SCALING); r <= 1; r++) for (unit lim = SCALING ? maxCap : EPS; ; lim /= 2) {
