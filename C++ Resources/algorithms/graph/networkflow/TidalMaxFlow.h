8ee47e6db8700b4b4110a66ea01ab500955a8849
wesley-a-leung <wesley.a.leung@gmail.com>
2019-03-18 17:45:35 -0400

    Tidal flow update

--- C++ Resources/algorithms/graph/networkflow/TidalMaxFlow.h
+++ C++ Resources/algorithms/graph/networkflow/TidalMaxFlow.h
@@ -10,21 +10,21 @@ using namespace std;
 template <const int MAXV, const int MAXE, class unit, const bool SCALING> struct TidalMaxFlow {
     unit INF, EPS; TidalMaxFlow(unit INF, unit EPS) : INF(INF), EPS(EPS) {}
     struct Edge {
-        int from, to; unit origCap, cap, promised; int rev; char isRev; Edge() {}
-        Edge(int from, int to, unit origCap, unit cap, char isRev) :
-            from(from), to(to), origCap(origCap), cap(cap), isRev(isRev) {}
+        int from, to; unit origCap, cap, maxCap, promised; int rev; char isRev; Edge() {}
+        Edge(int from, int to, unit cap, unit maxCap, char isRev) :
+            from(from), to(to), origCap(cap), cap(cap), maxCap(maxCap), isRev(isRev) {}
     };
     int E, qesz, level[MAXV], q[MAXV], qe[MAXE * 2], st[MAXV], deg[MAXV], ord[MAXE * 2], ind[MAXE * 2];
     bool cut[MAXV]; Edge e[MAXE * 2]; unit maxFlow, minCut, maxCap, h[MAXV], pool[MAXV];
     void addEdge(int v, int w, unit vw, unit wv = 0) {
-        e[E++] = Edge(v, w, max(vw, wv), vw, 0); e[E++] = Edge(w, v, max(vw, wv), wv, 1);
+        e[E++] = Edge(v, w, vw, max(vw, wv), 0); e[E++] = Edge(w, v, wv, max(vw, wv), 1);
         e[E - 2].rev = E - 1; e[E - 1].rev = E - 2; deg[v]++; deg[w]++; maxCap = max(maxCap, max(vw, wv));
     }
     bool bfs(int V, int s, int t, unit lim, char r) {
         fill(level, level + V, -1); level[s] = 0; int front = 0, back = 0; qesz = 0; q[back++] = s;
         while (front < back) {
             int v = q[front++];
-            for (int i = st[v]; i < st[v] + deg[v] && e[i].origCap > lim && e[i].isRev <= r; i++) {
+            for (int i = st[v]; i < st[v] + deg[v] && e[i].maxCap > lim && e[i].isRev <= r; i++) {
                 if (e[i].cap > EPS && level[e[i].to] == -1) { level[e[i].to] = level[v] + 1; q[back++] = e[i].to; }
                 if (e[i].cap > EPS && level[e[i].to] == level[v] + 1) qe[qesz++] = i;
             }
@@ -33,21 +33,16 @@ template <const int MAXV, const int MAXE, class unit, const bool SCALING> struct
     }
     unit tideCycle(int V, int s, int t) {
         fill(h, h + V, 0); h[s] = INF; // high tide
-        for (int qind = 0; qind < qesz; qind++) {
-            Edge &ei = e[qe[qind]];
-            h[ei.to] += ei.promised = min(ei.cap, h[ei.from]);
-        }
+        for (int qind = 0; qind < qesz; qind++) { Edge &ei = e[qe[qind]]; h[ei.to] += ei.promised = min(ei.cap, h[ei.from]); }
         if (h[t] <= EPS) return 0;
         fill(pool, pool + V, 0); pool[t] = h[t]; // low tide
         for (int qind = qesz - 1; qind >= 0; qind--) {
-            Edge &ei = e[qe[qind]];
-            ei.promised = min(ei.promised, min(h[ei.from] - pool[ei.from], pool[ei.to]));
+            Edge &ei = e[qe[qind]]; ei.promised = min(ei.promised, min(h[ei.from] - pool[ei.from], pool[ei.to]));
             pool[ei.to] -= ei.promised; pool[ei.from] += ei.promised;
         }
         fill(h, h + V, 0); h[s] = pool[s]; // erosion
         for (int qind = 0; qind < qesz; qind++) {
-            Edge &ei = e[qe[qind]];
-            ei.promised = min(ei.promised, h[ei.from]); h[ei.from] -= ei.promised; h[ei.to] += ei.promised;
+            Edge &ei = e[qe[qind]]; ei.promised = min(ei.promised, h[ei.from]); h[ei.from] -= ei.promised; h[ei.to] += ei.promised;
             ei.cap -= ei.promised; e[ei.rev].cap += ei.promised;
         }
         return h[t];
@@ -58,14 +53,14 @@ template <const int MAXV, const int MAXE, class unit, const bool SCALING> struct
         stable_sort(ord, ord + E, [&] (const int &a, const int &b) {
             if (e[a].from != e[b].from) return e[a].from < e[b].from;
             if (e[a].isRev != e[b].isRev) return e[a].isRev < e[b].isRev;
-            return e[a].origCap > e[b].origCap;
+            return e[a].maxCap > e[b].maxCap;
         });
         for (int i = 0; i < E; i++) ind[ord[i]] = i;
         for (int i = 0; i < E; i++) e[i].rev = ind[e[i].rev];
         stable_sort(e, e + E, [&] (const Edge &a, const Edge &b) {
             if (a.from != b.from) return a.from < b.from;
             if (a.isRev != b.isRev) return a.isRev < b.isRev;
-            return a.origCap > b.origCap;
+            return a.maxCap > b.maxCap;
         });
         for (int v = 0, curSum = 0; v < V; v++) { st[v] = curSum; curSum += deg[v]; }
         for (char r = 1 - int(SCALING); r <= 1; r++) for (unit lim = SCALING ? maxCap : EPS; ; lim /= 2) {
