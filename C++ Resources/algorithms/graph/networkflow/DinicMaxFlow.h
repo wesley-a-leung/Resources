41a2892308c0c90496dbe1e07598b9d45bf35bc6
wesley-a-leung <wesley.a.leung@gmail.com>
2019-03-17 20:28:17 -0400

    Dinic with Scaling

--- C++ Resources/algorithms/graph/networkflow/DinicMaxFlow.h
+++ C++ Resources/algorithms/graph/networkflow/DinicMaxFlow.h
@@ -3,9 +3,12 @@
 using namespace std;
 
 // Computes the maximum flow and minimum cut in a flow network using Dinic's algorithm
-// Time Complexity: O(V^2 * E), O(min(V^(2/3), E^(1/2)) * E) for unit capacities
+// Time Complexity:
+//   O(V^2 * E), much faster in practice
+//   O(min(V^(2/3), E^(1/2)) * E) for unit capacities
+//   O(VE log U) with cost scaling, where U is the maximum capacity of any edge
 // Memory Complexity: O(V + E)
-template <const int MAXV, const int MAXE, class unit> struct DinicMaxFlow {
+template <const int MAXV, const int MAXE, class unit, const bool SCALING> struct DinicMaxFlow {
     unit INF, EPS; DinicMaxFlow(unit INF, unit EPS) : INF(INF), EPS(EPS) {}
     struct Edge {
         int from, to; unit origCap, cap; int rev; Edge() {}
@@ -16,12 +19,12 @@ template <const int MAXV, const int MAXE, class unit> struct DinicMaxFlow {
     void addEdge(int v, int w, unit vw, unit wv = 0) {
         e[E++] = Edge(v, w, vw); e[E++] = Edge(w, v, wv); e[E - 2].rev = E - 1; e[E - 1].rev = E - 2; deg[v]++; deg[w]++;
     }
-    bool bfs(int V, int s, int t) {
+    bool bfs(int V, int s, int t, unit lim) {
         fill(level, level + V, -1); level[s] = 0; int front = 0, back = 0; q[back++] = s;
         while (front < back && level[t] == -1) {
             int v = q[front++];
             for (int i = st[v]; i < st[v] + deg[v]; i++)
-                if (e[i].cap > EPS && level[e[i].to] == -1) { level[e[i].to] = level[v] + 1; q[back++] = e[i].to; }
+                if (level[e[i].to] == -1 && e[i].cap > lim) { level[e[i].to] = level[v] + 1; q[back++] = e[i].to; }
         }
         return level[t] != -1;
     }
@@ -40,9 +43,12 @@ template <const int MAXV, const int MAXE, class unit> struct DinicMaxFlow {
         for (int i = 0; i < E; i++) e[i].rev = ind[e[i].rev];
         stable_sort(e, e + E, [&] (const Edge &a, const Edge &b) { return a.from < b.from; });
         for (int v = 0, curSum = 0; v < V; v++) { st[v] = curSum; curSum += deg[v]; }
-        while (bfs(V, s, t)) {
-            copy(st, st + V, cur);
-            for (unit flow = dfs(s, t, INF); flow > EPS; flow = dfs(s, t, INF)) maxFlow += flow;
+        for (unit lim = SCALING ? INF : EPS; ; lim /= 2) {
+            while (bfs(V, s, t, lim)) {
+                copy(st, st + V, cur);
+                for (unit flow = dfs(s, t, INF); flow > EPS; flow = dfs(s, t, INF)) maxFlow += flow;
+            }
+            if (lim <= EPS) break;
         }
         return maxFlow;
     }
