465981917f9a5af541fc3b5fbd645ee98d4c653f
wesley-a-leung <wesley.a.leung@gmail.com>
2019-03-17 23:02:54 -0400

    Hashing now uses combinatorics

--- C++ Resources/algorithms/string/Hashing.h
+++ C++ Resources/algorithms/string/Hashing.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <bits/stdc++.h>
+#include "../math/Combinatorics.h"
 using namespace std;
 
 seed_seq seq {
@@ -19,9 +20,9 @@ template <const int MAXN, class T, const T MOD> struct Hashing {
     void compute(const string &s, T base = -1) {
         if (base == -1) base = BASE;
         int N = int(s.length()); POW[0] = 1; H[N] = 0;
-        for (int i = 1; i <= N; i++) POW[i] = POW[i - 1] * base % MOD;
-        for (int i = N - 1; i >= 0; i--) H[i] = (H[i + 1] * base + s[i] - 'a' + 1) % MOD;
+        for (int i = 1; i <= N; i++) POW[i] = mulMod(POW[i - 1], base, MOD);
+        for (int i = N - 1; i >= 0; i--) H[i] = addMod(mulMod(H[i + 1], base, MOD), T(s[i] - 'a' + 1), MOD);
     }
     // hash for substring [l, r)
-    T getHash(int l, int r) { return ((H[l] - H[r] * POW[r - l]) % MOD + MOD) % MOD; }
+    T getHash(int l, int r) { return subMod(H[l], mulMod(H[r] * POW[r - l], MOD), MOD); }
 };
