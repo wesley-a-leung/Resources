4174ddce597b0968aa46a9b7125d62e9f0d97d08
wesley-a-leung <wesley.a.leung@gmail.com>
2019-03-20 21:02:20 -0400

    ISM

---
+++ C++ Resources/algorithms/greedy/IntervalSchedulingMax.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+// Solves the interval scheduling maximization problem
+// Given a set of intervals [li, ri], find the maximum number of non overlapping intervals
+// Time Complexity: O(N log N)
+// Memory Complexity: O(N)
+template <const int MAXN, class T> struct IntervalSchedulingMax {
+    pair<T, T> intervals[MAXN]; int cnt; bool marked[MAXN];
+    int solve(int N) {
+        sort(intervals, intervals + N, [&] (const pair<T, T> &a, const pair<T, T> &b) { return a.second < b.second; });
+        fill(marked, marked + N, false); cnt = 0; T last = (numeric_limits<T>::min)();
+        for (int i = 0; i < N; i++) if (last < intervals[i].first) { cnt++; marked[i] = true; last = intervals[i].second; }
+        return cnt;
+    }
+};
