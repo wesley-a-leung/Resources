4b4213d2d3104f83dfd25d1d48d922d8a1dc1f8e
Wesley Leung <wesley.a.leung@gmail.com>
2019-03-22 11:39:59 -0400

    Update XorSat.h

--- C++ Resources/algorithms/math/XorSat.h
+++ C++ Resources/algorithms/math/XorSat.h
@@ -9,7 +9,7 @@ using namespace std;
 // Memory Complexity: O(N^2)
 template <const int MAXN> struct GaussianElimination {
     bool A[MAXN][MAXN], b[MAXN], x[MAXN];
-    void solve(int N) {
+    bool solve(int N) {
         for (int p = 0; p < N; p++) {
             int m = p;
             for (int i = 0; i < p; i++) {
@@ -27,9 +27,9 @@ template <const int MAXN> struct GaussianElimination {
         }
         copy(b, b + N, x);
         for (int i = N - 1; i >= 0; i--) {
-            if (!A[i][i] && x[i]) throw runtime_error("Matrix has no solution");
+            if (!A[i][i] && x[i]) return false; // no solution
             if (x[i]) for (int j = i - 1; j >= 0; j--) { x[j] ^= A[j][i]; A[j][i] = false; }
         }
-        return b;
+        return true;
     }
 };
