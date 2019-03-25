0ded647a4c2aac2dba7e81dd9e3b1459a0be048c
Wesley Leung <wesley.a.leung@gmail.com>
2019-03-25 14:58:17 -0400

    Update BidirectionalBreadthFirstSearch.h

--- C++ Resources/algorithms/graph/search/BidirectionalBreadthFirstSearch.h
+++ C++ Resources/algorithms/graph/search/BidirectionalBreadthFirstSearch.h
@@ -6,18 +6,18 @@ using namespace std;
 // Time Complexity: O(V + E)
 // Memory Complexity: O(V + E)
 template <const int MAXV> struct BidirectionalBFS {
-    int dist[MAXV], src[MAXV], to[MAXV], q[MAXV]; vector<int> adj[MAXV]; pair<int, int> edgeOnPath;
+    int dist[MAXV], to[MAXV], q[MAXV], vis[MAXV], stamp = 0; vector<int> adj[MAXV]; pair<int, int> edgeOnPath;
     void addBiEdge(int v, int w) { adj[v].push_back(w); adj[w].push_back(v); }
-    void clear(int V = MAXV) { for (int i = 0; i < MAXV; i++) adj[i].clear(); }
-    int bfs(int V, int s, int t) {
+    void clear(int V = MAXV) { stamp = 0; for (int i = 0; i < MAXV; i++) adj[i].clear(); }
+    int run(int V, int s, int t) {
         if (s == t) return 0;
-        fill(dist, dist + V, INT_MAX); fill(to, to + V, -1); int front = 0, back = 0;
-        dist[s] = dist[t] = 0; q[back++] = src[s] = s; q[back++] = src[t] = t;
+        if (stamp == 0) fill(vis, vis + V, stamp);
+        stamp++; int front = 0, back = 0; dist[s] = dist[t] = 0; q[back++] = s; q[back++] = t; vis[s] = stamp; vis[t] = -stamp;
         while (front < back) {
             int v = q[front++];
             for (int w : adj[v]) {
-                if (dist[w] == INT_MAX) { dist[w] = dist[v] + 1; src[w] = src[v]; to[w] = v; q[back++] = w; }
-                else if (src[v] != src[w]) { edgeOnPath = make_pair(v, w); return dist[v] + dist[w] + 1; }
+                if (vis[v] == -vis[w]) { edgeOnPath = make_pair(v, w); return dist[v] + dist[w] + 1; }
+                else if (vis[v] != vis[w]) { dist[w] = dist[v] + 1; vis[w] = vis[v]; to[w] = v; q[back++] = w; }
             }
         }
         return -1;
