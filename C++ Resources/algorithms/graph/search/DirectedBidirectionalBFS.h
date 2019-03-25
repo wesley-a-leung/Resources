fded8a5eff0238d706cf4d031f5ff2572c8db162
Wesley Leung <wesley.a.leung@gmail.com>
2019-03-25 15:01:38 -0400

    Update DirectedBidirectionalBFS.h

--- C++ Resources/algorithms/graph/search/DirectedBidirectionalBFS.h
+++ C++ Resources/algorithms/graph/search/DirectedBidirectionalBFS.h
@@ -2,22 +2,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Bidirectional Breadth First Search of a graph
+// Directed Bidirectional Breadth First Search of a graph
 // Time Complexity: O(V + E)
 // Memory Complexity: O(V + E)
 template <const int MAXV> struct DirectedBidirectionalBFS {
-    int dist[MAXV], src[MAXV], to[MAXV], q[MAXV]; vector<int> adj[MAXV][2]; pair<int, int> edgeOnPath;
-    void addEdge(int v, int w) { adj[v][0].push_back(w); adj[w][1].push_back(v); }
-    void clear(int V = MAXV) { for (int i = 0; i < MAXV; i++) adj[i].clear(); }
-    int bfs(int V, int s, int t) {
+    int dist[MAXV], to[MAXV], q[MAXV], vis[MAXV], stamp = 0; vector<int> adj[2][MAXV]; pair<int, int> edgeOnPath;
+    void addEdge(int v, int w) { adj[0][v].push_back(w); adj[1][w].push_back(v); }
+    void clear(int V = MAXV) { stamp = 0; for (int h = 0; h < 2; h++) for (int i = 0; i < MAXV; i++) adj[h][i].clear(); }
+    int run(int V, int s, int t) {
         if (s == t) return 0;
-        fill(dist, dist + V, INT_MAX); fill(to, to + V, -1); int front = 0, back = 0;
-        dist[s] = dist[t] = 0; q[back++] = src[s] = s; q[back++] = src[t] = t;
+        if (stamp == 0) fill(vis, vis + V, stamp);
+        stamp++; int front = 0, back = 0; dist[s] = dist[t] = 0; q[back++] = s; q[back++] = t; vis[s] = stamp; vis[t] = -stamp;
         while (front < back) {
             int v = q[front++];
-            for (int w : adj[v][src[v] == t]) {
-                if (dist[w] == INT_MAX) { dist[w] = dist[v] + 1; src[w] = src[v]; to[w] = v; q[back++] = w; }
-                else if (src[v] != src[w]) { edgeOnPath = make_pair(v, w); return dist[v] + dist[w] + 1; }
+            for (int w : adj[v][vis[v] < 0]) {
+                if (vis[v] == -vis[w]) { edgeOnPath = make_pair(v, w); return dist[v] + dist[w] + 1; }
+                else if (vis[v] != vis[w]) { dist[w] = dist[v] + 1; vis[w] = vis[v]; to[w] = v; q[back++] = w; }
             }
         }
         return -1;
