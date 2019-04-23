c2e0ad27bf4b22e86bb400316a5690961a222ac7
wesley-a-leung <wesley.a.leung@gmail.com>
2019-04-23 13:28:48 -0400

    Updated Transitive Closure

--- C++ Resources/algorithms/graph/search/TransitiveClosureSCC.h
+++ C++ Resources/algorithms/graph/search/TransitiveClosureSCC.h
@@ -5,9 +5,9 @@ using namespace std;
 
 // Computes the Transitive Closure in a directed graph
 // Time Complexity: O(V + E + VE / 64)
-// Memory Complexity: O(V^2)
-template <const int MAXV> struct TransistiveClosureSCC {
-    TarjanSCC<MAXV> SCC; bool G[MAXV][MAXV], vis[MAXV]; bitset<MAXV> dp[MAXV]; vector<int> DAG[MAXV];
+// Memory Complexity: O(V^2 / 64)
+template <const int MAXV> struct TransitiveClosureSCC {
+    TarjanSCC<MAXV> SCC; bool vis[MAXV]; bitset<MAXV> dp[MAXV];
     void addEdge(int v, int w) { SCC.addEdge(v, w); }
     void addBiEdge(int v, int w) { addEdge(v, w); addEdge(w, v); }
     void clear() { SCC.clear(); }
@@ -15,16 +15,13 @@ template <const int MAXV> struct TransistiveClosureSCC {
         if (vis[v]) return dp[v];
         vis[v] = true;
         for (int w : SCC.components[v]) dp[v][w] = 1;
-        for (int w : DAG[v]) dp[v] |= dfs(w);
+        for (int w : SCC.DAG[v]) dp[v] |= dfs(w);
         return dp[v];
     }
     void run(int V) {
         SCC.run(V);
-        for (int i = 0; i < int(SCC.components.size()); i++) {
-            DAG[i].clear(); dp[i].reset(); fill(G[i], G[i] + int(SCC.components.size()), false); vis[i] = false;
-        }
-        for (int v = 0; v < V; v++) for (int w : SCC.adj[v]) if (SCC.id[v] != SCC.id[w]) G[SCC.id[v]][SCC.id[w]] = true;
-        for (int i = 0; i < int(SCC.components.size()); i++) for (int j = 0; j < int(SCC.components.size()); j++) if (G[i][j]) DAG[i].push_back(j);
+        for (int i = 0; i < int(SCC.components.size()); i++) { dp[i].reset(); vis[i] = false; }
+        SCC.genDAG(V);
         for (int i = 0; i < int(SCC.components.size()); i++) dfs(i);
     }
     // is w reachable from v
