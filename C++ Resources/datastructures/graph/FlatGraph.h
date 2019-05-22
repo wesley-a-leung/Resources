303201fd8fb2a16bdad788992401d9486f27f5ca
wesley-a-leung <wesley.a.leung@gmail.com>
2019-05-22 16:19:27 -0400

    Variable renaming

--- C++ Resources/datastructures/graph/FlatGraph.h
+++ C++ Resources/datastructures/graph/FlatGraph.h
@@ -11,11 +11,11 @@ using namespace std;
 //   build: O(V + E)
 // Memory Complexity: O(V + E)
 template <const int MAXV, const int MAXE, class unit = int> struct FlatGraph {
-    int st[MAXV], _from[MAXE], _to[MAXE], to[MAXE], E; unit weight[MAXE], _weight[MAXE];
+    int st[MAXV], to[MAXE], A[MAXE], B[MAXE], E; unit weight[MAXE], C[MAXE];
     void init(int V) { fill(st, st + V + 1, 0); E = 0; }
-    void addEdge(int v, int w, unit cost = 1) { st[v]++; _from[E] = v; _to[E] = w; _weight[E++] = cost; }
+    void addEdge(int v, int w, unit cost = 1) { st[v]++; A[E] = v; B[E] = w; C[E++] = cost; }
     void build(int V) {
         partial_sum(st, st + V + 1, st);
-        for (int e = 0; e < E; e++) { to[--st[_from[e]]] = _to[e]; weight[st[_from[e]]] = _weight[e]; }
+        for (int e = 0; e < E; e++) { to[--st[A[e]]] = B[e]; weight[st[A[e]]] = C[e]; }
     }
 };
