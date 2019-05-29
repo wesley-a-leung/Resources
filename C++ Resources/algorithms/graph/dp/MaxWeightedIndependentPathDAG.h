d9c742ea5bfb3cdc16e34e430ec01f3ac63fdd12
wesley-a-leung <wesley.a.leung@gmail.com>
2019-05-29 00:03:21 -0400

    Updated MWIS

--- C++ Resources/algorithms/graph/dp/MaxWeightedIndependentPathDAG.h
+++ C++ Resources/algorithms/graph/dp/MaxWeightedIndependentPathDAG.h
@@ -6,13 +6,13 @@ using namespace std;
 // a source an (optional) destination vertex on a Directed Acyclic Graph
 // Time Complexity: O(V + E)
 // Memory Complexity: O(V + E)
-template <const int MAXV> struct MaxWeightedIndependentPathDAG {
-    vector<int> adj[MAXV]; int val[MAXV], dp[MAXV][2];
+template <const int MAXV, class T> struct MaxWeightedIndependentPathDAG {
+    vector<int> adj[MAXV]; T val[MAXV], dp[MAXV][2];
     void addEdge(int v, int w) { adj[v].push_back(w); }
-    int dfs(int v, int t, bool take) {
+    T dfs(int v, int t, bool take) {
         if (v == t) return take ? val[v] : 0;
-        if (dp[v][take] != -1) return dp[v][take];
-        int ret = INT_MIN;
+        if (dp[v][take] != (numeric_limits<T>::max)()) return dp[v][take];
+        T ret = (numeric_limits<T>::min)();
         for (int w : adj[v]) {
             if (!take) ret = max(ret, dfs(w, true));
             ret = max(ret, dfs(w, false));
@@ -20,8 +20,8 @@ template <const int MAXV> struct MaxWeightedIndependentPathDAG {
         return take ? (dp[v][take] = ret + val[v]) : (dp[v][take] = ret);
     }
     void clear(int V = MAXV) { for (int i = 0; i < V; i++) adj[i].clear(); }
-    int solve(int V, int s, int t) {
-        for (int i = 0; i < V; i++) dp[i][0] = dp[i][1] = -1;
+    T solve(int V, int s, int t) {
+        for (int i = 0; i < V; i++) dp[i][0] = dp[i][1] = (numeric_limits<T>::max)();
         return dfs(s, t, true);
     }
 };
