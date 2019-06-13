03688084bbd8bab29f27f2b420945be1c2f74baa
wesley-a-leung <wesley.a.leung@gmail.com>
2019-06-13 10:46:07 -0400

    Interpolation search update

--- C++ Resources/algorithms/search/InterpolationSearch.h
+++ C++ Resources/algorithms/search/InterpolationSearch.h
@@ -8,12 +8,13 @@ using namespace std;
 // Time Complexity: O(log log (hi - lo)) * (cost to compute f(x))
 template <class T, class F> T interpolationSearch(T lo, T hi, F f, T key) {
     hi--;
-    while (lo < hi) {
+    while (lo < hi && f(lo) != f(hi) && f(lo) <= key && key <= f(hi)) {
         T guess = ((key - f(lo)) / f(hi) - f(lo)) * (hi - lo) + lo;
         auto fguess = f(guess);
         if (fguess < key) lo = guess + 1;
         else if (fguess > key) hi = guess - 1;
         else return guess;
     }
+    if (key == f(lo)) return lo;
     return -1;
 }
