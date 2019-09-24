649d396f36167e4057df8ebe1d97a20be9738153
wesley-a-leung <wesley.a.leung@gmail.com>
2019-09-23 21:36:15 -0400

    Updated FenwickTreeMax

--- C++ Resources/datastructures/trees/FenwickTreeMax1D.h
+++ C++ Resources/datastructures/trees/FenwickTreeMax1D.h
@@ -9,7 +9,8 @@ using namespace std;
 // Memory Complexity: O(N)
 template <const int MAXN, class T, const bool ONE_INDEXED> struct FenwickTreeMax1D {
     T BIT[MAXN], NEG_INF;
+    T op(T a, T b) { return max(a, b); }
     FenwickTreeMax1D(T NEG_INF) : NEG_INF(NEG_INF) { fill(BIT, BIT + MAXN, NEG_INF); }
-    void update(int i, T v) { for (i += !ONE_INDEXED; i < MAXN; i += i & -i) BIT[i] = max(BIT[i], v); }
-    T rmq(int i) { T ret = NEG_INF; for (i += !ONE_INDEXED; i > 0; i -= i & -i) ret = max(ret, BIT[i]); return ret; }
+    void update(int i, T v) { for (i += !ONE_INDEXED; i < MAXN; i += i & -i) BIT[i] = op(BIT[i], v); }
+    T rmq(int i) { T ret = NEG_INF; for (i += !ONE_INDEXED; i > 0; i -= i & -i) ret = op(ret, BIT[i]); return ret; }
 };
--- C++ Resources/datastructures/trees/FenwickTreeMaxPointRange1D.h
+++ C++ Resources/datastructures/trees/FenwickTreeMaxPointRange1D.h
@@ -10,14 +10,15 @@ using namespace std;
 // Memory Complexity: O(N)
 template <const int MAXN, class T, const bool ONE_INDEXED> struct FenwickTreeMaxPointRange1D {
     T A[MAXN], BIT[MAXN], NEG_INF;
+    T op(T a, T b) { return max(a, b); }
     FenwickTreeMaxPointRange1D(T NEG_INF) : NEG_INF(NEG_INF) { fill(A, A + MAXN, NEG_INF); fill(BIT, BIT + MAXN, NEG_INF); }
-    void update(int i, T v) { for (i += !ONE_INDEXED, A[i] = max(A[i], v); i < MAXN; i += i & -i) BIT[i] = max(BIT[i], v); }
-    T rmq(int i) { T ret = NEG_INF; for (i += !ONE_INDEXED; i > 0; i -= i & -i) ret = max(ret, BIT[i]); return ret; }
+    void update(int i, T v) { for (i += !ONE_INDEXED, A[i] = op(A[i], v); i < MAXN; i += i & -i) BIT[i] = op(BIT[i], v); }
+    T rmq(int i) { T ret = NEG_INF; for (i += !ONE_INDEXED; i > 0; i -= i & -i) ret = op(ret, BIT[i]); return ret; }
     T rmq(int l, int r) {
         T ret = NEG_INF; l += !ONE_INDEXED; r += !ONE_INDEXED;
         while (l <= r) {
-            if (l <= r - (r & -r) + 1) { ret = max(ret, BIT[r]); r -= r & -r; }
-            else ret = max(ret, A[r--]);
+            if (l <= r - (r & -r) + 1) { ret = op(ret, BIT[r]); r -= r & -r; }
+            else ret = op(ret, A[r--]);
         }
         return ret;
     }
