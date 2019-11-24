cef789f44862c10c0459b606a5c35e84691492f0
wesley-a-leung <wesley.a.leung@gmail.com>
2019-11-23 19:23:20 -0500

    Boyer Moore Majority Algorithm

--- C++ Resources/algorithms/math/BisectionRootFinding.h
+++ C++ Resources/algorithms/math/BisectionRootFinding.h
@@ -4,7 +4,7 @@ using namespace std;
 
 // Finds the root of a function using a bisection search
 // Time Complexity: O(log(hi - lo)) * (cost to compute f(x)) * iters
-template <class T, class F> T bisection_root_finding(T lo, T hi, F f, int iters) {
+template <class T, class F> T bisectionSearch(T lo, T hi, F f, int iters) {
     T mid;
     for (int it = 0; it < iters; it++) {
         mid = lo + (hi - lo) / 2;
