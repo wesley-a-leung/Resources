c3cd89f4f577c40f93271c5ee0e45e775d179b63
wesley-a-leung <wesley.a.leung@gmail.com>
2019-11-25 18:24:15 -0500

    Updated Dynamic Connectivity

--- C++ Resources/algorithms/graph/components/DynamicConnectivitySqrtDecomp.h
+++ C++ Resources/algorithms/graph/components/DynamicConnectivitySqrtDecomp.h
@@ -1,63 +1,37 @@
 #pragma once
 #include <bits/stdc++.h>
+#include "../../../datastructures/UnionFindUndo.h"
 using namespace std;
 
 // Support queries for the number of components in a graph, after edges have been added or removed
 // Sqrt Decomposition Solution
-// Time Complexity: O(V * (Q / B) + Q * B)
+// Time Complexity: O((Q / B) * (V + Q + B^2))
 // Memory Complexity: O(V + Q)
 template <const int MAXV, const int MAXQ, const int BLOCKSZ> struct DynamicConnectivitySqrtDecomp {
-    int Q = 0, vis[MAXV], root[MAXV]; vector<int> ans; unordered_set<int> adj[MAXV], toRem[MAXV]; unordered_map<int, int> adj2[MAXV];
-    struct Query { int type, v, w; } q[MAXQ];
-    void dfs1(int v, int r) {
-        root[v] = r;
-        for (int w : adj[v]) if (root[w] == -1 && !toRem[v].count(w)) dfs1(w, r);
+    int Q = 0; vector<int> ans; unordered_map<int, int> present[MAXV]; UnionFindUndo<MAXV, 0> uf;
+    struct Query { int type, v, w, otherTime; } q[MAXQ];
+    void clear(int V = MAXV) { ans.clear(); Q = 0; uf.clearHistory(); for (int i = 0; i < V; i++) present[i].clear(); }
+    void addEdge(int v, int w) {
+        if (v > w) swap(v, w);
+        present[v][w] = Q; q[Q++] = {1, v, w, INT_MAX};
     }
-    bool dfs2(int v, int t, int i) {
-        if (v == t) return true;
-        vis[v] = i;
-        for (auto && e : adj2[v]) if (vis[e.first] != i && dfs2(e.first, t, i)) return true;
-        return false;
+    void removeEdge(int v, int w) {
+        if (v > w) swap(v, w);
+        int insTime = present[v][w]; q[Q] = {-1, v, w, insTime}; q[insTime].otherTime = Q++; present[v].erase(w);
     }
-    void clear(int V = MAXV) { ans.clear(); Q = 0; for (int i = 0; i < V; i++) { adj[i].clear(); toRem[i].clear(); adj2[i].clear(); } }
-    void addEdge(int v, int w) { q[Q++] = {1, v, w}; }
-    void removeEdge(int v, int w) { q[Q++] = {-1, v, w}; }
-    void query() { q[Q++] = {0, -1, -1}; }
+    void query() { q[Q] = {0, -1, -1, Q}; Q++; }
     void solve(int V) {
-        int cnt = V;
-        fill(vis, vis + MAXV, -1);
         for (int st = 0; st < Q; st += BLOCKSZ) {
-            fill(root, root + V, -1);
-            for (int i = st; i < min(st + BLOCKSZ, Q); i++) {
-                if (q[i].type != -1) continue;
-                toRem[q[i].v].insert(q[i].w); toRem[q[i].w].insert(q[i].v);
+            int en = min(Q, st + BLOCKSZ); uf.init(V);
+            for (int i = 0; i < st; i++) if (q[i].type == 1 && q[i].otherTime >= en) uf.join(q[i].v, q[i].w);
+            for (int i = st; i < en; i++) if (q[i].type == 0) {
+                int curSize = uf.history.size();
+                for (int j = st; j <= i; j++) if (q[j].type == 1 && q[j].otherTime > i) uf.join(q[j].v, q[j].w);
+                for (int j = i + 1; j < en; j++) if (q[j].type == -1 && q[j].otherTime < st) uf.join(q[j].v, q[j].w);
+                ans.push_back(uf.cnt);
+                while (int(uf.history.size()) > curSize) uf.undo();
             }
-            for (int v = 0; v < V; v++) {
-                if (root[v] == -1) dfs1(v, v);
-                adj2[v].clear();
-            }
-            for (int v = 0; v < V; v++) {
-                for (int w : adj[v]) if (root[v] != root[w]) adj2[root[v]][root[w]]++;
-                toRem[v].clear();
-            }
-            for (int i = st; i < min(st + BLOCKSZ, Q); i++) {
-                if (q[i].type == 1) {
-                    adj[q[i].v].insert(q[i].w); adj[q[i].w].insert(q[i].v);
-                    int rv = root[q[i].v], rw = root[q[i].w];
-                    if (rv != rw) {
-                        if (!dfs2(rv, rw, i)) cnt--;
-                        adj2[rv][rw]++; adj2[rw][rv]++;
-                    }
-                } else if (q[i].type == -1) {
-                    adj[q[i].v].erase(q[i].w); adj[q[i].w].erase(q[i].v);
-                    int rv = root[q[i].v], rw = root[q[i].w];
-                    if (rv != rw) {
-                        if (--adj2[rv][rw] == 0) adj2[rv].erase(rw);
-                        if (--adj2[rw][rv] == 0) adj2[rw].erase(rv);
-                        if (!dfs2(rv, rw, i)) cnt++;
-                    }
-                } else ans.push_back(cnt);
-            }
+            uf.clearHistory();
         }
     }
 };
