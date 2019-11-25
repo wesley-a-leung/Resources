56e927f4d54344bacf950d4fa929a76e0466e1d4
wesley-a-leung <wesley.a.leung@gmail.com>
2019-11-24 22:12:20 -0500

    Updated Gabow

--- C++ Resources/algorithms/graph/mst/GabowMinArborescence.h
+++ C++ Resources/algorithms/graph/mst/GabowMinArborescence.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <bits/stdc++.h>
 #include "../../../datastructures/IncrementalSkewHeap.h"
+#include "../../../datastructures/UnionFind.h"
 using namespace std;
 
 // Computes the minimum arborescence, or directed minimum spanning tree using Gabow's variant of Edmonds' algorithm
@@ -15,16 +16,10 @@ template <const int MAXV, class unit> struct GabowMinArborescence {
         bool operator > (const Edge &e) const { return weight > e.weight; }
         Edge operator + (const unit &add) const { Edge ret; ret.from = from; ret.to = to; ret.weight = weight + add; return ret; }
     };
-    int UF[MAXV], vis[MAXV]; vector<Edge> edges; unit weight; vector<IncrementalSkewHeap<Edge, greater<Edge>, unit>> H;
+    int vis[MAXV]; vector<Edge> edges; UnionFind<MAXV, 0> uf; unit weight; vector<IncrementalSkewHeap<Edge, greater<Edge>, unit>> H;
     void addEdge(int from, int to, unit weight) { edges.push_back({from, to, weight}); }
-    int find(int v) { return UF[v] < 0 ? v : UF[v] = find(UF[v]); }
-    bool join(int v, int w) {
-        if ((v = find(v)) == (w = find(w))) return false;
-        if (UF[v] > UF[w]) swap(v, w);
-        UF[v] += UF[w]; UF[w] = v; return true;
-    }
     unit run(int V, int root) {
-        weight = 0; fill(UF, UF + V, -1); fill(vis, vis + V, -1); vis[root] = root;
+        weight = 0; fill(vis, vis + V, -1); vis[root] = root; uf.init(V);
         for (int v = 0; v < V; v++) H.emplace_back(0); 
         for (auto &&e : edges) H[e.to].push(e);
         for (int s = 0; s < V; s++) {
@@ -32,14 +27,14 @@ template <const int MAXV, class unit> struct GabowMinArborescence {
             for (int v = s; vis[v] == -1;) {
                 path.push(v); vis[v] = s;
                 if (H[v].empty()) return INF;
-                Edge minEdge = H[v].top(); int w = find(minEdge.from);
+                Edge minEdge = H[v].top(); int w = uf.find(minEdge.from);
                 weight += minEdge.weight; H[v].increment(-minEdge.weight); H[v].pop();
                 if (vis[w] == s) {
                     IncrementalSkewHeap<Edge, greater<Edge>, unit> temp(0); int x;
-                    do { x = path.top(); path.pop(); temp.merge(H[x]); } while (join(w, x));
-                    H[find(w)] = move(temp); vis[find(w)] = -1;
+                    do { x = path.top(); path.pop(); temp.merge(H[x]); } while (uf.join(w, x));
+                    H[uf.find(w)] = move(temp); vis[uf.find(w)] = -1;
                 }
-                v = find(w);
+                v = uf.find(w);
             }
         }
         return weight;
