2b1436603e0bceda26e39a574051982994c84e3f
wesley-a-leung <wesley.a.leung@gmail.com>
2019-11-27 01:38:32 -0500

    Max Flow with Demands

--- C++ Resources/algorithms/graph/networkflow/PushRelabelMaxFlow.h
+++ C++ Resources/algorithms/graph/networkflow/PushRelabelMaxFlow.h
@@ -8,38 +8,38 @@ using namespace std;
 // Memory Complexity: O(V + E)
 template <const int MAXV, class unit> struct PushRelabelMaxFlow {
     struct Edge {
-        int to; unit origCap, cap; int rev;
-        Edge(int to, unit cap, int rev) : to(to), origCap(cap), cap(cap), rev(rev) {}
+        int to; unit cap, resCap; int rev;
+        Edge(int to, unit cap, int rev) : to(to), cap(cap), resCap(cap), rev(rev) {}
     };
-    unit EPS, maxFlow, minCut, ex[MAXV]; PushRelabelMaxFlow(unit EPS) : EPS(EPS) {}
+    unit EPS, maxFlow, ex[MAXV]; PushRelabelMaxFlow(unit EPS) : EPS(EPS) {}
     int h[MAXV], cnt[MAXV * 2]; bool cut[MAXV]; vector<int> hs[MAXV * 2]; vector<Edge> adj[MAXV]; typename vector<Edge>::iterator cur[MAXV];
     void addEdge(int v, int w, unit vw, unit wv = 0) {
         if (v == w) return;
         adj[v].emplace_back(w, vw, int(adj[w].size())); adj[w].emplace_back(v, wv, int(adj[v].size()) - 1);
     }
-    void init(int V = MAXV) { fill(cut, cut + V, false); for (int i = 0; i < V; i++) adj[i].clear(); }
+    void init(int V) { fill(cut, cut + V, false); for (int i = 0; i < V; i++) adj[i].clear(); }
     void push(int v, Edge &e, unit df) {
         int w = e.to;
         if (abs(ex[w]) <= EPS && df > EPS) hs[h[w]].push_back(w);
-        e.cap -= df; adj[w][e.rev].cap += df; ex[v] -= df; ex[w] += df;
+        e.resCap -= df; adj[w][e.rev].resCap += df; ex[v] -= df; ex[w] += df;
     }
     unit getFlow(int V, int s, int t) {
         if (s == t) return maxFlow = 0;
         fill(h, h + V, 0); h[s] = V; fill(ex, ex + V, 0); ex[t] = 1; fill(cnt, cnt + V * 2, 0); cnt[0] = V - 1;
         for (int v = 0; v < V; v++) cur[v] = adj[v].begin();
         for (int i = 0; i < V * 2; i++) hs[i].clear();
-        for (auto &&e : adj[s]) push(s, e, e.cap);
+        for (auto &&e : adj[s]) push(s, e, e.resCap);
         if (!hs[0].empty()) for (int hi = 0; hi >= 0;) {
             int v = hs[hi].back(); hs[hi].pop_back();
             while (ex[v] > EPS) {
                 if (cur[v] == adj[v].end()) {
                     h[v] = INT_MAX;
                     for (auto e = adj[v].begin(); e != adj[v].end(); e++)
-                        if (e->cap > EPS && h[v] > h[e->to] + 1) { h[v] = h[e->to] + 1; cur[v] = e; }
+                        if (e->resCap > EPS && h[v] > h[e->to] + 1) { h[v] = h[e->to] + 1; cur[v] = e; }
                     cnt[h[v]]++;
                     if (--cnt[hi] == 0 && hi < V) for (int i = 0; i < V; i++) if (hi < h[i] && h[i] < V) { cnt[h[i]]--; h[i] = V + 1; }
                     hi = h[v];
-                } else if (cur[v]->cap > EPS && h[v] == h[cur[v]->to] + 1) push(v, *cur[v], min(ex[v], cur[v]->cap));
+                } else if (cur[v]->resCap > EPS && h[v] == h[cur[v]->to] + 1) push(v, *cur[v], min(ex[v], cur[v]->resCap));
                 else cur[v]++;
             }
             while (hi >= 0 && hs[hi].empty()) hi--;
@@ -48,11 +48,11 @@ template <const int MAXV, class unit> struct PushRelabelMaxFlow {
     }
     void inferMinCutDfs(int v) {
         cut[v] = true;
-        for (auto &&e : adj[v]) if (e.cap > EPS && !cut[e.to]) inferMinCutDfs(e.to);
+        for (auto &&e : adj[v]) if (e.resCap > EPS && !cut[e.to]) inferMinCutDfs(e.to);
     }
     unit inferMinCut(int V, int s) {
-        inferMinCutDfs(s); minCut = 0;
-        for (int v = 0; v < V; v++) if (cut[v]) for (auto &&e : adj[v]) if (!cut[e.to]) minCut += e.origCap;
+        inferMinCutDfs(s); unit minCut = 0;
+        for (int v = 0; v < V; v++) if (cut[v]) for (auto &&e : adj[v]) if (!cut[e.to]) minCut += e.cap;
         return minCut;
     }
 };
--- C++ Resources/algorithms/graph/networkflow/SAPMinCostMaxFlow.h
+++ C++ Resources/algorithms/graph/networkflow/SAPMinCostMaxFlow.h
@@ -12,29 +12,29 @@ template <const int MAXV, const int MAXE, class flowUnit, class costUnit> struct
     using heap = __gnu_pbds::priority_queue<pair<costUnit, int>, greater<pair<costUnit, int>>, pairing_heap_tag>;
     SAPMinCostMaxFlow(flowUnit FLOW_INF, flowUnit FLOW_EPS, costUnit COST_INF) : FLOW_INF(FLOW_INF), FLOW_EPS(FLOW_EPS), COST_INF(COST_INF) {}
     struct Edge {
-        int from, to; flowUnit origCap, cap; costUnit origCost, cost; int ind, rev; Edge() {}
-        Edge(int from, int to, flowUnit cap, costUnit cost) : from(from), to(to), origCap(cap), cap(cap), origCost(cost), cost(cost) {}
+        int from, to; flowUnit cap, resCap; costUnit cost; int ind, rev; Edge() {}
+        Edge(int from, int to, flowUnit cap, costUnit cost) : from(from), to(to), cap(cap), resCap(cap), cost(cost) {}
         bool operator < (const Edge &other) const { return from < other.from; }
     };
-    int E, prev[MAXV], index[MAXV], st[MAXV], deg[MAXV], ind[MAXE * 2]; Edge e[MAXE * 2];
+    int E, prev[MAXV], index[MAXV], st[MAXV], ind[MAXE * 2]; Edge e[MAXE * 2];
     flowUnit maxFlow; costUnit phi[MAXV], dist[MAXV], minCost; bool hasNegativeEdgeCost; typename heap::point_iterator ptr[MAXV];
     void addEdge(int v, int w, flowUnit flow, costUnit cost) {
         if (cost < 0) hasNegativeEdgeCost = true;
-        e[E++] = Edge(v, w, flow, cost); e[E++] = Edge(w, v, 0, -cost); deg[v]++; deg[w]++;
+        e[E++] = Edge(v, w, flow, cost); e[E++] = Edge(w, v, 0, -cost); st[v + 1]++; st[w + 1]++;
         e[E - 2].ind = E - 2; e[E - 1].ind = E - 1; e[E - 2].rev = E - 1; e[E - 1].rev = E - 2;
     }
     void bellmanFord(int V, int s, int t) {
         fill(phi, phi + V, COST_INF); phi[s] = 0;
         for (int j = 0; j < V - 1; j++) for (int i = 0; i < E; i++)
-            if (e[i].cap > FLOW_EPS) phi[e[i].to] = min(phi[e[i].to], phi[e[i].from] + e[i].cost);
+            if (e[i].resCap > FLOW_EPS) phi[e[i].to] = min(phi[e[i].to], phi[e[i].from] + e[i].cost);
     }
     bool dijkstra(int V, int s, int t) {
         fill(dist, dist + V, COST_INF); fill(prev, prev + V, -1); fill(index, index + V, -1);
         heap PQ; fill(ptr, ptr + V, PQ.end()); ptr[s] = PQ.push({dist[s] = 0, s});
         while (!PQ.empty()) {
             int v = PQ.top().second; PQ.pop();
-            for (int i = st[v]; i < st[v] + deg[v]; i++) {
-                if (e[i].cap <= FLOW_EPS) continue;
+            for (int i = st[v]; i < st[v + 1]; i++) {
+                if (e[i].resCap <= FLOW_EPS) continue;
                 int w = e[i].to; costUnit d = dist[v] + e[i].cost + phi[v] - phi[w];
                 if (dist[w] <= d) continue;
                 prev[w] = v; index[w] = i;
@@ -44,19 +44,18 @@ template <const int MAXV, const int MAXE, class flowUnit, class costUnit> struct
         }
         return dist[t] != COST_INF;
     }
-    void init(int V = MAXV) { E = 0; hasNegativeEdgeCost = false; fill(deg, deg + V, 0); }
+    void init(int V) { E = 0; hasNegativeEdgeCost = false; fill(st, st + V + 1, 0); }
     pair<flowUnit, costUnit> getMaxFlowMinCost(int V, int s, int t) {
-        maxFlow = 0; minCost = 0; fill(phi, phi + V, 0); sort(e, e + E);
+        maxFlow = 0; minCost = 0; fill(phi, phi + V, 0); sort(e, e + E); partial_sum(st, st + V + 1, st);
         for (int i = 0; i < E; i++) ind[e[i].ind] = i;
-        for (int i = 0; i < E; i++) e[i].rev = ind[e[i].rev];
-        for (int v = 0, curSum = 0; v < V; v++) { st[v] = curSum; curSum += deg[v]; }
+        for (int i = 0; i < E; i++) e[e[i].ind = i].rev = ind[e[i].rev];
         if (hasNegativeEdgeCost) bellmanFord(V, s, t);
         while (dijkstra(V, s, t)) {
             flowUnit aug = FLOW_INF; int cur = t;
-            while (prev[cur] != -1) { aug = min(aug, e[index[cur]].cap); cur = prev[cur]; }
+            while (prev[cur] != -1) { aug = min(aug, e[index[cur]].resCap); cur = prev[cur]; }
             maxFlow += aug; cur = t;
             while (prev[cur] != -1) {
-                e[index[cur]].cap -= aug; e[e[index[cur]].rev].cap += aug; minCost += aug * e[index[cur]].origCost; cur = prev[cur];
+                e[index[cur]].resCap -= aug; e[e[index[cur]].rev].resCap += aug; minCost += aug * e[index[cur]].cost; cur = prev[cur];
             }
             for (int v = 0; v < V; v++) if (dist[v] != COST_INF) phi[v] += dist[v];
         }
