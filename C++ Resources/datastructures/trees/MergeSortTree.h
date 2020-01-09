d7f5e89926814b3042f395b5fb072cc67e09df38
wesley-a-leung <wesley.a.leung@gmail.com>
2020-01-09 17:06:04 -0500

    MergeSortTree

---
+++ C++ Resources/datastructures/trees/MergeSortTree.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+// Merge Sort Tree supporting rank operations for a subarray
+// Time Complexity:
+//   init: O(N log N)
+//   rank: O((log N)^2)
+// Memory Complexity: O(N log N)
+template <const int MAXN, class T, class Comparator = less<T>> struct MergeSortTree {
+    Comparator cmp; int N; vector<T> TR[MAXN << 1];
+    template <class It> void init(It st, It en) {
+        N = en - st; for (int i = 0; i < N; i++) { TR[N + i] = vector<T>(1, *(st + i)); }
+        for (int i = N - 1; i > 0; i--) {
+            TR[i] = vector<T>(); TR[i].reserve(TR[i << 1].size() + TR[i << 1 | 1].size());
+            merge(TR[i << 1].begin(), TR[i << 1].end(), TR[i << 1 | 1].begin(), TR[i << 1 | 1].end(), back_inserter(TR[i]), cmp);
+        }
+    }
+    int rank(int l, int r, T k) { // counts the number of elements less than k in the range [l, r] (0-indexed)
+        int ret = 0;
+        for (l += N, r += N; l <= r; l >>= 1, r >>= 1) {
+            if (l & 1) { ret += lower_bound(TR[l].begin(), TR[l].end(), k, cmp) - TR[l].begin(); l++; }
+            if (!(r & 1)) { ret += lower_bound(TR[r].begin(), TR[r].end(), k, cmp) - TR[r].begin(); r--; }
+        }
+        return ret;
+    }
+};
