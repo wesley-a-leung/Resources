b218caa9ec79ae51e42313b5e99b83ee8178a891
wesley-a-leung <wesley.a.leung@gmail.com>
2020-01-16 02:06:13 -0500

    Updated Treap

--- C++ Resources/datastructures/trees/ImplicitTreapLazy.h
+++ C++ Resources/datastructures/trees/ImplicitTreapLazy.h
@@ -8,12 +8,13 @@ using namespace std;
 //   updateVal, queryVal, updateRange, queryRange, reverseRange: O(log N)
 // Memory Complexity: O(N)
 
-#define STORE_PARENT 1
 seed_seq seq {
     (uint64_t)chrono::duration_cast<chrono::nanoseconds>(chrono::high_resolution_clock::now().time_since_epoch()).count(),
     (uint64_t)__builtin_ia32_rdtsc(),(uint64_t)(uintptr_t)make_unique<char>().get()
 };
-mt19937_64 rng64(seq); uniform_int_distribution<long long> dis;
+mt19937_64 rng64(seq);
+uniform_int_distribution<long long> dis;
+#define STORE_PARENT 1
 using Data = int; using Lazy = int; const Data vdef = 0, qdef = 0; const Lazy ldef = 0;
 Data merge(const Data &l, const Data &r); // to be implemented
 Lazy getSegmentVal(const Lazy &v, int k); // to be implemented
