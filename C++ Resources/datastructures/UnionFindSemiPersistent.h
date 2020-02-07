ab72af7b2b3331f035f1682a5f03deeba780e485
wesley-a-leung <wesley.a.leung@gmail.com>
2020-02-07 13:49:27 -0500

    Updated UnionFindSemiPersistent

--- C++ Resources/datastructures/UnionFindSemiPersistent.h
+++ C++ Resources/datastructures/UnionFindSemiPersistent.h
@@ -6,6 +6,7 @@ using namespace std;
 // Time Complexity:
 //   init: O(N)
 //   find, join, connected, getSize, getCnt: O(log N)
+//   query: O(log N log Q) for Q join operations
 // Memory Complexity: O(N)
 template <const int MAXN, const bool ONE_INDEXED> struct UnionFindSemiPersistent {
     vector<pair<int, int>> UF[MAXN], cnt; int curTime;
@@ -25,4 +26,16 @@ template <const int MAXN, const bool ONE_INDEXED> struct UnionFindSemiPersistent
     bool connected(int t, int v, int w) { return find(t, v) == find(t, w); }
     int getSize(int t, int v) { v = find(t, v); return -prev(upper_bound(UF[v].begin(), UF[v].end(), make_pair(t, INT_MAX)))->second; }
     int getCnt(int t) { return prev(upper_bound(cnt.begin(), cnt.end(), make_pair(t, INT_MAX)))->second; }
+    // returns the first time i (0-indexed) such that v and w are in the same connected component
+    // after the ith join
+    // returns -1 if v = w and curTime + 1 if they are never in the same connected component
+    int query(int v, int w) {
+        int lo = -1, hi = curTime + 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (connected(mid, v, w)) hi = mid;
+            else lo = mid + 1;
+        }
+        return lo;
+    }
 };
