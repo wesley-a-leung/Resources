30dc7b792bf76f7b4b5394faf841a85f5c0ee015
wesley-a-leung <wesley.a.leung@gmail.com>
2020-02-24 17:01:16 -0500

    Updated Karatsuba

--- C++ Resources/algorithms/math/Karatsuba.h
+++ C++ Resources/algorithms/math/Karatsuba.h
@@ -6,39 +6,35 @@ using namespace std;
 // Karatsuba Multiplication
 // Time Complexity of multiplyInteger, multiplyPolynomial: O(N ^ log_2(3)) where N = size(a) + size(b)
 
-const int CUTOFF = 32, DIG = 1;
+const int CUTOFF = 64, DIG = 1;
 
-template <class T> vector<T> karatsuba(const vector<T> &a, const vector<T> &b) {
-    int n = int(a.size());
+template <class T, class ItA, class ItB, class ItRes> void karatsuba(int n, ItA a, ItB b, ItRes res) {
     if (n <= CUTOFF) {
-        vector<T> res(n + n);
+        fill(res, res + n * 2, 0);
         for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) res[i + j] += a[i] * b[j];
-        return res;
+        return;
     }
-    int k = n >> 1; vector<T> a2(a.begin() + k, a.end()), b2(b.begin() + k, b.end()), a2b2 = karatsuba(a2, b2);
-    for (int i = 0; i < k; i++) { a2[i] += a[i]; b2[i] += b[i]; }
-    vector<T> r = karatsuba(a2, b2), a1b1 = karatsuba(vector<T>(a.begin(), a.begin() + k), vector<T>(b.begin(), b.begin() + k)), res(n << 1);
-    for (int i = 0; i < int(r.size()); i++) { res[i] += a1b1[i]; res[i + k] += r[i] - a1b1[i] - a2b2[i]; res[i + n] += a2b2[i]; }
-    return res;
+    int k = n / 2; vector<T> tmp(n, 0), c(n, 0); auto atmp = tmp.begin(), btmp = atmp + k;
+    for (int i = 0; i < k; i++) { atmp[i] = a[i] + a[i + k]; btmp[i] = b[i] + b[i + k]; }
+    karatsuba<T>(k, atmp, btmp, c.begin()); karatsuba<T>(k, a, b, res); karatsuba<T>(k, a + k, b + k, res + n);
+    for (int i = 0; i < k; i++) { T t = res[i + k]; res[i + k] += c[i] - res[i] - res[i + k * 2]; res[i + k * 2] += c[i + k] - t - res[i + k * 3]; }
 }
 
 // Multiplies 2 big integers
-template <class T> vector<T> multiplyInteger(vector<T> &a, vector<T> &b) {
+template <class T> void multiplyInteger(vector<T> a, vector<T> b, vector<T> &res) {
     while (int(a.size()) < int(b.size())) a.push_back(0);
     while (int(b.size()) < int(a.size())) b.push_back(0);
     while (int(a.size()) & (int(a.size()) - 1)) { a.push_back(0); b.push_back(0); }
-    vector<T> c = karatsuba(a, b); T carry = 0; static T BASE = pow2(T(10), DIG);
-    for (int i = 0; i < int(c.size()); i++) { T cur = c[i] + carry; c[i] = T(cur % BASE); carry = T(cur / BASE); }
-    while (int(c.size()) > 1 && c.back() == 0) c.pop_back();
-    return c;
+    int n = int(a.size()); res.resize(n * 2, 0); karatsuba<T>(n, a.begin(), b.begin(), res.begin()); T carry = 0; static T BASE = pow2(T(10), DIG);
+    for (int i = 0; i < n * 2; i++) { T cur = res[i] + carry; res[i] = T(cur % BASE); carry = T(cur / BASE); }
+    while (int(res.size()) > 1 && res.back() == 0) res.pop_back();
 }
 
 // Multiplies 2 polynomials
-template <class T> vector<T> multiplyPolynomial(vector<T> &a, vector<T> &b) {
+template <class T> void multiplyPolynomial(vector<T> a, vector<T> b, vector<T> &res) {
     while (int(a.size()) < int(b.size())) a.push_back(0);
     while (int(b.size()) < int(a.size())) b.push_back(0);
     while (int(a.size()) & (int(a.size()) - 1)) { a.push_back(0); b.push_back(0); }
-    vector<T> c = karatsuba(a, b);
-    while (int(c.size()) > 1 && c.back() == 0) c.pop_back();
-    return c;
+    int n = int(a.size()); res.resize(n * 2, 0); karatsuba<T>(n, a.begin(), b.begin(), res.begin());
+    while (int(res.size()) > 1 && res.back() == 0) res.pop_back();
 }
