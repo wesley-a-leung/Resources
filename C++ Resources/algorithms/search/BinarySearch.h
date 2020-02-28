3741fa0352fe109b72b236749a1fe19dde1e5a16
wesley-a-leung <wesley.a.leung@gmail.com>
2020-02-28 00:39:42 -0500

    Updated bsearch

--- C++ Resources/algorithms/search/BinarySearch.h
+++ C++ Resources/algorithms/search/BinarySearch.h
@@ -2,29 +2,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// returns the first value in the range [lo, hi) where f(x) is true
-// if no value in [lo, hi) satisfies f(x), then it returns hi
-// assumes that all values where f(x) is true are greater than all values where f(x) is false
+// If ISFIRST is true:
+//   returns the first value in the range [lo, hi) where f(x) is true
+//   if no value in [lo, hi) satisfies f(x), then it returns hi
+//   assumes that all values where f(x) is true are greater than all values where f(x) is false
+// IF ISFIRST is false:
+//   returns the last value in the range [lo, hi) where f(x) is true
+//   if no value in [lo, hi) satisfies f(x), then it returns lo - 1
+//   assumes that all values where f(x) is true are less than all values where f(x) is false
 // Time Complexity: O(log (hi - lo)) * (cost to compute f(x))
-template <class T, class F> T getFirst(T lo, T hi, F f) {
-    while (lo < hi) {
-        T mid = lo + (hi - lo) / 2;
-        if (f(mid)) hi = mid;
-        else lo = mid + 1;
-    }
-    return lo;
-}
 
-// returns the last value in the range [lo, hi) where f(x) is true
-// if no value in [lo, hi) satisfies f(x), then it returns lo - 1
-// assumes that all values where f(x) is true are less than all values where f(x) is false
-// Time Complexity: O(log (hi - lo)) * (cost to compute f(x))
-template <class T, class F> T getLast(T lo, T hi, F f) {
+const bool First = true, Last = false;
+template <const bool ISFIRST, class T, class F> T bsearch(T lo, T hi, F f) {
     hi--;
     while (lo <= hi) {
         T mid = lo + (hi - lo) / 2;
-        if (f(mid)) lo = mid + 1;
-        else hi = mid - 1;
+        if (f(mid) == ISFIRST) hi = mid - 1;
+        else lo = mid + 1;
     }
-    return hi;
+    return ISFIRST ? lo : hi;
 }
