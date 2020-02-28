829e89bf4f77624f8d5afb50a3fc79d0d75b4eb9
wesley-a-leung <wesley.a.leung@gmail.com>
2020-02-28 01:05:58 -0500

    Updated Ternary Search

--- C++ Resources/algorithms/math/TernarySearch.h
+++ C++ Resources/algorithms/math/TernarySearch.h
@@ -2,14 +2,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Ternary search to find the minimum of a function
+// Ternary search to find the maximum of a function based on the comparator
+// comparator convention is same as priority_queue in STL
 // Time Complexity: O(iters * (cost to compute f(x)))
-template <class T, class F> T ternary_search(T lo, T hi, F f, int iters) {
+
+template <class T, class F, class Cmp = less<T>> T ternary_search(T lo, T hi, F f, int iters, Cmp cmp = less<T>()) {
     T m1, m2;
     for (int it = 0; it < iters; it++) {
         m1 = lo + (hi - lo) / 3; m2 = hi - (hi - lo) / 3;
-        if (f(m1) < f(m2)) hi = m2; // f(m1) < f(m2) for minimum, f(m1) > f(m2) for maximum
-        else lo = m1;
+        if (cmp(f(m1), f(m2))) lo = m1;
+        else hi = m2;
     };
     return lo + (hi - lo) / 2;
 }
