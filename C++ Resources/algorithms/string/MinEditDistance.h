5dcb173cb06fc68216436a62cc82255a7f869ece
wesley-a-leung <wesley.a.leung@gmail.com>
2020-03-04 13:10:43 -0500

    Edit Distance Update

--- C++ Resources/algorithms/string/MinEditDistance.h
+++ C++ Resources/algorithms/string/MinEditDistance.h
@@ -5,15 +5,16 @@ using namespace std;
 // Computes the minimum edit distance between 2 strings, allowing for custom penalties
 // Time Complexity: O(len(s1) * len(s2))
 // Memory Complexity: O(len(s2))
-template <const int MAXS> struct MinEditDistance {
-    int dp[2][MAXS]; string substring;
-    int solve(const string &s1, const string &s2, int repPen = 1, int insPen = 1, int delPen = 1) {
-        iota(dp[0], dp[0] + s2.length() + 1, 0);
+template <const int MAXS, class T> struct MinEditDistance {
+    T dp[2][MAXS]; string substring;
+    T solve(const string &s1, const string &s2, T cpyPen = 0, T repPen = 1, T insPen = 1, T delPen = 1) {
+        assert(cpyPen < insPen + delPen); assert(repPen < insPen + delPen);
+        for (int i = 0; i <= int(s2.length); i++) dp[0][i] = i * delPen;
         for (int i = 1; i <= int(s1.length()); i++) {
-            dp[i % 2][0] = i;
+            dp[i % 2][0] = i * insPen;
             for (int j = 1; j <= int(s2.length()); j++) {
-                if (s1[i - 1] == s2[j - 1]) dp[i % 2][j] = dp[1 - i % 2][j - 1];
-                else dp[i % 2][j] = min(min(dp[1 - i % 2][j - 1] + repPen, dp[i % 2][j - 1] + insPen), dp[1 - i % 2][j] + delPen);
+                dp[i % 2][j] = min(min(dp[1 - i % 2][j - 1] + repPen, dp[i % 2][j - 1] + insPen), dp[1 - i % 2][j] + delPen);
+                if (s1[i - 1] == s2[j - 1]) dp[i % 2][j] = min(dp[i % 2][j], dp[1 - i % 2][j - 1] + cpyPen);
             }
         }
         return dp[int(s1.length()) % 2][int(s2.length())];
