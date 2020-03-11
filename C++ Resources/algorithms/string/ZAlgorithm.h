8204512496bf34fd41b8b6ff04530e01c1eec00c
wesley-a-leung <wesley.a.leung@gmail.com>
2020-03-10 21:12:27 -0400

    Small changes to Z-Algorithm

--- C++ Resources/algorithms/string/ZAlgorithm.h
+++ C++ Resources/algorithms/string/ZAlgorithm.h
@@ -12,7 +12,7 @@ template <const int MAXS> struct ZAlgorithm {
         if (int(S.length()) > 0) z[0] = int(S.length());
         for (int i = 1, l = 0, r = 0; i < int(S.length()); i++) {
             if (i <= r) z[i] = min(r - i + 1, z[i - l]);
-            while (i + z[i] < int(S.length()) && S[z[i]] == S[i + z[i]]) ++z[i];
+            while (i + z[i] < int(S.length()) && S[z[i]] == S[i + z[i]]) z[i]++;
             if (i + z[i] - 1 > r) r = (l = i) + z[i] - 1;
         }
     }
