5352ac272240ba168dc254a2fdef428e4eb046e4
wesley-a-leung <wesley.a.leung@gmail.com>
2020-03-10 21:01:36 -0400

    Updated Suffix Array

--- C++ Resources/datastructures/string/ManberMyersSuffixArray.h
+++ C++ Resources/datastructures/string/ManberMyersSuffixArray.h
@@ -8,13 +8,13 @@ using namespace std;
 // Time Complexity: O(S log S)
 // Memory Complexity: O(S + ALPHABET_SIZE)
 template <const int MAXS, const int ALPHABET_SIZE, const int OFFSET> struct ManberMyersSuffixArray {
-    int N, ind[MAXS], rnk[MAXS], temp[MAXS], freq[ALPHABET_SIZE], preFreq[ALPHABET_SIZE], LCP[MAXS]; string S;
+    int N, ind[MAXS], rnk[MAXS], temp[MAXS], freq[ALPHABET_SIZE], LCP[MAXS]; string S;
     void run(const string &s) {
-        S = s; N = S.length(); ind[N] = N; rnk[N] = -1; fill(freq, freq + ALPHABET_SIZE, 0); preFreq[0] = 0;
-        for (int i = 0; i < N; i++) freq[S[i] - OFFSET]++;
-        for (int i = 1; i < ALPHABET_SIZE; i++) preFreq[i] = preFreq[i - 1] + freq[i - 1];
-        for (int i = 0; i < N; i++) rnk[i] = preFreq[S[i] - OFFSET];
-        for (int i = 0; i < N; i++) ind[preFreq[S[i] - OFFSET]++] = i;
+        S = s; N = S.length(); ind[N] = N; rnk[N] = -1; fill(freq, freq + ALPHABET_SIZE, 0);
+        for (int i = 0; i < N; i++) if (S[i] - OFFSET + 1 != ALPHABET_SIZE) freq[S[i] - OFFSET + 1]++;
+        for (int i = 1; i < ALPHABET_SIZE; i++) freq[i] += freq[i - 1];
+        for (int i = 0; i < N; i++) rnk[i] = freq[S[i] - OFFSET];
+        for (int i = 0; i < N; i++) ind[freq[S[i] - OFFSET]++] = i;
         for (int offset = 1; offset < N; offset += offset) for (int i = 1, cnt = 0; i <= N; i++) {
             if (rnk[ind[i]] == rnk[ind[i - 1]]) cnt++;
             else if (cnt > 0) {
