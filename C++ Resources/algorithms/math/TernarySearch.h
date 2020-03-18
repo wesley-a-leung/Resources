9c01eb735854369047d1a2d07e949e0772193fd4
wesley-a-leung <wesley.a.leung@gmail.com>
2020-03-18 06:11:13 -0400

    Updated ternary search and bisection root finding

--- C++ Resources/algorithms/math/TernarySearch.h
+++ C++ Resources/algorithms/math/TernarySearch.h
@@ -7,9 +7,8 @@ using namespace std;
 // Time Complexity: O(iters * (cost to compute f(x)))
 
 template <class T, class F, class Cmp = less<T>> T ternary_search(T lo, T hi, F f, int iters, Cmp cmp = less<T>()) {
-    T m1, m2;
     for (int it = 0; it < iters; it++) {
-        m1 = lo + (hi - lo) / 3; m2 = hi - (hi - lo) / 3;
+        T m1 = lo + (hi - lo) / 3, m2 = hi - (hi - lo) / 3;
         if (cmp(f(m1), f(m2))) lo = m1;
         else hi = m2;
     };
