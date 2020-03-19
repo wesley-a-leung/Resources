39fef2c9e004734a723329cfbb4002ac5fa01d1a
wesley-a-leung <wesley.a.leung@gmail.com>
2020-03-19 14:11:12 -0400

    Hashing Update

--- C++ Resources/algorithms/string/Hashing.h
+++ C++ Resources/algorithms/string/Hashing.h
@@ -9,20 +9,31 @@ seed_seq seq {
 };
 mt19937_64 rng64(seq);
 
-// Computes the hash of a string to allow for easy computation of substring hashes
+// Computes the hash of a string to allow for easy computation of substring hashes for H hashes
 // MOD should be at least S^2 to prevent collisions
 // Time Complexity:
-//   compute: O(S)
-//   getHash: O(1)
-// Memory Complexity: O(S)
-template <const int MAXN, class T, const T MOD, const int OFFSET> struct Hashing {
-    T H[MAXN], POW[MAXN], BASE = uniform_int_distribution<T>(MOD / 2, MOD - 2)(rng64);
-    void compute(const string &s, T base = -1) {
-        if (base == -1) base = BASE;
-        int N = int(s.length()); POW[0] = 1; H[N] = 0;
-        for (int i = 1; i <= N; i++) POW[i] = mulMod(POW[i - 1], base, MOD);
-        for (int i = N - 1; i >= 0; i--) H[i] = addMod(mulMod(H[i + 1], base, MOD), T(s[i] - OFFSET + 1), MOD);
+//   compute: O(S * H)
+//   getHash: O(H)
+// Memory Complexity: O(S * H)
+template <const int MAXN, const int HASHES, class T, const int OFFSET> struct Hashing {
+    T BASE[HASHES], MOD[HASHES], H[MAXN][HASHES], POW[MAXN][HASHES];
+    Hashing(const array<T, HASHES> &mod) {
+        for (int h = 0; h < HASHES; h++) { MOD[h] = mod[h]; BASE[h] = uniform_int_distribution<T>(MOD[h] / 2, MOD[h] - 2)(rng64); }
     }
-    // hash for substring [l, r)
-    T getHash(int l, int r) { return subMod(H[l], mulMod(H[r], POW[r - l], MOD), MOD); }
+    void compute(const string &s) {
+        int N = int(s.length()); fill(POW[0], POW[0] + HASHES, 1); fill(H[N], POW[N] + HASHES, 1);
+        for (int i = 1; i <= N; i++) for (int h = 0; h < HASHES; h++) POW[i][h] = mulMod(POW[i - 1][h], BASE[h], MOD[h]);
+        for (int i = N - 1; i >= 0; i--) for (int h = 0; h < HASHES; h++)
+            H[i][h] = addMod(mulMod(H[i + 1][h], BASE[h], MOD[h]), T(s[i] - OFFSET + 1), MOD[h]);
+    }
+    array<T, HASHES> getHash(int l, int r) { // hash for substring [l, r)
+        array<T, HASHES> ret;
+        for (int h = 0; h < HASHES; h++) ret[h] = subMod(H[l][h], mulMod(H[r][h], POW[r - l][h], MOD[h]), MOD[h]);
+        return ret;
+    }
+    array<T, HASHES> concat(int l1, int r1, int l2, int r2) { // hash for the concatenated substring [l1, r1) + [l2, r2)
+        array<T, HASHES> h1 = getHash(l1, r1), h2 = getHash(l2, r2), ret;
+        for (int h = 0; h < HASHES; h++) addMod(h1[h], mulMod(h2[h], POW[r1 - l1][h], MOD[h]), MOD[h]);
+        return ret;
+    }
 };
