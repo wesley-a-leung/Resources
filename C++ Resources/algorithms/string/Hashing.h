e1205e0af25c01bd96b53b8bb1a823ae3381dc21
wesley-a-leung <wesley.a.leung@gmail.com>
2020-03-19 17:19:20 -0400

    Updated hashing

--- C++ Resources/algorithms/string/Hashing.h
+++ C++ Resources/algorithms/string/Hashing.h
@@ -16,24 +16,26 @@ mt19937_64 rng64(seq);
 //   getHash: O(H)
 // Memory Complexity: O(S * H)
 template <const int MAXN, const int HASHES, class T, const int OFFSET> struct Hashing {
-    T BASE[HASHES], MOD[HASHES], H[MAXN][HASHES], POW[MAXN][HASHES];
+    T MOD[HASHES], BASE[HASHES], H[HASHES][MAXN], POW[HASHES][MAXN];
     Hashing(const array<T, HASHES> &mod) {
         for (int h = 0; h < HASHES; h++) { MOD[h] = mod[h]; BASE[h] = uniform_int_distribution<T>(MOD[h] / 2, MOD[h] - 2)(rng64); }
     }
     void compute(const string &s) {
-        int N = int(s.length()); fill(POW[0], POW[0] + HASHES, 1); fill(H[N], POW[N] + HASHES, 1);
-        for (int i = 1; i <= N; i++) for (int h = 0; h < HASHES; h++) POW[i][h] = mulMod(POW[i - 1][h], BASE[h], MOD[h]);
-        for (int i = N - 1; i >= 0; i--) for (int h = 0; h < HASHES; h++)
-            H[i][h] = addMod(mulMod(H[i + 1][h], BASE[h], MOD[h]), T(s[i] - OFFSET + 1), MOD[h]);
+        int N = int(s.length());
+        for (int h = 0; h < HASHES; h++) {
+            POW[h][0] = 1; H[h][N] = 0;
+            for (int i = 1; i <= N; i++) POW[h][i] = mulMod(POW[h][i - 1], BASE[h], MOD[h]);
+            for (int i = N - 1; i >= 0; i--) H[h][i] = addMod(mulMod(H[h][i + 1], BASE[h], MOD[h]), T(s[i] - OFFSET + 1), MOD[h]);
+        }
     }
     array<T, HASHES> getHash(int l, int r) { // hash for substring [l, r)
         array<T, HASHES> ret;
-        for (int h = 0; h < HASHES; h++) ret[h] = subMod(H[l][h], mulMod(H[r][h], POW[r - l][h], MOD[h]), MOD[h]);
+        for (int h = 0; h < HASHES; h++) ret[h] = subMod(H[h][l], mulMod(H[h][r], POW[h][r - l], MOD[h]), MOD[h]);
         return ret;
     }
     array<T, HASHES> concat(int l1, int r1, int l2, int r2) { // hash for the concatenated substring [l1, r1) + [l2, r2)
         array<T, HASHES> h1 = getHash(l1, r1), h2 = getHash(l2, r2), ret;
-        for (int h = 0; h < HASHES; h++) addMod(h1[h], mulMod(h2[h], POW[r1 - l1][h], MOD[h]), MOD[h]);
+        for (int h = 0; h < HASHES; h++) addMod(h1[h], mulMod(h2[h], POW[h][r1 - l1], MOD[h]), MOD[h]);
         return ret;
     }
 };
