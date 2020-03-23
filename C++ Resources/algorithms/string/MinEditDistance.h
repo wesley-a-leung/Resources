76bfd2e5c0ff1cd70683cc5854650ccb6f272ea0
wesley-a-leung <wesley.a.leung@gmail.com>
2020-03-23 00:22:58 -0400

    Updated Edit Dist

--- C++ Resources/algorithms/string/MinEditDistance.h
+++ C++ Resources/algorithms/string/MinEditDistance.h
@@ -8,9 +8,9 @@ using namespace std;
 template <const int MAXS, class T> struct MinEditDistance {
     T dp[2][MAXS];
     T solve(const string &s1, const string &s2, T cpyPen = 0, T repPen = 1, T insPen = 1, T delPen = 1) {
-        for (int i = 0; i <= int(s2.length()); i++) dp[0][i] = i * delPen;
+        for (int i = 0; i <= int(s2.length()); i++) dp[0][i] = i * insPen;
         for (int i = 1; i <= int(s1.length()); i++) {
-            dp[i % 2][0] = i * insPen;
+            dp[i % 2][0] = i * delPen;
             for (int j = 1; j <= int(s2.length()); j++) {
                 dp[i % 2][j] = min(min(dp[1 - i % 2][j - 1] + repPen, dp[i % 2][j - 1] + insPen), dp[1 - i % 2][j] + delPen);
                 if (s1[i - 1] == s2[j - 1]) dp[i % 2][j] = min(dp[i % 2][j], dp[1 - i % 2][j - 1] + cpyPen);
