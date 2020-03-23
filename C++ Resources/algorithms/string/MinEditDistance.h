30e01720dde76663d967288db719d2e89e222a43
wesley-a-leung <wesley.a.leung@gmail.com>
2020-03-23 00:16:35 -0400

    Updated EditDist

--- C++ Resources/algorithms/string/MinEditDistance.h
+++ C++ Resources/algorithms/string/MinEditDistance.h
@@ -6,7 +6,7 @@ using namespace std;
 // Time Complexity: O(len(s1) * len(s2))
 // Memory Complexity: O(len(s2))
 template <const int MAXS, class T> struct MinEditDistance {
-    T dp[2][MAXS]; string substring;
+    T dp[2][MAXS];
     T solve(const string &s1, const string &s2, T cpyPen = 0, T repPen = 1, T insPen = 1, T delPen = 1) {
         for (int i = 0; i <= int(s2.length()); i++) dp[0][i] = i * delPen;
         for (int i = 1; i <= int(s1.length()); i++) {
