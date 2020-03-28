d4500223c0c291d2605161f102eba5804943329c
wesley-a-leung <wesley.a.leung@gmail.com>
2020-03-28 17:44:04 -0400

    Updated Templates

--- C++ Resources/algorithms/graph/search/DepthFirstOrder.h
+++ C++ Resources/algorithms/graph/search/DepthFirstOrder.h
@@ -30,7 +30,7 @@ template <const int MAXV> struct TopologicalOrder {
     void addEdge(int v, int w) { adj[v].push_back(w); inDeg[w]++; }
     void init(int V = MAXV) { for (int i = 0; i < V; i++) { adj[i].clear(); inDeg[i] = 0; } }
     void run(int V) {
-        int front = 0, back = 0;
+        int front = 0, back = 0; fill(ind, ind + V, -1);
         for (int v = 0; v < V; v++) if (inDeg[v] == 0) ord[back++] = v;
         while (front < back) {
             int v = ord[front]; ind[v] = front++;
