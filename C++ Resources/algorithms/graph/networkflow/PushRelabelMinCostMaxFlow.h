1f2e942fb75b6481634a16c5d7586f661b556359
wesley-a-leung <wesley.a.leung@gmail.com>
2020-04-01 00:24:08 -0400

    Updated min cost max flow

--- C++ Resources/algorithms/graph/networkflow/PushRelabelMinCostMaxFlow.h
+++ C++ Resources/algorithms/graph/networkflow/PushRelabelMinCostMaxFlow.h
@@ -5,20 +5,20 @@ using namespace std;
 // Computes the maximum flow using a path with the minimum cost using the Push Relabel algorithm with look ahead heuristics
 // Time Complexity: O(E V^2 log (V * C)), much faster in practice
 // Memory Complexity: O(V + E)
-template <const int MAXV, class flowUnit, class costUnit, const bool USE_LOOK_AHEAD, const int SCALE = 2> struct PushRelabelMinCostMaxFlow {
+template <const int MAXV, class flowUnit, class costUnit, const int SCALE = 2> struct PushRelabelMinCostMaxFlow {
     static_assert(is_integral<costUnit>::value, "costUnit must be an integral type");
     struct Edge {
         int to; flowUnit cap, resCap; costUnit cost; int rev;
         Edge(int to, flowUnit cap, costUnit cost, int rev) : to(to), cap(cap), resCap(cap), cost(cost), rev(rev) {}
     };
-    int V, cnt[MAXV * 2], stk[MAXV]; flowUnit FLOW_EPS, maxFlow, ex[MAXV]; costUnit COST_INF, h[MAXV], bnd, minCost;
+    int cnt[MAXV * 2], stk[MAXV]; flowUnit FLOW_EPS, maxFlow, ex[MAXV]; costUnit COST_INF, h[MAXV], bnd, minCost;
     bool inStk[MAXV]; vector<int> hs[MAXV * 2]; vector<Edge> adj[MAXV]; typename vector<Edge>::iterator cur[MAXV];
     PushRelabelMinCostMaxFlow(flowUnit FLOW_EPS, costUnit COST_INF) : FLOW_EPS(FLOW_EPS), COST_INF(COST_INF) {}
     void addEdge(int v, int w, flowUnit flow, costUnit cost) {
         adj[v].emplace_back(w, flow, cost, int(adj[w].size())); adj[w].emplace_back(v, 0, -cost, int(adj[v].size()) - 1);
     }
-    void init(int V) { this->V = V; bnd = 0; for (int i = 0; i < V; i++) adj[i].clear(); }
-    flowUnit getFlow(int s, int t) {
+    void init(int V) { bnd = 0; for (int i = 0; i < V; i++) adj[i].clear(); }
+    flowUnit getFlow(int V, int s, int t) {
         auto push = [&] (int v, Edge &e, flowUnit df) {
             int w = e.to;
             if (abs(ex[w]) <= FLOW_EPS && df > FLOW_EPS) hs[h[w]].push_back(w);
@@ -46,7 +46,7 @@ template <const int MAXV, class flowUnit, class costUnit, const bool USE_LOOK_AH
         }
         return maxFlow = -ex[s];
     }
-    pair<flowUnit, costUnit> getMaxFlowMinCost(int s, int t, bool circulation) {
+    pair<flowUnit, costUnit> getMaxFlowMinCost(int V, int s, int t, bool circulation) {
         auto push = [&] (int v, Edge &e, flowUnit df) {
             int w = e.to;
             if (e.resCap < df) df = e.resCap;
@@ -72,7 +72,7 @@ template <const int MAXV, class flowUnit, class costUnit, const bool USE_LOOK_AH
         };
         minCost = 0;
         for (int v = 0; v < V; v++) for (auto &&e : adj[v]) { e.cost *= V; bnd = max(bnd, e.cost); minCost += e.cost * e.resCap; }
-        maxFlow = circulation ? 0 : getFlow(s, t);
+        maxFlow = circulation ? 0 : getFlow(V, s, t);
         fill(h, h + V, 0); fill(ex, ex + V, 0); fill(inStk, inStk + V, false);
         for (; bnd > 0; bnd >>= SCALE) {
             for (int v = 0; v < V; v++) cur[v] = adj[v].begin();
@@ -86,7 +86,7 @@ template <const int MAXV, class flowUnit, class costUnit, const bool USE_LOOK_AH
                     for (; cur[v] < adj[v].end(); cur[v]++) {
                         if (cur[v]->resCap <= FLOW_EPS) continue;
                         if (h[v] + cur[v]->cost - h[cur[v]->to] < 0) {
-                            if (USE_LOOK_AHEAD && lookAhead(cur[v]->to)) { cur[v]--; continue; }
+                            if (lookAhead(cur[v]->to)) { cur[v]--; continue; }
                             push(v, *cur[v], ex[v]);
                             if (!inStk[cur[v]->to]) inStk[stk[top++] = cur[v]->to] = true;
                             if (abs(ex[v]) <= FLOW_EPS) break;
