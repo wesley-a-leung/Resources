bd9e346530e3ea34ea7fdc0c5c87d39bcf3d465d
wesley-a-leung <wesley.a.leung@gmail.com>
2020-04-08 06:44:00 -0400

    Shortened Templates

--- C++ Resources/algorithms/graph/mst/BoruvkaMST.h
+++ C++ Resources/algorithms/graph/mst/BoruvkaMST.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <bits/stdc++.h>
+#include "../../../datastructures/UnionFind.h"
 using namespace std;
 
 // Computes the minimum spanning tree using Boruvka's algorithm
@@ -7,27 +8,21 @@ using namespace std;
 // Memory Complexity: O(V + E)
 template <const int MAXV, class unit> struct BoruvkaMST {
     struct Edge { int v, w; unit weight; };
-    int UF[MAXV], closest[MAXV]; vector<Edge> edges, mst; unit weight;
+    UnionFind<MAXV> uf; int closest[MAXV]; vector<Edge> edges, mst; unit weight;
     void addEdge(int v, int w, unit weight) { edges.push_back({v, w, weight}); }
-    int find(int v) { return UF[v] < 0 ? v : UF[v] = find(UF[v]); }
-    bool join(int v, int w) {
-        if ((v = find(v)) == (w = find(w))) return false;
-        if (UF[v] > UF[w]) swap(v, w);
-        UF[v] += UF[w]; UF[w] = v; return true;
-    }
     unit run(int V) {
-        weight = 0; fill(UF, UF + V, -1);
+        weight = 0; uf.init(V);
         for (int t = 1; t < V && int(mst.size()) < V - 1; t *= 2) {
             fill(closest, closest + V, -1);
             for (int e = 0; e < int(edges.size()); e++) {
-                int i = find(edges[e].v), j = find(edges[e].w);
+                int i = uf.find(edges[e].v), j = uf.find(edges[e].w);
                 if (i == j) continue;
                 if (closest[i] == -1 || edges[e].weight < edges[closest[i]].weight) closest[i] = e;
                 if (closest[j] == -1 || edges[e].weight < edges[closest[j]].weight) closest[j] = e;
             }
             for (int i = 0; i < V; i++) if (closest[i] != -1) {
                 int v = edges[closest[i]].v, w = edges[closest[i]].w;
-                if (join(v, w)) { mst.push_back(edges[closest[i]]); weight += edges[closest[i]].weight; }
+                if (uf.join(v, w)) { mst.push_back(edges[closest[i]]); weight += edges[closest[i]].weight; }
             }
         }
         return weight;
--- C++ Resources/algorithms/graph/mst/KruskalMST.h
+++ C++ Resources/algorithms/graph/mst/KruskalMST.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <bits/stdc++.h>
+#include "../../../datastructures/UnionFind.h"
 using namespace std;
 
 // Computes the minimum spanning tree using Kruskal's algorithm
@@ -10,19 +11,13 @@ template <const int MAXV, class unit> struct KruskalMST {
         int v, w; unit weight;
         bool operator < (const Edge &e) const { return weight < e.weight; }
     };
-    int UF[MAXV]; vector<Edge> edges, mst; unit weight;
+    UnionFind<MAXV> uf; vector<Edge> edges, mst; unit weight;
     void addEdge(int v, int w, unit weight) { edges.push_back({v, w, weight}); }
-    int find(int v) { return UF[v] < 0 ? v : UF[v] = find(UF[v]); }
-    bool join(int v, int w) {
-        if ((v = find(v)) == (w = find(w))) return false;
-        if (UF[v] > UF[w]) swap(v, w);
-        UF[v] += UF[w]; UF[w] = v; return true;
-    }
     unit run(int V) {
-        weight = 0; fill(UF, UF + V, -1); sort(edges.begin(), edges.end());
+        weight = 0; uf.init(V); sort(edges.begin(), edges.end());
         for (auto &&e : edges) {
             if (int(mst.size()) >= V - 1) break;
-            if (join(e.v, e.w)) { mst.push_back(e); weight += e.weight; }
+            if (uf.join(e.v, e.w)) { mst.push_back(e); weight += e.weight; }
         }
         return weight;
     }
