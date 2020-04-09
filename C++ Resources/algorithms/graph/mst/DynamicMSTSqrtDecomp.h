5c5abf2c543f8abb86637fa603c97bf3907ad668
wesley-a-leung <wesley.a.leung@gmail.com>
2020-04-09 10:35:03 -0400

    Compressed Code

--- C++ Resources/algorithms/graph/mst/DynamicMSTSqrtDecomp.h
+++ C++ Resources/algorithms/graph/mst/DynamicMSTSqrtDecomp.h
@@ -13,11 +13,9 @@ template <const int MAXV, const int MAXE, const int MAXQ, const int BLOCKSZ, cla
     void prop(int i, int v) { if (vis[i][v] != stamp[i]) { vis[i][v] = stamp[i]; UF[i][v] = -1; } }
     int find(int i, int v) { prop(i, v); return UF[i][v] < 0 ? v : UF[i][v] = find(i, UF[i][v]); }
     bool join(int i, int v, int w) {
-        v = find(i, v); w = find(i, w);
-        if (v == w) return false;
+        if ((v = find(i, v)) == (w = find(i, w))) return false;
         if (UF[i][v] > UF[i][w]) swap(v, w);
-        UF[i][v] += UF[i][w]; UF[i][w] = v;
-        return true;
+        UF[i][v] += UF[i][w]; UF[i][w] = v; return true;
     }
     void addEdge(int v, int w, unit weight) { edges.push_back({v, w, weight}); }
     void query(int i, unit weight) { q[Q++] = {i, weight}; }
@@ -32,13 +30,11 @@ template <const int MAXV, const int MAXE, const int MAXQ, const int BLOCKSZ, cla
                 small.emplace(edges[q[i].first].weight, q[i].first); 
             }
             stamp[1]++;
-            for (auto &&p : large) if (flag[p.second] != curFlag)
-                if (join(1, edges[p.second].v, edges[p.second].w)) ind[cnt++] = p.second;
+            for (auto &&p : large) if (flag[p.second] != curFlag && join(1, edges[p.second].v, edges[p.second].w)) ind[cnt++] = p.second;
             stamp[1]++; forest = 0;
             for (int i = 0; i < cnt; i++) {
                 if (join(0, edges[ind[i]].v, edges[ind[i]].w)) {
-                    join(1, edges[ind[i]].v, edges[ind[i]].w);
-                    forest += edges[ind[i]].weight;
+                    join(1, edges[ind[i]].v, edges[ind[i]].w); forest += edges[ind[i]].weight;
                 } else small.emplace(edges[ind[i]].weight, ind[i]);
             }
             for (int i = l; i <= r; i++) {
@@ -46,8 +42,7 @@ template <const int MAXV, const int MAXE, const int MAXQ, const int BLOCKSZ, cla
                 edges[q[i].first].weight = q[i].second;
                 large.emplace(edges[q[i].first].weight, q[i].first); small.emplace(edges[q[i].first].weight, q[i].first);
                 stamp[0]++; mst = 0;
-                for (auto &&p: small) if (join(0, find(1, edges[p.second].v), find(1, edges[p.second].w)))
-                    mst += edges[p.second].weight;
+                for (auto &&p: small) if (join(0, find(1, edges[p.second].v), find(1, edges[p.second].w))) mst += edges[p.second].weight;
                 ans[i] = forest + mst;
             }
         }
