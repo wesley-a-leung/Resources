cae40363d1e0f8937cfdc90d474c8f7cf1a285df
wesley-a-leung <wesley.a.leung@gmail.com>
2020-04-11 05:29:24 -0400

    Variable renaming

--- C++ Resources/datastructures/LeftistHeapIncremental.h
+++ C++ Resources/datastructures/LeftistHeapIncremental.h
@@ -9,27 +9,27 @@ using namespace std;
 //   pop, push, merge: O(log N)
 template <class Value, class Comparator = less<Value>, class Delta = Value> struct LeftistHeapIncremental {
     struct Node {
-        Value val; Delta delta; int dist; unique_ptr<Node> left, right; Node(const Value &v, const Delta &d) : val(v), delta(d), dist(0) {}
+        Value val; Delta delta; int dist; unique_ptr<Node> l, r; Node(const Value &v, const Delta &d) : val(v), delta(d), dist(0) {}
     };
     Comparator cmp; Delta ddef; int cnt; unique_ptr<Node> root;
     void propagate(const unique_ptr<Node> &a) {
         a->val = a->val + a->delta;
-        if (a->left) a->left->delta = a->left->delta + a->delta;
-        if (a->right) a->right->delta = a->right->delta + a->delta;
+        if (a->l) a->l->delta = a->l->delta + a->delta;
+        if (a->r) a->r->delta = a->r->delta + a->delta;
         a->delta = ddef;
     }
     unique_ptr<Node> merge(unique_ptr<Node> a, unique_ptr<Node> b) {
         if (!a || !b) return a ? move(a) : move(b);
         propagate(a); propagate(b);
         if (cmp(a->val, b->val)) a.swap(b);
-        a->right = merge(move(a->right), move(b));
-        if (!a->left || a->left->dist < a->right->dist) a->left.swap(a->right);
-        a->dist = (a->right ? a->right->dist : 0) + 1; return move(a);
+        a->r = merge(move(a->r), move(b));
+        if (!a->l || a->l->dist < a->r->dist) a->l.swap(a->r);
+        a->dist = (a->r ? a->r->dist : 0) + 1; return move(a);
     }
     LeftistHeapIncremental(const Delta &ddef) : ddef(ddef), cnt(0) {}
     bool empty() const { return !root; }
     Value top() { propagate(root); return root->val; }
-    Value pop() { propagate(root); Value ret = root->val; root = merge(move(root->left), move(root->right)); cnt--; return ret; }
+    Value pop() { propagate(root); Value ret = root->val; root = merge(move(root->l), move(root->r)); cnt--; return ret; }
     void push(const Value &val) { root = merge(move(root), make_unique<Node>(val, ddef)); cnt++; }
     void increment(const Delta &delta) { if (root) root->delta = root->delta + delta; }
     void merge(LeftistHeapIncremental &h) { root = merge(move(root), move(h.root)); cnt += h.cnt; }
--- C++ Resources/datastructures/SkewHeapIncremental.h
+++ C++ Resources/datastructures/SkewHeapIncremental.h
@@ -15,26 +15,26 @@ std::mt19937 rng(seq);
 //   pop, push, merge: O(log N) expected if randomized, amortized if not
 template <class Value, class Comparator = less<Value>, class Delta = Value, const bool RANDOMIZED = false> struct SkewHeapIncremental {
     struct Node {
-        Value val; Delta delta; unique_ptr<Node> left, right; Node(const Value &v, const Delta &d) : val(v), delta(d) {}
+        Value val; Delta delta; unique_ptr<Node> l, r; Node(const Value &v, const Delta &d) : val(v), delta(d) {}
     };
     Comparator cmp; Delta ddef; int cnt; unique_ptr<Node> root;
     void propagate(const unique_ptr<Node> &a) {
         a->val = a->val + a->delta;
-        if (a->left) a->left->delta = a->left->delta + a->delta;
-        if (a->right) a->right->delta = a->right->delta + a->delta;
+        if (a->l) a->l->delta = a->l->delta + a->delta;
+        if (a->r) a->r->delta = a->r->delta + a->delta;
         a->delta = ddef;
     }
     unique_ptr<Node> merge(unique_ptr<Node> a, unique_ptr<Node> b) {
         if (!a || !b) return a ? move(a) : move(b);
         propagate(a); propagate(b);
         if (cmp(a->val, b->val)) a.swap(b);
-        if (!RANDOMIZED || rng() % 2) a->left.swap(a->right);
-        a->right = merge(move(b), move(a->right)); return move(a);
+        if (!RANDOMIZED || rng() % 2) a->l.swap(a->r);
+        a->r = merge(move(b), move(a->r)); return move(a);
     }
     SkewHeapIncremental(const Delta &ddef) : ddef(ddef), cnt(0) {}
     bool empty() const { return !root; }
     Value top() { propagate(root); return root->val; }
-    Value pop() { propagate(root); Value ret = root->val; root = merge(move(root->left), move(root->right)); cnt--; return ret; }
+    Value pop() { propagate(root); Value ret = root->val; root = merge(move(root->l), move(root->r)); cnt--; return ret; }
     void push(const Value &val) { root = merge(move(root), make_unique<Node>(val, ddef)); cnt++; }
     void increment(const Delta &delta) { if (root) root->delta = root->delta + delta; }
     void merge(SkewHeapIncremental &h) { root = merge(move(root), move(h.root)); cnt += h.cnt; }
