287e0e32eb4460a88048011f7cd4d9dc73e4e496
wesley-a-leung <wesley.a.leung@gmail.com>
2020-04-13 15:22:38 -0400

    Added static allocator

---
+++ C++ Resources/utils/StaticAllocator.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr const int MB = 200;
+char buf[MB << 20];
+size_t buf_ind = sizeof(buf);
+void *operator new(size_t n) { return (void *) &buf[buf_ind -= n]; }
+void operator delete(void *) {}
+
+template <class T> struct StaticAllocator {
+    typedef T value_type; StaticAllocator() {}
+    template <class U> StaticAllocator(const U &) {}
+    T *allocate(size_t n) { (buf_ind -= n * sizeof(T)) &= 0 - alignof(T); return (T*) (buf + buf_ind); }
+    void deallocate(T *, size_t) {}
+};
