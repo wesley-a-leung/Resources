6d5c1f6409114d37d29a438a28a7e032a225d822
wesley-a-leung <wesley.a.leung@gmail.com>
2020-05-29 22:15:48 -0400

    New segment tree template

--- C++ Resources/search/BinarySearch.h
+++ C++ Resources/search/BinarySearch.h
@@ -16,6 +16,8 @@ using namespace std;
 //   https://mcpt.ca/problem/lcc18c5s3
 //   https://dmoj.ca/problem/apio19p3
 //   https://dmoj.ca/problem/pib20p2
+//   https://dmoj.ca/problem/coci17c1p5
+//   https://dmoj.ca/problem/dmopc18c5p5
 //   https://codingcompetitions.withgoogle.com/codejam/round/000000000019ffb9/00000000003384ea
 const bool First = true, Last = false;
 template <const bool ISFIRST, class T, class F> T bsearch(T lo, T hi, F f) {
