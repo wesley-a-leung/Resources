c602074bab85bd04779800f0d7c88c7daacd4e56
wesley-a-leung <wesley.a.leung@gmail.com>
2020-06-09 23:42:44 -0400

    Updated documentation

--- C++ Resources/datastructures/trees/binarysearchtrees/DynamicRangeOperations.cpp
+++ C++ Resources/datastructures/trees/binarysearchtrees/DynamicRangeOperations.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 // Operations on ranges of a dynamic array, backed by a generic binary search tree
+// Indices are 0-indexed and ranges are inclusive
+// Supperts point/range updates/queries, range reversals and binary searching as long as Node contains the appropriate flags
 // Tree should be of type Treap or Splay, or any struct that has the following:
 //   Node: struct with at least following (more may be required by the underlying Tree being used):
 //     Data: typedef/using of the value type this node stores
