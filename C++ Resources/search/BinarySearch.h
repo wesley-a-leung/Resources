7224ddfd2b9a334cb666dda6362abd20c5567762
wesley-a-leung <wesley.a.leung@gmail.com>
2020-06-11 04:40:07 -0400

    Updated formatting

--- C++ Resources/search/BinarySearch.h
+++ C++ Resources/search/BinarySearch.h
@@ -3,13 +3,15 @@
 using namespace std;
 
 // If ISFIRST is true:
-//   returns the first value in the range [lo, hi) where f(x) is true
-//   if no value in [lo, hi) satisfies f(x), then it returns hi
-//   assumes that all values where f(x) is true are greater than all values where f(x) is false
+//   Returns the first value in the range [lo, hi) where f(x) is true
+//     if no value in [lo, hi) satisfies f(x), then it returns hi
+//     assumes that all values where f(x) is true are greater than all values
+//     where f(x) is false
 // IF ISFIRST is false:
-//   returns the last value in the range [lo, hi) where f(x) is true
-//   if no value in [lo, hi) satisfies f(x), then it returns lo - 1
-//   assumes that all values where f(x) is true are less than all values where f(x) is false
+//   Returns the last value in the range [lo, hi) where f(x) is true
+//     if no value in [lo, hi) satisfies f(x), then it returns lo - 1
+//     assumes that all values where f(x) is true are less than all values
+//     where f(x) is false
 // Time Complexity: O(log (hi - lo)) * (cost to compute f(x))
 // Memory Complexity: O(1)
 // Tested:
@@ -18,7 +20,6 @@ using namespace std;
 //   https://dmoj.ca/problem/pib20p2
 //   https://dmoj.ca/problem/coci17c1p5
 //   https://dmoj.ca/problem/dmopc18c5p5
-//   https://codingcompetitions.withgoogle.com/codejam/round/000000000019ffb9/00000000003384ea
 const bool First = true, Last = false;
 template <const bool ISFIRST, class T, class F> T bsearch(T lo, T hi, F f) {
     hi--;
