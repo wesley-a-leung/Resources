0be1b20c2eb6ce2e6d5ec8e5e722dfb59f5b2b48
wesley-a-leung <wesley.a.leung@gmail.com>
2020-06-16 23:40:44 -0400

    Replacing pragmas

---
+++ ReplacePragmas.py
@@ -0,0 +1,25 @@
+import pathlib
+import sys
+input = sys.stdin.readline
+
+for i in range(1, len(sys.argv)):
+  filename = sys.argv[i]
+  print("Removing #pragma once for", filename)
+  headerguard = pathlib.Path(filename).stem.upper() + "_H"
+  output = []
+  with open(filename, "r") as file:
+    replacePragmaOnce = False
+    for line in file:
+      if line == "#pragma once\n":
+        output.append("#ifndef " + headerguard + "\n")
+        output.append("#define " + headerguard + "\n")
+        output.append("\n")
+        replacePragmaOnce = True
+      else:
+        output.append(line)
+    if replacePragmaOnce:
+      output.append("\n")
+      output.append("#endif\n")
+  with open(filename, "w") as file:
+    for line in output:
+      file.write(line)
