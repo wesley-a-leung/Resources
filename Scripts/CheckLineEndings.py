57beca2009324c9c0cc605ccbe45209469a98c5d
wesley-a-leung <wesley.a.leung@gmail.com>
2020-06-22 03:24:07 -0400

    Updated line ending script

--- Scripts/CheckLineEndings.py
+++ Scripts/CheckLineEndings.py
@@ -1,17 +1,21 @@
-# Script to check that every line ends in \n
+# Script to check that every line ends in \n and not \r\n
 import sys
 input = sys.stdin.readline
 
 exitCode = 0
 for i in range(1, len(sys.argv)):
   filename = sys.argv[i]
-  with open(filename, "r") as file:
+  with open(filename, "rb") as file:
     replacePragmaOnce = False
     curLine = 0
     for line in file:
       curLine += 1
-      if line[-1] != '\n':
+      if not line.endswith(b"\n"):
         print(filename + " contains a line that does not end in \\n")
+        exitCode = 1
+        break
+      if line.endswith(b"\r\n"):
+        print(filename + " contains a line that end in \\r\\n")
         exitCode = 1
         break
 sys.exit(exitCode)
