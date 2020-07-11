6123d14c7940d0a3460e2b0f8e1b0af035c02004
wesley-a-leung <wesley.a.leung@gmail.com>
2020-07-11 00:22:34 -0400

    Classical Prim

--- Content/C++/graph/minimumspanningtree/PrimMST.h
+++ Content/C++/graph/minimumspanningtree/PrimMST.h
@@ -23,24 +23,67 @@ using namespace std;
 //   https://open.kattis.com/problems/minspantree
 template <class T> struct PrimMST {
   using Edge = tuple<int, int, T>; T mstWeight; vector<Edge> mstEdges;
-  template <class WeightedGraph> PrimMST(const WeightedGraph &G,
-                                         T INF = numeric_limits<T>::max())
+  template <class WeightedGraph>
+  PrimMST(const WeightedGraph &G, T INF = numeric_limits<T>::max())
       : mstWeight() {
-    int V = G.size(); vector<bool> vis(V, false);
-    vector<T> cost(V, INF); vector<int> to(V, -1);
+    int V = G.size(); vector<bool> done(V, false);
+    vector<T> mn(V, INF); vector<int> to(V, -1);
     std::priority_queue<pair<T, int>, vector<pair<T, int>>,
                         greater<pair<T, int>>> PQ;
-    for (int s = 0; s < V; s++) if (!vis[s]) {
-      PQ.emplace(cost[s] = T(), s); while (!PQ.empty()) {
-        int v = PQ.top().second; PQ.pop(); if (vis[v]) continue;
-        vis[v] = true;
-        for (auto &&e : G[v]) if (!vis[e.first] && e.second < cost[e.first]) {
-          to[e.first] = v; PQ.emplace(cost[e.first] = e.second, e.first);
+    for (int s = 0; s < V; s++) if (!done[s]) {
+      PQ.emplace(mn[s] = T(), s); while (!PQ.empty()) {
+        int v = PQ.top().second; PQ.pop(); if (done[v]) continue;
+        done[v] = true;
+        for (auto &&e : G[v]) if (!done[e.first] && e.second < mn[e.first]) {
+          to[e.first] = v; PQ.emplace(mn[e.first] = e.second, e.first);
         }
       }
     }
     for (int v = 0; v < V; v++) if (to[v] != -1) {
-      mstEdges.emplace_back(v, to[v], cost[v]); mstWeight += cost[v];
+      mstEdges.emplace_back(v, to[v], mn[v]); mstWeight += mn[v];
+    }
+  }
+};
+
+// Computes the minimum spanning tree (or forest) using Classical Prims's
+//   algorithm
+// Vertices are 0-indexed
+// Constructor Arguments:
+//   G: a generic weighted graph data structure
+//     with the [] operator (const) defined to iterate over the adjacency list
+//     (which is a list of pair<int, T> with weights of type T), as well as a
+//     member function size() (const) that returns the number of vertices
+//     in the graph
+//   INF: a value for infinity
+// Fields:
+//   mstWeight: the weight of the mst
+//   mstEdges: a vector of tuples of the edges in the mst
+// In practice, has a small constant
+// Time Complexity:
+//   constructor: O(V^2 + E)
+// Memory Complexity: O(V + E)
+// Tested:
+//   Stress Tested
+//   https://judge.evanzhang.ca/problem/anmstproblem
+template <class T> struct ClassicalPrimMST {
+  using Edge = tuple<int, int, T>; T mstWeight; vector<Edge> mstEdges;
+  template <class WeightedGraph>
+  ClassicalPrimMST(const WeightedGraph &G, T INF = numeric_limits<T>::max())
+      : mstWeight() {
+    int V = G.size(); vector<bool> done(V, false); vector<T> mn(V, INF);
+    vector<int> to(V, -1); for (int s = 0; s < V; s++) if (!done[s]) {
+      mn[s] = T(); while (true) {
+        int v = -1; for (int w = 0; w < V; w++)
+          if (!done[w] && (v == -1 || mn[v] > mn[w])) v = w;
+        if (v == -1 || mn[v] == INF) break;
+        done[v] = true;
+        for (auto &&e : G[v]) if (!done[e.first] && e.second < mn[e.first]) {
+          to[e.first] = v; mn[e.first] = e.second;
+        }
+      }
+    }
+    for (int v = 0; v < V; v++) if (to[v] != -1) {
+      mstEdges.emplace_back(v, to[v], mn[v]); mstWeight += mn[v];
     }
   }
 };
--- Tests/C++/graph/minimumspanningtree/MinimumSpanningTreeStressTest.cpp
+++ Tests/C++/graph/minimumspanningtree/MinimumSpanningTreeStressTest.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 void test1() {
   mt19937_64 rng(0);
-  int V = 1e6, E = 5e6;
+  int V = 1e4, E = 5e6;
   vector<KruskalMST<long long>::Edge> edges;
   for (int i = 0; i < E; i++) {
     int v = rng() % V, w = rng() % V;
@@ -36,19 +36,17 @@ void test1() {
 void test2() {
   mt19937_64 rng(0);
   int V = 1e6, E = 5e6;
-  StaticWeightedGraph<long long> G(V);
-  G.reserveDiEdges(E * 2);
+  vector<KruskalMST<long long>::Edge> edges;
   for (int i = 0; i < E; i++) {
     int v = rng() % V, w = rng() % V;
     long long weight = rng() % (long long)(1e9) + 1;
-    G.addBiEdge(v, w, weight);
+    edges.emplace_back(v, w, weight);
   }
-  G.build();
   const auto start_time = chrono::system_clock::now();
-  PrimMST<long long> mst(move(G));
+  KruskalMST<long long> mst(V, move(edges));
   const auto end_time = chrono::system_clock::now();
   double sec = ((end_time - start_time).count() / double(chrono::system_clock::period::den));
-  cout << "Subtest 2 (Prim) Passed" << endl;
+  cout << "Subtest 2 (Kruskal) Passed" << endl;
   cout << "  V: " << V << endl;
   cout << "  E: " << E << endl;
   cout << "  Time: " << fixed << setprecision(3) << sec << "s" << endl;
@@ -65,7 +63,7 @@ void test2() {
 
 void test3() {
   mt19937_64 rng(0);
-  int V = 1e6, E = 5e6;
+  int V = 1e4, E = 5e6;
   vector<BoruvkaMST<long long>::Edge> edges;
   for (int i = 0; i < E; i++) {
     int v = rng() % V, w = rng() % V;
@@ -91,10 +89,132 @@ void test3() {
   cout << "  Checksum: " << checkSum << endl;
 }
 
+void test4() {
+  mt19937_64 rng(0);
+  int V = 1e6, E = 5e6;
+  vector<BoruvkaMST<long long>::Edge> edges;
+  for (int i = 0; i < E; i++) {
+    int v = rng() % V, w = rng() % V;
+    long long weight = rng() % (long long)(1e9) + 1;
+    edges.emplace_back(v, w, weight);
+  }
+  const auto start_time = chrono::system_clock::now();
+  BoruvkaMST<long long> mst(V, move(edges));
+  const auto end_time = chrono::system_clock::now();
+  double sec = ((end_time - start_time).count() / double(chrono::system_clock::period::den));
+  cout << "Subtest 4 (Boruvka) Passed" << endl;
+  cout << "  V: " << V << endl;
+  cout << "  E: " << E << endl;
+  cout << "  Time: " << fixed << setprecision(3) << sec << "s" << endl;
+  long long checkSum = mst.mstWeight % (long long)(1e9 + 7);
+  for (auto &&e : mst.mstEdges) if (get<0>(e) > get<1>(e)) swap(get<0>(e), get<1>(e));
+  sort(mst.mstEdges.begin(), mst.mstEdges.end());
+  for (auto &&e : mst.mstEdges) {
+    checkSum = (31 * checkSum + get<0>(e)) % (long long)(1e9 + 7);
+    checkSum = (31 * checkSum + get<1>(e)) % (long long)(1e9 + 7);
+    checkSum = (31 * checkSum + get<2>(e)) % (long long)(1e9 + 7);
+  }
+  cout << "  Checksum: " << checkSum << endl;
+}
+
+void test5() {
+  mt19937_64 rng(0);
+  int V = 1e4, E = 5e6;
+  StaticWeightedGraph<long long> G(V);
+  G.reserveDiEdges(E * 2);
+  for (int i = 0; i < E; i++) {
+    int v = rng() % V, w = rng() % V;
+    long long weight = rng() % (long long)(1e9) + 1;
+    G.addBiEdge(v, w, weight);
+  }
+  G.build();
+  const auto start_time = chrono::system_clock::now();
+  PrimMST<long long> mst(move(G));
+  const auto end_time = chrono::system_clock::now();
+  double sec = ((end_time - start_time).count() / double(chrono::system_clock::period::den));
+  cout << "Subtest 5 (Prim) Passed" << endl;
+  cout << "  V: " << V << endl;
+  cout << "  E: " << E << endl;
+  cout << "  Time: " << fixed << setprecision(3) << sec << "s" << endl;
+  long long checkSum = mst.mstWeight % (long long)(1e9 + 7);
+  for (auto &&e : mst.mstEdges) if (get<0>(e) > get<1>(e)) swap(get<0>(e), get<1>(e));
+  sort(mst.mstEdges.begin(), mst.mstEdges.end());
+  for (auto &&e : mst.mstEdges) {
+    checkSum = (31 * checkSum + get<0>(e)) % (long long)(1e9 + 7);
+    checkSum = (31 * checkSum + get<1>(e)) % (long long)(1e9 + 7);
+    checkSum = (31 * checkSum + get<2>(e)) % (long long)(1e9 + 7);
+  }
+  cout << "  Checksum: " << checkSum << endl;
+}
+
+void test6() {
+  mt19937_64 rng(0);
+  int V = 1e6, E = 5e6;
+  StaticWeightedGraph<long long> G(V);
+  G.reserveDiEdges(E * 2);
+  for (int i = 0; i < E; i++) {
+    int v = rng() % V, w = rng() % V;
+    long long weight = rng() % (long long)(1e9) + 1;
+    G.addBiEdge(v, w, weight);
+  }
+  G.build();
+  const auto start_time = chrono::system_clock::now();
+  PrimMST<long long> mst(move(G));
+  const auto end_time = chrono::system_clock::now();
+  double sec = ((end_time - start_time).count() / double(chrono::system_clock::period::den));
+  cout << "Subtest 6 (Prim) Passed" << endl;
+  cout << "  V: " << V << endl;
+  cout << "  E: " << E << endl;
+  cout << "  Time: " << fixed << setprecision(3) << sec << "s" << endl;
+  long long checkSum = mst.mstWeight % (long long)(1e9 + 7);
+  for (auto &&e : mst.mstEdges) if (get<0>(e) > get<1>(e)) swap(get<0>(e), get<1>(e));
+  sort(mst.mstEdges.begin(), mst.mstEdges.end());
+  for (auto &&e : mst.mstEdges) {
+    checkSum = (31 * checkSum + get<0>(e)) % (long long)(1e9 + 7);
+    checkSum = (31 * checkSum + get<1>(e)) % (long long)(1e9 + 7);
+    checkSum = (31 * checkSum + get<2>(e)) % (long long)(1e9 + 7);
+  }
+  cout << "  Checksum: " << checkSum << endl;
+}
+
+void test7() {
+  mt19937_64 rng(0);
+  int V = 1e4, E = 5e6;
+  StaticWeightedGraph<long long> G(V);
+  G.reserveDiEdges(E * 2);
+  for (int i = 0; i < E; i++) {
+    int v = rng() % V, w = rng() % V;
+    long long weight = rng() % (long long)(1e9) + 1;
+    G.addBiEdge(v, w, weight);
+  }
+  G.build();
+  const auto start_time = chrono::system_clock::now();
+  ClassicalPrimMST<long long> mst(move(G));
+  const auto end_time = chrono::system_clock::now();
+  double sec = ((end_time - start_time).count() / double(chrono::system_clock::period::den));
+  cout << "Subtest 7 (Classical Prim) Passed" << endl;
+  cout << "  V: " << V << endl;
+  cout << "  E: " << E << endl;
+  cout << "  Time: " << fixed << setprecision(3) << sec << "s" << endl;
+  long long checkSum = mst.mstWeight % (long long)(1e9 + 7);
+  for (auto &&e : mst.mstEdges) if (get<0>(e) > get<1>(e)) swap(get<0>(e), get<1>(e));
+  sort(mst.mstEdges.begin(), mst.mstEdges.end());
+  for (auto &&e : mst.mstEdges) {
+    checkSum = (31 * checkSum + get<0>(e)) % (long long)(1e9 + 7);
+    checkSum = (31 * checkSum + get<1>(e)) % (long long)(1e9 + 7);
+    checkSum = (31 * checkSum + get<2>(e)) % (long long)(1e9 + 7);
+  }
+  cout << "  Checksum: " << checkSum << endl;
+}
+
 int main() {
   test1();
   test2();
   test3();
+  test4();
+  test5();
+  test6();
+  test7();
   cout << "Test Passed" << endl;
   return 0;
 }
