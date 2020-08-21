97063dbeed5c6d25bb3943f8e8ef710b4e45b487
wesley-a-leung <wesley.a.leung@gmail.com>
2020-08-20 23:24:26 -0400

    Added more cycle stress tests

--- Tests/C++/graph/cycles/CycleStressTest.cpp
+++ Tests/C++/graph/cycles/CycleStressTest.cpp
@@ -26,8 +26,32 @@ void test1() {
   cout << "  Checksum: " << checkSum << endl;
 }
 
+void test2() {
+  mt19937_64 rng(0);
+  int V = 4e6, E = 4e6;
+  StaticGraph G(V);
+  G.reserveDiEdges(E * 2);
+  for (int i = 0; i < E; i++) {
+    int v = i, w = (i + 1) % V;
+    G.addBiEdge(v, w);
+  }
+  G.build();
+  const auto start_time = chrono::system_clock::now();
+  Cycle cycle(G);
+  const auto end_time = chrono::system_clock::now();
+  double sec = ((end_time - start_time).count() / double(chrono::system_clock::period::den));
+  cout << "Subtest 2 Passed" << endl;
+  cout << "  V: " << V << endl;
+  cout << "  E: " << E << endl;
+  cout << "  Time: " << fixed << setprecision(3) << sec << "s" << endl;
+  long long checkSum = 0;
+  for (int v : cycle.cycle) checkSum = 31 * checkSum + v;
+  cout << "  Checksum: " << checkSum << endl;
+}
+
 int main() {
   test1();
+  test2();
   cout << "Test Passed" << endl;
   return 0;
 }
