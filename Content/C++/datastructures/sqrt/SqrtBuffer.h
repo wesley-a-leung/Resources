90699ec69dee506393c5822d48a14300c26a11cf
wesley-a-leung <wesley.a.leung@gmail.com>
2020-09-14 17:33:38 -0400

    Updated Documentation for SqrtBuffer

--- Content/C++/datastructures/sqrt/SqrtBuffer.h
+++ Content/C++/datastructures/sqrt/SqrtBuffer.h
@@ -2,11 +2,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Maintains the rank of an element in an array, allowing for multiple
-//   insertions at the same time (including negative)
+// Maintains the rank of an element in a multiset, allowing for multiple
+//   insertions of the same element at the same time (including negative)
 // Indices are 0-indexed and ranges are inclusive with the exception of
 //   functions that accept two iterators as a parameter, such as
 //   the constructor, which are exclusive
+// Template Arguments:
+//   T: the type of the values being stored
+//   CountType: the type of the count of elements in the set
+//   Cmp: the comparator to compare two f(x) values,
+//     Required Functions:
+//       operator (a, b): returns true if and only if a compares less than b
+// Constructor Arguments:
+//   st: an iterator pointing to the first element in an array of pairs
+//     containing the sorted initial values and the count of each element
+//   en: an iterator pointing to after the last element in an array of pairs
+//     containing the sorted initial values and the count of each element
+//   SCALE: the value to scale sqrt by
+// Functions:
+//   rebuild(): rebuilds the multiset by moving all elements in the small
+//     container to the large container
+//   insert(p): inserts the element p.first with a count of p.second
+//     into the multiset
+//   emplace(v, c): inserts the element v with a count of c
+//     into the multiset
+//   aboveInd(val): returns the index of the smallest element where
+//     val compares less than that element
+//   ceilingInd(val): returns the index of the smallest element that
+//     does not compare less than val
+//   floorInd(val): returns the index of the largest element where
+//     val does not compare less than that element
+//   belowInd(val): returns the index of the largest element that
+//     compares less than val
+//   contains(val): returns whether val is in the multiset or not
+//   count(lo, hi): returns the number of values in the range [lo, hi]
+//   empty(): returns whether the multiset is empty or not
+//   count(): returns the number of elements the multiset
+//   clear(): clears the multiset
+//   values(): returns a vector of pairs containing the sorted values and
+//     their count in the multiset
 // In practice, has a very small constant, performs similarly to RootArray,
 //   and is faster than balanced binary search trees
 // Time Complexity:
--- Content/C++/datastructures/sqrt/SqrtBufferSimple.h
+++ Content/C++/datastructures/sqrt/SqrtBufferSimple.h
@@ -2,10 +2,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Maintains the rank of an element in an array
+// Maintains the rank of an element in a multiset
 // Indices are 0-indexed and ranges are inclusive with the exception of
 //   functions that accept two iterators as a parameter, such as
 //   the constructor, which are exclusive
+// Template Arguments:
+//   T: the type of the values being stored
+//   Cmp: the comparator to compare two f(x) values,
+//     Required Functions:
+//       operator (a, b): returns true if and only if a compares less than b
+// Constructor Arguments:
+//   st: an iterator pointing to the first element in the sorted array of
+//     initial values
+//   en: an iterator pointing to after the last element in the sorted array of
+//     initial values
+//   SCALE: the value to scale sqrt by
+// Functions:
+//   rebuild(): rebuilds the multiset by moving all elements in the small
+//     container to the large container
+//   insert(val): inserts val into the multiset
+//   aboveInd(val): returns the index of the smallest element where
+//     val compares less than that element
+//   ceilingInd(val): returns the index of the smallest element that
+//     does not compare less than val
+//   floorInd(val): returns the index of the largest element where
+//     val does not compare less than that element
+//   belowInd(val): returns the index of the largest element that
+//     compares less than val
+//   contains(val): returns whether val is in the multiset or not
+//   count(lo, hi): returns the number of values in the range [lo, hi]
+//   empty(): returns whether the multiset is empty or not
+//   size(): returns the number of elements the multiset
+//   clear(): clears the multiset
+//   values(): returns a vector of the sorted values in the multiset
 // In practice, has a very small constant, performs similarly to RootArray,
 //   and is faster than balanced binary search trees
 // Time Complexity:
