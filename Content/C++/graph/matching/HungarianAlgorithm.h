a1b2517c1c07eaac45e2150e78d48d1832adcd96
wesley-a-leung <wesley.a.leung@gmail.com>
2020-11-18 02:12:44 -0500

    Updated Hungarian

--- Content/C++/graph/matching/HungarianAlgorithm.h
+++ Content/C++/graph/matching/HungarianAlgorithm.h
@@ -2,105 +2,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Solves the assignment problem of matching W workers to J jobs with the minimum cost
-// where each worker can only be assigned to 1 job and each job can only be assigned to 1 worker
-// Time Complexity: O(max(W, J)^3)
-// Memory Complexity: O(max(W, J)^2)
-template <const int MAXWJ, class unit> struct HungarianAlgorithm {
-    unit INF, EPS; HungarianAlgorithm(unit INF, unit EPS) : INF(INF), EPS(EPS) {}
-    int rows, cols, dim; bool committedWorkers[MAXWJ];
-    unit matrix[MAXWJ][MAXWJ], costMatrix[MAXWJ][MAXWJ], labelByWorker[MAXWJ], labelByJob[MAXWJ], minSlackValueByJob[MAXWJ], totalCost;
-    int minSlackWorkerByJob[MAXWJ], matchJobByWorker[MAXWJ], matchWorkerByJob[MAXWJ], parentWorkerByCommittedJob[MAXWJ];
-    void match(int w, int j) { matchJobByWorker[w] = j; matchWorkerByJob[j] = w; }
-    void updateLabeling(unit slack) {
-        for (int w = 0; w < dim; w++) if (committedWorkers[w]) labelByWorker[w] += slack;
-        for (int j = 0; j < dim; j++) {
-            if (parentWorkerByCommittedJob[j] != -1) labelByJob[j] -= slack;
-            else minSlackValueByJob[j] -= slack;
+// Solves the assignment problem of matching N workers to M jobs with the
+//   minimum cost where each worker can only be assigned to at most 1 job and
+//   each job can only be assigned to at most 1 worker
+// Template Arguments:
+//   T: the type of the cost
+// Constructor Arguments:
+//   A: a matrix of size N x M with A[i][j] representing the cost of assigning
+//     worker i to job j
+//   INF: a value for infinity
+// Fields:
+//   N: the number of workers
+//   M: the number of jobs
+//   cost: the minimum cost for a valid assignment
+//   jobForWorker: a vector representing the job assigned to each worker, or -1
+//     if that worker is not assigned any job
+//   workerForJob: a vector representing the worker assigned to each job, or -1
+//     if that job is not assigned any worker
+// In practice, has a very small constant
+// Time Complexity:
+//   constructor: O(N^2 M)
+// Memory Complexity: O(NM)
+// Tested:
+//   https://judge.yosupo.jp/problem/assignment
+//   https://open.kattis.com/problems/cordonbleu
+//   https://www.spoj.com/problems/BABY/
+//   https://dmoj.ca/problem/tle17c7p5
+template <class T> struct HungarianAlgorithm {
+  int N, M; T cost; vector<int> jobForWorker, workerForJob;
+  HungarianAlgorithm(vector<vector<T>> A, T INF = numeric_limits<T>::max())
+      : N(A.size()), M(A.empty() ? 0 : A[0].size()), cost(T()) {
+    bool rev = N > M; if (rev) {
+      swap(N, M); vector<vector<T>> B(N, vector<T>(M));
+      for (int i = 0; i < N; i++) for (int j = 0; j < M; j++)
+        B[i][j] = A[j][i];
+      A.swap(B);
+    }
+    jobForWorker.assign(N, -1), workerForJob.assign(M + 1, N);
+    vector<T> d1(N + 1, T()), d2(M + 1, T()); for (int i = 0; i < N; i++) {
+      int j0 = M; workerForJob[j0] = i; vector<T> dist(M + 1, INF);
+      vector<int> par(M + 1, -1); vector<bool> done(M + 1, false); do {
+        done[j0] = true; int i0 = workerForJob[j0], j1 = M; T delta = INF;
+        for (int j = 0; j < M; j++) if (!done[j]) {
+          T d = A[i0][j] - d1[i0] - d2[j];
+          if (d < dist[j]) { dist[j] = d; par[j] = j0; }
+          if (dist[j] < delta) delta = dist[j1 = j];
         }
-    }
-    void reduce() {
-        for (int w = 0; w < dim; w++) {
-            unit minCost = INF;
-            for (int j = 0; j < dim; j++) minCost = min(minCost, costMatrix[w][j]);
-            for (int j = 0; j < dim; j++) costMatrix[w][j] -= minCost;
+        j0 = j1; for (int j = 0; j <= M; j++) {
+          if (done[j]) { d1[workerForJob[j]] += delta; d2[j] -= delta; }
+          else dist[j] -= delta;
         }
-        for (int j = 0; j < dim; j++) {
-            unit minCost = INF;
-            for (int w = 0; w < dim; w++) minCost = min(minCost, costMatrix[w][j]);
-            for (int w = 0; w < dim; w++) costMatrix[w][j] -= minCost;
-        }
+      } while (workerForJob[j0] != N);
+      for (; j0 != M; j0 = par[j0]) workerForJob[j0] = workerForJob[par[j0]];
     }
-    void computeInitialSolution() {
-        for (int j = 0; j < dim; j++) labelByJob[j] = INF;
-        for (int w = 0; w < dim; w++) for (int j = 0; j < dim; j++)
-            if (costMatrix[w][j] < labelByJob[j]) labelByJob[j] = costMatrix[w][j];
+    workerForJob.pop_back(); for (int j = 0; j < M; j++) {
+      if (workerForJob[j] == N) workerForJob[j] = -1;
+      else jobForWorker[workerForJob[j]] = j;
     }
-    void greedyMatch() {
-        for (int w = 0; w < dim; w++) for (int j = 0; j < dim; j++)
-            if (matchJobByWorker[w] == -1 && matchWorkerByJob[j] == -1 && costMatrix[w][j] - labelByWorker[w] - labelByJob[j] <= EPS)
-                match(w, j);
-    }
-    int getUnmatchedWorker() {
-        for (int w = 0; w < dim; w++) if (matchJobByWorker[w] == -1) return w;
-        return dim;
-    }
-    void initialize(int w) {
-        for (int i = 0; i < dim; i++) { committedWorkers[i] = false; parentWorkerByCommittedJob[i] = -1; }
-        committedWorkers[w] = true;
-        for (int j = 0; j < dim; j++) {
-            minSlackValueByJob[j] = costMatrix[w][j] - labelByWorker[w] - labelByJob[j];
-            minSlackWorkerByJob[j] = w;
-        }
-    }
-    void execute() {
-        while (true) {
-            int minSlackWorker = -1, minSlackJob = -1; unit minSlackValue = INF;
-            for (int j = 0; j < dim; j++) {
-                if (parentWorkerByCommittedJob[j] == -1) {
-                    if (minSlackValueByJob[j] < minSlackValue) {
-                        minSlackValue = minSlackValueByJob[j]; minSlackWorker = minSlackWorkerByJob[j]; minSlackJob = j;
-                    }
-                }
-            }
-            if (minSlackValue > EPS) updateLabeling(minSlackValue);
-            parentWorkerByCommittedJob[minSlackJob] = minSlackWorker;
-            if (matchWorkerByJob[minSlackJob] == -1) {
-                int committedJob = minSlackJob, parentWorker = parentWorkerByCommittedJob[committedJob];
-                while (true) {
-                    int temp = matchJobByWorker[parentWorker]; match(parentWorker, committedJob); committedJob = temp;
-                    if (committedJob == -1) break;
-                    parentWorker = parentWorkerByCommittedJob[committedJob];
-                }
-                return;
-            } else {
-                int worker = matchWorkerByJob[minSlackJob]; committedWorkers[worker] = true;
-                for (int j = 0; j < dim; j++) {
-                    if (parentWorkerByCommittedJob[j] == -1) {
-                        unit slack = costMatrix[worker][j] - labelByWorker[worker] - labelByJob[j];
-                        if (minSlackValueByJob[j] > slack) { minSlackValueByJob[j] = slack; minSlackWorkerByJob[j] = worker; }
-                    }
-                }
-            }
-        }
-    }
-    void init(int maxwj = MAXWJ) { for (int i = 0; i < maxwj; i++) fill(matrix[i], matrix[i] + maxwj, INF); }
-    unit run(int workers, int jobs) {
-        dim = max(workers, jobs); rows = workers; cols = jobs;
-        for (int i = 0; i < dim; i++) {
-            labelByWorker[i] = labelByJob[i] = minSlackValueByJob[i] = 0;
-            minSlackWorkerByJob[i] = 0;
-            matchJobByWorker[i] = matchWorkerByJob[i] = parentWorkerByCommittedJob[i] = -1;
-            committedWorkers[i] = false;
-            for (int j = 0; j < dim; j++) costMatrix[i][j] = matrix[i][j];
-        }
-        reduce(); computeInitialSolution(); greedyMatch();
-        int w = getUnmatchedWorker();
-        while (w < dim) { initialize(w); execute(); w = getUnmatchedWorker(); }
-        for (w = 0; w < rows; w++) if (matchJobByWorker[w] >= cols) matchJobByWorker[w] = -1;
-        for (int j = 0; j < cols; j++) if (matchWorkerByJob[j] >= rows) matchWorkerByJob[j] = -1;
-        totalCost = 0;
-        for (int w = 0; w < rows; w++) if (matchJobByWorker[w] != -1) totalCost += matrix[w][matchJobByWorker[w]];
-        return totalCost;
-    }
+    cost = -d2[M]; if (rev) { swap(N, M); jobForWorker.swap(workerForJob); }
+  }
 };
