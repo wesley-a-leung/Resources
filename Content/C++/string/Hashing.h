7cf4691ae34bea29d3de18f913af099d6128ef4d
wesley-a-leung <wesley.a.leung@gmail.com>
2020-12-10 01:26:16 -0500

    Updated Hashing

--- Content/C++/string/Hashing.h
+++ Content/C++/string/Hashing.h
@@ -1,45 +1,81 @@
 #pragma once
 #include <bits/stdc++.h>
 #include "../math/ModularArithmetic.h"
-#if __cplusplus < 201402L
-#include "../utils/MakeUnique.h"
-#endif
+#include "../utils/Random.h"
 using namespace std;
 
-std::seed_seq seq{
-    (uint64_t)std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now().time_since_epoch()).count(),
-    (uint64_t)__builtin_ia32_rdtsc(),(uint64_t)(uintptr_t)make_unique<char>().get()
-};
-std::mt19937_64 rng64(seq);
-
-// Computes the hash of a string to allow for easy computation of substring hashes for H hashes
-// MOD should be at least S^2 to prevent collisions
+// Computes the hash of an array/string to allow for easy computation of
+//   substring hashes
+// Template Arguments:
+//   T: the type of each element
+//   HTYPE: the type of the hash
+//   HASHES: the number of hashes to compute
+// Construtor Arguments:
+//   st: an iterator pointing to the first element in the string/array
+//   en: an iterator pointing to after the last element in the string/array
+//   offset: the amount to subtract from each element, a - offset + 1 should be
+//     positive
+//   BASE: array<HTYPE, HASHES> that contains the base for each hash, must
+//     be less than the repective mod and positive (preferably in the range
+//     [mod / 4, mod / 4 * 3])
+//   MOD: array<HTYPE, HASHES> that contains the mods for each hash, should
+//     be large (products should be least N^2) and prime
+// Fields:
+//   N: the size of the string
+//   BASE: aarray<HTYPE, HASHES> with the bases for each hash
+//   MOD: array<HTYPE, HASHES> with the mods for each hash
+//   H: a vector of array<HTYPE, HASHES> of size N + 1 with the suffix hashes
+//     starting at each index
+//   POW: a vector of array<HTYPE, HASHES> of size N + 1 with the ith
+//     representing base^i modulo mod
+// Functions:
+//   initBase(MOD): returns an array<HTYPE, HASHES> with randomly initialized
+//     values for the bases for each mod in the range [mod / 4, mod / 4 * 3]
+//   getHash(l, r): returns a type HASH with the hashes in the substring [l, r)
+//   merge(h1, h2): returns a type HASH of 2 hashes merged together as if
+//     the substring represented by h2 was immediately after h1
+//   concat(l1, r1, l2, r2): returns a type HASH of
+//     the substring [l1, r1) contactenated with the substring [l2, r2)
+// In practice, has a moderate constant
 // Time Complexity:
-//   compute: O(S * H)
-//   getHash: O(H)
-// Memory Complexity: O(S * H)
-template <const int MAXN, const int HASHES, class T, const int OFFSET> struct Hashing {
-    T BASE[HASHES], MOD[HASHES], H[MAXN][HASHES], POW[MAXN][HASHES]; struct HASH : public array<T, HASHES> { int len; };
-    Hashing(const array<T, HASHES> &mod) {
-        for (int h = 0; h < HASHES; h++) { MOD[h] = mod[h]; BASE[h] = uniform_int_distribution<T>(MOD[h] / 4, MOD[h] / 4 * 3)(rng64); }
-    }
-    void run(const string &s) {
-        int N = int(s.length()); fill(POW[0], POW[0] + HASHES, 1); fill(H[N], H[N] + HASHES, 0);
-        for (int i = 1; i <= N; i++) for (int h = 0; h < HASHES; h++) POW[i][h] = mulMod(POW[i - 1][h], BASE[h], MOD[h]);
-        for (int i = N - 1; i >= 0; i--) for (int h = 0; h < HASHES; h++)
-            H[i][h] = addMod(mulMod(H[i + 1][h], BASE[h], MOD[h]), T(s[i] - OFFSET + 1), MOD[h]);
-    }
-    HASH getHash(int l, int r) { // hash for substring [l, r)
-        HASH ret; ret.len = r - l; 
-        for (int h = 0; h < HASHES; h++) ret[h] = subMod(H[l][h], mulMod(H[r][h], POW[r - l][h], MOD[h]), MOD[h]);
-        return ret;
-    }
-    HASH merge(const HASH &h1, const HASH &h2) { // merges 2 hashes
-        HASH ret; ret.len = h1.len + h2.len;
-        for (int h = 0; h < HASHES; h++) ret[h] = addMod(h1[h], mulMod(h2[h], POW[h1.len][h], MOD[h]), MOD[h]);
-        return ret;
-    }
-    HASH concat(int l1, int r1, int l2, int r2) { // hash for the concatenated substring [l1, r1) + [l2, r2)
-        return merge(getHash(l1, r1), getHash(l2, r2));
-    }
+//   constructor: O(N HASHES)
+//   getHash, merge, concat: O(N HASHES)
+// Memory Complexity: O(N HASHES)
+// Tested:
+//   https://dmoj.ca/problem/globexcup19j5hard
+template <class T, class HTYPE, const int HASHES> struct Hashing {
+  using arr = array<HTYPE, HASHES>; int N; arr BASE, MOD; vector<arr> H, POW;
+  struct HASH : public arr { int len; };
+  template <class It>
+  Hashing(It st, It en, T offset, const arr &BASE, const arr &MOD)
+      : N(en - st), BASE(BASE), MOD(MOD), H(N + 1), POW(N + 1) {
+    POW[0].fill(HTYPE(1)); H[N].fill(HTYPE());
+    for (int i = 1; i <= N; i++) for (int h = 0; h < HASHES; h++)
+      POW[i][h] = mulMod(POW[i - 1][h], BASE[h], MOD[h]);
+    for (int i = N - 1; i >= 0; i--) for (int h = 0; h < HASHES; h++)
+      H[i][h] = addMod(mulMod(H[i + 1][h], BASE[h], MOD[h]),
+                       posMod(HTYPE(st[i] - offset) + HTYPE(1), MOD[h]),
+                       MOD[h]);
+  }
+  static arr initBase(const arr &MOD) {
+    arr base;
+    for (int h = 0; h < HASHES; h++) base[h] = uniform_int_distribution<HTYPE>(
+        MOD[h] / 4, MOD[h] / 4 * 3)(rng64);
+    return base;
+  }
+  template <class It> Hashing(It st, It en, T offset, const arr &MOD)
+      : Hashing(st, en, offset, initBase(MOD), MOD) {}
+  HASH getHash(int l, int r) {
+    HASH ret; ret.len = r - l; for (int h = 0; h < HASHES; h++)
+      ret[h] = subMod(H[l][h], mulMod(H[r][h], POW[r - l][h], MOD[h]), MOD[h]);
+    return ret;
+  }
+  HASH merge(const HASH &h1, const HASH &h2) {
+    HASH ret; ret.len = h1.len + h2.len; for (int h = 0; h < HASHES; h++)
+      ret[h] = addMod(h1[h], mulMod(h2[h], POW[h1.len][h], MOD[h]), MOD[h]);
+    return ret;
+  }
+  HASH concat(int l1, int r1, int l2, int r2) {
+    return merge(getHash(l1, r1), getHash(l2, r2));
+  }
 };
