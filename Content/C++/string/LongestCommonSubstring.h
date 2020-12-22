a20161c4fe9a2297ed07cba020a3056a71b2de40
wesley-a-leung <wesley.a.leung@gmail.com>
2020-12-22 12:17:46 -0500

    Fixed bug in LCS

--- Content/C++/string/LongestCommonSubstring.h
+++ Content/C++/string/LongestCommonSubstring.h
@@ -24,9 +24,10 @@ using namespace std;
 //     second string/array
 //   enB: an iterator pointing to after the last element in
 //     second string/array
+//   sep: a separator character that does not appear in either string/array
 // Fields:
 //   SA: the associated suffix array constructed from the string/array A,
-//     concatenated with the string/array B
+//     concatenated with the separator, conatenated with the string/array B
 //   lcs: the string/array of longest common substring of A and B
 // In practice, the constructor has a very small constant
 // Time Complexity:
@@ -36,24 +37,28 @@ using namespace std;
 //   https://www.spoj.com/problems/LCS/
 template <class T> struct LongestCommonSubstring {
   SuffixArray<T> SA; vector<T> lcs;
-  template <class F, class G> SuffixArray<T> init(int N, F f, int M, G g) {
+  template <class F, class G>
+  SuffixArray<T> init(int N, F f, int M, G g, T sep) {
     int i = 0;
-    return SuffixArray<T>(N + M, [&] { return i++ < N ? f() : g(); });
+    return SuffixArray<T>(N + M + 1, [&] {
+      T ret = i < N ? f() : i == N ? sep : g(); i++; return ret;
+    });
   }
-  template <class F, class G> LongestCommonSubstring(int N, F f, int M, G g)
-      : SA(init(N, f, M, g)) {
-    pair<int, int> mx(-1, -1); for (int i = 0; i < N + M - 1; i++)
-      if ((SA.ind[i] < N) != (SA.ind[i + 1] < N)) {
-        int bnd = N - (SA.ind[i] < N ? SA.ind[i] : SA.ind[i + 1]);
-        mx = max(mx, make_pair(min(SA.LCP[i], bnd), SA.ind[i]));
-      }
+  template <class F, class G>
+  LongestCommonSubstring(int N, F f, int M, G g, T sep)
+      : SA(init(N, f, M, g, sep)) {
+    pair<int, int> mx(-1, -1); for (int i = 0; i < N + M; i++)
+      if (SA.ind[i] != N && SA.ind[i + 1] != N
+          && (SA.ind[i] < N) != (SA.ind[i + 1] < N))
+        mx = max(mx, make_pair(SA.LCP[i], SA.ind[i]));
     if (mx.first != -1) {
       lcs.reserve(mx.first);
       for (int i = 0; i < mx.first; i++) lcs.push_back(SA.S[mx.second + i]);
     }
   }
-  template <class ItA, class ItB>
-  LongestCommonSubstring(ItA stA, ItA enA, ItB stB, ItB enB)
-      : LongestCommonSubstring(enA - stA, [&] { return *stA++; },
-                               enB - stB, [&] { return *stB++; }) {}
+  template <class ItPat, class ItTxt>
+  LongestCommonSubstring(ItPat stPat, ItPat enPat, ItTxt stTxt, ItTxt enTxt,
+                         T sep)
+      : LongestCommonSubstring(enPat - stPat, [&] { return *stPat++; },
+                               enTxt - stTxt, [&] { return *stTxt++; }, sep) {}
 };
