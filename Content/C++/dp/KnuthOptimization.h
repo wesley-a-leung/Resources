6b39a8bb407286f7f2950ca8b988f1bd7008df91
wesley-a-leung <wesley.a.leung@gmail.com>
2020-12-24 10:33:40 -0500

    Updated knuth

--- Content/C++/dp/KnuthOptimization.h
+++ Content/C++/dp/KnuthOptimization.h
@@ -3,24 +3,41 @@
 using namespace std;
 
 // Knuth's Dynamic Programming Optimization
-// Must statisfy dp[i][j] = min(dp[i][k] + dp[k][j] + cost(i, k, j)) for i <= k <= j
-// and A[i][j - 1] <= A[i][j] <= A[i + 1][j], where A[i][j] is the optimal value of k
-// for dp[i][j]
-// Time Complexity: Reduces the runtime from O(N^3) to O(N^2)
+// Must statisfy dp[l][r] = max(dp[l][m] + dp[m][r] + cost(l, m, r))
+//   for l <= m <= r and
+//   max(l + 1, mid[l][r - 1]) <= mid[l][r] <= min(mid[l + 1][r], r - 1),
+//   where mid[l][r] is the optimal value of m for dp[l][r]
+// Template Arguments:
+//   T: the type of the return value of the cost function
+//   F: the type of the function used to compute the cost function (can be
+//     can be deduced automatically in some cases)
+//   Cmp: the comparator to compare two T values,
+//       convention is same as priority_queue in STL
+//     Required Functions:
+//       operator (a, b): returns true if and only if a compares less than b
+// Function Arguments:
+//   N: the size of the array, must be positive
+//   f(l, m, r): computes the cost function for the range [l, r) with the
+//     midpoint m
+//   cmp: an instance of the Cmp struct
+// Return Value: the maximum dp value (based on the comparator) for
+//   the range [0, N)
+// In practice, has a small constant
+// Time Complexity: O(N^2)
 // Memory Complexity: O(N^2)
-template <const int MAXN, class T> struct KnuthOptimization {
-    T dp[MAXN][MAXN], INF; int mid[MAXN][MAXN];
-    KnuthOptimization(T INF) : INF(INF) {}
-    T cost(int l, int m, int r); // to be implemented
-    T solve(int N) {
-        for (int l = N - 1; l >= 0; l--) for (int r = l; r <= N; r++) {
-            if (r - l < 2) { dp[l][r] = 0; mid[l][r] = l; continue; }
-            dp[l][r] = INF;
-            for (int m = mid[l][r - 1]; m <= mid[l + 1][r]; m++) {
-                T temp = dp[l][m] + dp[m][r] + cost(l, m, r);
-                if (dp[l][r] > temp) { dp[l][r] = temp; mid[l][r] = m; }
-            }
-        }
-        return dp[0][N];
+// Tested:
+//   https://atcoder.jp/contests/dp/tasks/dp_n
+template <class T, class F, class Cmp = less<T>>
+T knuth(int N, F f, Cmp cmp = Cmp()) {
+  assert(N > 0); vector<vector<T>> dp(N, vector<T>(N + 1, T()));
+  vector<vector<int>> mid(N, vector<int>(N + 1));
+  for (int l = N - 1; l >= 0; l--) for (int r = l; r <= N; r++) {
+    if (r - l <= 1) { mid[l][r] = l; continue; }
+    int st = max(l + 1, mid[l][r - 1]), en = min(mid[l + 1][r], r - 1);
+    for (int m = st; m <= en; m++) {
+      T cost = dp[l][m] + dp[m][r] + f(l, m, r);
+      if (m == st || cmp(dp[l][r], cost)) { dp[l][r] = cost; mid[l][r] = m; }
     }
-};
+  }
+  return dp[0][N];
+}
