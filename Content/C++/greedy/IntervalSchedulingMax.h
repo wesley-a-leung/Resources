5ee97eabdf3067feb8feecf7a6c9bd04fc7649c6
wesley-a-leung <wesley.a.leung@gmail.com>
2020-12-25 01:31:23 -0500

    Updated IntervalSchedulingMax

--- Content/C++/greedy/IntervalSchedulingMax.h
+++ Content/C++/greedy/IntervalSchedulingMax.h
@@ -2,22 +2,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using T = long double; const T EPS = 1e-9;
-
-bool lt(T a, T b) { return a + EPS < b; }
-
 // Solves the interval scheduling maximization problem
-// Given a set of intervals in the form [L, R), find the maximum number of non overlapping intervals
-// Returns an iterator to right after the last disjoint interval
+// Given a set of intervals in the form [l, r) which are sorted by r,
+//   find the maximum number of non overlapping intervals
+// Template Arguments:
+//   It: the type of the iterator for the array of pairs
+//     with the first element being the inclusive left bound of the interval
+//     and the second element being the exclusive right bound of the interval
+//   Cmp: the comparator to compare two indices,
+//     Required Functions:
+//       operator (a, b): returns true if and only if a compares less than b
+// Constructor Arguments:
+//   st: an iterator pointing to the first element in the array of pairs
+//     with the first element being the inclusive left bound of the interval
+//     and the second element being the exclusive right bound of the interval
+//   en: an iterator pointing to after the last element in the array of pairs
+//     with the first element being the inclusive left bound of the interval
+//     and the second element being the exclusive right bound of the interval
+//   cmp: an instance of the Cmp struct
+// Return Value: an iterator to after the last disjoint interval after the
+//   array is modified
+// In practice, has a very small constant
 // Time Complexity: O(N)
 // Memory Complexity: O(1)
-bool cmp(const pair<T, T> &a, const pair<T, T> &b) { return lt(a.second, b.second); }
-template <class It> It intervalSchedulingMax(It st, It en) {
-    assert(is_sorted(st, en, cmp));
-    It cur = st;
-    for (It l = st, r; l < en; l = r, cur++) {
-        *cur = *l;
-        for (r = l + 1; r < en && lt(r->first, cur->second); r++);
-    }
-    return cur;
+// Tested:
+//   https://codeforces.com/contest/1141/problem/F2
+template <
+    class It,
+    class Cmp = less<typename iterator_traits<It>::value_type::first_type>>
+It intervalSchedulingMax(It st, It en, Cmp cmp = Cmp()) {
+  using Pair = typename iterator_traits<It>::value_type;
+  assert(is_sorted(st, en, [&] (const Pair &a, const Pair &b) {
+    return cmp(a.second, b.second);
+  }));
+  It cur = st; for (It l = st, r = st; l != en; l = r, cur++) {
+    *cur = *l; for (r = l + 1; r != en && cmp(r->first, cur->second); r++);
+  }
+  return cur;
 }
