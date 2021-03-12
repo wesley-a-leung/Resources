0a17a6731f60e714049c647faf02dfd72989bc83
wesley-a-leung <wesley.a.leung@gmail.com>
2021-03-12 04:31:39 -0500

    Updated hungarian

--- Content/C++/graph/matching/HungarianAlgorithm.h
+++ Content/C++/graph/matching/HungarianAlgorithm.h
@@ -5,12 +5,14 @@ using namespace std;
 // Solves the assignment problem of matching N workers to M jobs with the
 //   minimum cost where each worker can only be assigned to at most 1 job and
 //   each job can only be assigned to at most 1 worker
+// Maximum cost can be found by negating all non infinity values in the matrix
+//   and taking the negative of the minimum cost
 // Template Arguments:
 //   T: the type of the cost
 // Constructor Arguments:
 //   A: a matrix of size N x M with A[i][j] representing the cost of assigning
 //     worker i to job j
-//   INF: a value for infinity, must be negatable
+//   INF: a value for infinity
 // Fields:
 //   N: the number of workers
 //   M: the number of jobs
@@ -55,9 +57,8 @@ template <class T> struct HungarianAlgorithm {
         done[j0] = true; int i0 = workerForJob[j0], j1 = M;
         pair<int, T> delta = make_pair(1, T());
         for (int j = 0; j < M; j++) if (!done[j]) {
-          pair<int, T> d = make_pair(0, A[i0][j]);
-          if (A[i0][j] == INF) d = make_pair(1, T());
-          else if (A[i0][j] == -INF) d = make_pair(-1, T());
+          pair<int, T> d = A[i0][j] == INF ? make_pair(1, T())
+                                           : make_pair(0, A[i0][j]);
           sub(d, d1[i0]); sub(d, d2[j]);
           if (d < dist[j]) { dist[j] = d; par[j] = j0; }
           if (dist[j] < delta) delta = dist[j1 = j];
