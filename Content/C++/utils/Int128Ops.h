43e25decce58ce1d8011910aa4510959a828c623
wesley-a-leung <wesley.a.leung@gmail.com>
2021-03-28 16:25:38 -0400

    Reordered lines

--- Content/C++/utils/Int128Ops.h
+++ Content/C++/utils/Int128Ops.h
@@ -7,7 +7,7 @@ __int128_t abs(__int128_t a) { return a >= 0 ? a : -a; }
 #define FUN(name) \
   long double name(__int128_t a) { return name((long double)(a)); }
 FUN(sqrt) FUN(sin) FUN(cos) FUN(tan) FUN(asin) FUN(acos) FUN(atan)
+#undef FUN
 long double atan2(__int128_t y, __int128_t x) {
   return atan2((long double)(y), (long double)(x));
 }
-#undef FUN
